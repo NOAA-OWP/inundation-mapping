5b0a658b130949da7a56e33ec92e444dad84b1c2
BradfordBates-NOAA <bradford.bates@noaa.gov>
2020-06-02 14:58:22 -0500

    Revert "Merge remote-tracking branch 'remotes/origin/foss-fim-batch-processing-eol_testing' into foss-fim-development"
    This reverts commit 62384cac2759a994d7d0a3f805799b2831b9df55, reversing
    changes made to 832723ad8eb9a3c75debc877a0749cbf0bafb7b4.

--- lib/get_all_huc_in_inputs.py
+++ lib/get_all_huc_in_inputs.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
 
 import geopandas as gpd
@@ -37,43 +36,3 @@ if __name__ == '__main__':
     hucLength = args['huc_length']
 
     find_unique_hucs(inputsDir,hucLength)
-=======
-#!/usr/bin/env python3
-
-import geopandas as gpd
-import pandas as pd
-import numpy as np
-from glob import glob
-import argparse
-from os.path import join
-from tqdm import tqdm
-
-def find_unique_hucs(inputsDir,hucLength):
-
-    # get file list with glob
-    huc4_vaa_list = glob(join(inputsDir,'NHDPlusFlowlineVAA_*.gpkg'))
-
-    unique_hucs = np.array([])
-    for vaa_file in tqdm(huc4_vaa_list):
-        reachCodes = gpd.read_file(vaa_file)['ReachCode']
-        reachCodes = reachCodes.astype(str)
-        reachCodes = reachCodes.apply(lambda x : x[0:8])
-        unique_hucs = np.append(unique_hucs,reachCodes.apply(lambda x: x[0:hucLength]).unique())
-    
-    unique_hucs = pd.Series(unique_hucs)
-    unique_hucs.to_csv(join(inputsDir,'included_huc{}.lst'.format(hucLength)),header=False,index=False)
-
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Get unique HUCs in results data dir')
-    parser.add_argument('-i','--inputs-directory',help='Basins polygons to use within project path',required=True)
-    parser.add_argument('-l','--huc-length',help='Basins polygons to use within proiject path',required=True,type=int)
-
-    args = vars(parser.parse_args())
-
-    inputsDir = args['inputs_directory']
-    hucLength = args['huc_length']
-
-    find_unique_hucs(inputsDir,hucLength)
->>>>>>> remotes/origin/foss-fim-batch-processing-eol_testing
