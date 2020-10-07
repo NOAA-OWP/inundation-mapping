111426bc54d0cc62a5ff71b24b8f33f61b1d30d5
brian.avant <brian.avant@noaa.gov>
2020-10-07 20:57:26 +0000

    minor edits to mannings_crosswalk_adj.sh

--- tests/mannings_crosswalk_adj.sh
+++ tests/mannings_crosswalk_adj.sh
@@ -7,6 +7,7 @@ usage ()
     echo ''
     echo 'REQUIRED:'
     echo '  -d/--indir    : initial run directory with default mannings values'
+    echo '  -t/--hucnumber    : '
     echo '  -o/--outdir     : directory for output mannings parameter adjustment runs'
     echo '  -n/--paramfile    : parameter set file'
     echo '  -f/--paramfolder    : parameter set file path'
@@ -58,17 +59,22 @@ if [ "$indir" = "" ]
 then
     usage
 fi
-
+# runtestcase_outfolder=mannings_test_calib
+pathdir=$indir/$hucnumber
 count=1
 while read p; do
   echo "$p"
   # Generate mannings_table.json
-  python3 foss_fim/config/create_mannings_table.py -d "$p" -f $paramfolder/mannings_table.json
+  python3 foss_fim/config/create_mannings_table.py -d "$p" -f $paramfolder/mannings_template.json
 
   subdir=$outdir"_"$count
   mkdir -p $subdir
 
   $libDir/add_crosswalk.py $indir/gw_catchments_reaches_clipped_addedAttributes.gpkg $indir/demDerived_reaches_split_clipped.gpkg $indir/src_base.csv $indir/majority.geojson $subdir/gw_catchments_reaches_clipped_addedAttributes_crosswalked.gpkg $subdir/demDerived_reaches_split_clipped_addedAttributes_crosswalked.gpkg $subdir/src_full_crosswalked.csv $subdir/src.json $subdir/crosswalk_table.csv $subdir/hydroTable.csv $indir/wbd8_clp.gpkg $indir/nwm_subset_streams.gpkg $paramfolder/mannings_table.json
   count=`expr $count + 1`
+
+  python3 foss_fim/tests/run_test_case.py -r $subdir -t $p"_ble" -b $subdir -c
+
+  python3 foss_fim/tests/calc_obj_func.py -d data/test_cases/performance_archive/development_versions/$subdir -o 'data/temp/mannings_test.txt' -p $count
 
 done <$paramfolder/$paramfile
