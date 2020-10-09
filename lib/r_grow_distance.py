57f49c21399e88487c4e462d342ce2827c5f7ba5
TrevorGrout-NOAA <trevor.grout@noaa.gov>
2020-10-09 10:58:00 -0500

    updates to agree and r grow distance scripts

--- lib/r_grow_distance.py
+++ lib/r_grow_distance.py
@@ -8,18 +8,20 @@ from grass_session import Session
 import os
 import shutil 
 import grass.script as gscript
+import argparse
 
-workspace = 'data/temp/tsg/grassdata'
-input_raster = '/data/temp/tsg/grass/data/vect_grid.tif'
-def r_grow_distance(input_raster, workspace):
+# workspace = 'data/temp/tsg/grassdata'
+# input_raster = '/data/temp/tsg/grass/data/vect_grid.tif'
+# input_raster = '/data/temp/tsg/grass/data/bufgrid.tif'
+def r_grow_distance(input_raster, grass_workspace):
     input_raster_directory = os.path.dirname(input_raster)
     input_raster_name = os.path.splitext(os.path.basename(input_raster))[0]
     
-    grass_gisdb = workspace
+    grass_gisdb = grass_workspace
     grass_location = 'temporary_location'
     grass_mapset = 'temporary_mapset'
     projected_file = input_raster
-
+    
     PERMANENT = Session()
     PERMANENT.open(gisdb = grass_gisdb, location = grass_location, create_opts = projected_file)
     PERMANENT.close()
@@ -42,3 +44,21 @@ def r_grow_distance(input_raster, workspace):
     
     temporary_session.close()
     shutil.rmtree(grass_gisdb)
+    return proximity_filename,allocation_filename
+
+if __name__ == '__main__':
+    #Parse arguments
+    parser = argparse.ArgumentParser(description = 'Calculate AGREE DEM')
+    parser.add_argument('-i', '--in_raster', help = 'raster to perform r.grow.distance', required = True)
+    parser.add_argument('-g', '--grass_workspace', help = 'Temporary GRASS workspace', required = True)
+
+    #Extract to dictionary and assign to variables.
+    args = vars(parser.parse_args())
+
+    # rename variable inputs
+    input_raster = args['in_raster']
+    grass_workspace = args['grass_workspace']
+    
+    #Run r_grow_distance
+    r_grow_distance(input_raster, grass_workspace)
+    
