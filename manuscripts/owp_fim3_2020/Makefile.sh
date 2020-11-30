f1f7f0f16aa88ff53a11e13a0c523631e329339a
fernando-aristizabal <fernando.aristizabal@noaa.gov>
2020-11-30 20:41:25 +0000

    Various changes * added to introduction * added more references * added figures directory * deleted variables file

---
+++ manuscripts/owp_fim3_2020/Makefile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+fullFile=owp_fim3.tex
+baseName="${fullFile%%.*}"
+extension="${fullFile#*.}"
+
+pdflatex "$fullFile"
+bibtex "$baseName".aux
+pdflatex "$fullFile"
+pdflatex "$fullFile"
+
+
+
+
