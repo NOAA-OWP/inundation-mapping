ae75a50a43a06a1fc03e3cc8a0c11dc0205fea30
nickchadwick-noaa <nick.chadwick@noaa.gov>
2021-01-08 18:53:56 +0000

    Finished Output Handler, general cleanup
     - Finished output handler to write resulting outputs to frontend server
     - Updated web interface and included all options for fim_run
     - Finished connection between connector and output handler services

--- api/node/connector/entrypoint.sh
+++ api/node/connector/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd /opt/connector/
-echo "Starting Gunicorn"
-exec gunicorn -k gevent -w 1 --bind 0.0.0.0:6000 --log-level=warning --reload wsgi:app
+echo "Starting Connector"
+python3 ./connector.py
--- api/node/connector/wsgi.py
+++
@@ -1,4 +0,0 @@
-from connector import app
-
-if __name__ == "__main__":
-    app.run()
