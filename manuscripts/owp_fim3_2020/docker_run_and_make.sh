977d836d647ff5a18d142a8c098d9a70c4a73d1c
fernando-aristizabal <fernando.aristizabal@noaa.gov>
2021-02-02 22:34:58 +0000

    more work on m&m. about to start on deriving fim network

--- manuscripts/owp_fim3_2020/docker_run_and_make.sh
+++ manuscripts/owp_fim3_2020/docker_run_and_make.sh
@@ -2,4 +2,4 @@
 
 codeDir=$1
 
-docker run --rm -v "$1":/foss_fim fernandoaristizabal/latex-full:dev_20201207 /foss_fim/manuscripts/owp_fim3_2020/Makefile.sh
+docker run --rm -v "$1":/foss_fim fim3_latex:20210131 /foss_fim/manuscripts/owp_fim3_2020/Makefile.sh
