4264ba3c8b80792607eb5e7da840524f42355821
Trevor Grout <trevor.grout@noaa.gov>
2021-03-10 14:50:42 +0000

    modified comments, commented out creation of location points for generated flow files

--- tools/generate_categorical_fim_flows.py
+++ tools/generate_categorical_fim_flows.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+#import variables from .env file
 API_BASE_URL = os.getenv("API_BASE_URL")
 EVALUATED_SITES_CSV = os.getenv("EVALUATED_SITES_CSV")
 WBD_LAYER = os.getenv("WBD_LAYER")
@@ -76,10 +77,10 @@ def static_flow_lids(workspace, nwm_us_search, nwm_ds_search):
     print('Getting list of mainstem segments')
     #Import list of evaluated sites
     list_of_sites = pd.read_csv(EVALUATED_SITES_CSV)['Total_List'].to_list()
-    #!!!Be sure to check the must_include argument within this function!!!
+    #The entire routine to get mainstems is harcoded in this function.
     ms_segs = mainstem_nwm_segs(metadata_url, list_of_sites)
     
-    #Loop through each aggregate unit
+    #Loop through each huc unit
     all_messages = []
     for huc in huc_dictionary:
         print(f'Iterating through {huc}')
@@ -87,12 +88,13 @@ def static_flow_lids(workspace, nwm_us_search, nwm_ds_search):
         nws_lids = huc_dictionary[huc]
         #Loop through each lid in list to create flow file
         for lid in nws_lids:
+            #In some instances the lid is not assigned a name, skip over these.
             if not isinstance(lid,str):
                 print(f'{lid} is {type(lid)}')
                 continue
             #Convert lid to lower case
             lid = lid.lower()
-            #Get stages and flows for each threshold
+            #Get stages and flows for each threshold from the WRDS API. Priority given to USGS calculated flows.
             stages, flows = get_thresholds(threshold_url = threshold_url, location_ids = lid, physical_element = 'all', threshold = 'all', bypass_source_flag = False)
             #If stages/flows don't exist write message and exit out.
             if not (stages and flows):
@@ -100,10 +102,10 @@ def static_flow_lids(workspace, nwm_us_search, nwm_ds_search):
                 all_messages.append(message)
                 continue
 
-            #Instead of calling WRDS API, find corresponding record in the list of dictionaries
+            #find lid metadata from master list of metadata dictionaries (line 66).
             metadata = next((item for item in all_lists if item['identifiers']['nws_lid'] == lid.upper()), False)
         
-            #Get NWM Segments by intersecting with known mainstem segments
+            #Get mainstem segments of LID by intersecting LID segments with known mainstem segments.
             segments = get_nwm_segs(metadata)        
             site_ms_segs = set(segments).intersection(ms_segs)
             segments = list(site_ms_segs)       
@@ -115,6 +117,7 @@ def static_flow_lids(workspace, nwm_us_search, nwm_ds_search):
                 continue
             #For each flood category
             for category in ['action', 'minor', 'moderate', 'major', 'record']:
+                #Get the flow
                 flow = flows[category]
                 #If there is a valid flow value, write a flow file.
                 if flow:
@@ -130,8 +133,8 @@ def static_flow_lids(workspace, nwm_us_search, nwm_ds_search):
                 else:
                     message = f'{lid}_{category}_no flow'
                     all_messages.append(message)
-            #This section will produce a point file of the AHPS location
-            #Get various attributes of the site including lat, lon, rfc, state, county, name, flows, and stages for each threshold.
+            #This section will produce a point file of the LID location
+            #Get various attributes of the site.
             lat = float(metadata['usgs_data']['latitude'])
             lon = float(metadata['usgs_data']['longitude'])
             wfo = metadata['nws_data']['wfo']
@@ -154,11 +157,12 @@ def static_flow_lids(workspace, nwm_us_search, nwm_ds_search):
             stage_units = stages['units']
             stage_source = stages['source']
             wrds_timestamp = stages['wrds_timestamp']
-            #Create a GeoDataFrame using the lat/lon information
+            #Create a DataFrame using the collected attributes
             df = pd.DataFrame({'nws_lid': [lid], 'name':name, 'WFO': wfo, 'rfc':rfc, 'huc':[huc], 'state':state, 'county':county, 'q_act':q_act, 'q_min':q_min, 'q_mod':q_mod, 'q_maj':q_maj, 'q_rec':q_rec, 'q_uni':flow_units, 'q_src':flow_source, 'stage_act':s_act, 'stage_min':s_min, 'stage_mod':s_mod, 'stage_maj':s_maj, 'stage_rec':s_rec, 'stage_uni':stage_units, 's_src':stage_source, 'wrds_time':wrds_timestamp, 'lat':[lat], 'lon':[lon]})
             #Round stages and flows to nearest hundredth
             df = df.round({'q_act':2,'q_min':2,'q_mod':2,'q_maj':2,'q_rec':2,'stage_act':2,'stage_min':2,'stage_mod':2,'stage_maj':2,'stage_rec':2})
-            gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['lon'], df['lat']), crs =  "EPSG:4326") 
+            #Create a geodataframe using lat/lon. Assume WGS84 for CRS.
+            #gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['lon'], df['lat']), crs =  "EPSG:4326") 
 
             #Create a csv with same info as shapefile
             csv_df = pd.DataFrame()
@@ -168,37 +172,38 @@ def static_flow_lids(workspace, nwm_us_search, nwm_ds_search):
             #Round flow and stage columns to 2 decimal places.
             csv = csv_df.round({'q':2,'stage':2})
 
-            
+            #If a site folder exists (ie a flow file was written) save files containing site attributes.
             try:
-                #Save GeoDataFrame to shapefile format and export csv
+                #Save GeoDataFrame to shapefile format and export csv containing attributes
                 output_dir = workspace / huc / lid
-                gdf.to_file(output_dir / f'{lid}_location.shp' )
+                #gdf.to_file(output_dir / f'{lid}_location.shp' )
                 csv_df.to_csv(output_dir / f'{lid}_info.csv', index = False)
             except:
                 print(f'{lid} missing all flows')
                 message = f'{lid} missing all flows'
                 all_messages.append(message)
-    #Write out messages
+    #Write out messages to file
     messages_df  = pd.DataFrame(all_messages, columns = ['message'])
     messages_df.to_csv(workspace / f'all_messages.csv', index = False)
 
-    #Find all location shapefiles
-    locations_files = list(workspace.rglob('*_location.shp'))    
-    spatial_layers = gpd.GeoDataFrame()
+    #Recursively find all location shapefiles
+    #locations_files = list(workspace.rglob('*_location.shp'))    
+    #spatial_layers = gpd.GeoDataFrame()
     #Append all shapefile info to a geodataframe
-    for location in locations_files:
-        gdf = gpd.read_file(location)
-        spatial_layers = spatial_layers.append(gdf)
+    #for location in locations_files:
+    #    gdf = gpd.read_file(location)
+    #    spatial_layers = spatial_layers.append(gdf)
     #Write appended spatial data to disk.
-    output_file = workspace /'all_mapped_ahps.shp'
-    spatial_layers.to_file(output_file)
-    #Find all *_info csv files
+    #output_file = workspace /'all_mapped_ahps.shp'
+    #spatial_layers.to_file(output_file)
+    
+    #Recursively find all *_info csv files and append
     csv_files = list(workspace.rglob('*_info.csv'))
     all_csv_df = pd.DataFrame()
     for csv in csv_files:
         temp_df = pd.read_csv(csv)
         all_csv_df = all_csv_df.append(temp_df, ignore_index = True)
-    #Write csv to file
+    #Write appended _info.csvs to file
     all_info_csv = workspace / '_info.csv'
     all_csv_df.to_csv(all_info_csv, index = False)
     all_end = time.time()
