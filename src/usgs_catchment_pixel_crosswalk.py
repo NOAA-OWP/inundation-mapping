8e36fab37c3fb1fff0cb5f650f11f988c34120fc
brian.avant <brian.avant@noaa.gov>
2021-03-10 20:04:01 +0000

    handles no nearby hydroids

--- src/usgs_catchment_pixel_crosswalk.py
+++ src/usgs_catchment_pixel_crosswalk.py
@@ -3,6 +3,7 @@
 import os
 import geopandas as gpd
 import pandas as pd
+import numpy as np
 import rasterio
 import argparse
 import pygeos
@@ -38,42 +39,45 @@ def crosswalk_usgs_gage(usgs_gages_filename,dem_filename,input_flows_filename,in
         print (f"usgs gage: {gage.site_no}")
         # Get stream attributes
         hydro_id = closest_hydro_id.loc[closest_hydro_id.site_no==gage.site_no].HydroID.item()
-        min_thal_elev = round(closest_hydro_id.loc[closest_hydro_id.site_no==gage.site_no].Min_Thal_Elev_m.item(),2)
-        med_thal_elev = round(closest_hydro_id.loc[closest_hydro_id.site_no==gage.site_no].Median_Thal_Elev_m.item(),2)
-        max_thal_elev = round(closest_hydro_id.loc[closest_hydro_id.site_no==gage.site_no].Max_Thal_Elev_m.item(),2)
 
-        # Convert headwater point geometries to WKB representation
-        wkb_gages = dumps(gage.geometry)
+        if not np.isnan(hydro_id):
 
-        # Create pygeos headwater point geometries from WKB representation
-        gage_bin_geom = pygeos.io.from_wkb(wkb_gages)
+            min_thal_elev = round(closest_hydro_id.loc[closest_hydro_id.site_no==gage.site_no].Min_Thal_Elev_m.item(),2)
+            med_thal_elev = round(closest_hydro_id.loc[closest_hydro_id.site_no==gage.site_no].Median_Thal_Elev_m.item(),2)
+            max_thal_elev = round(closest_hydro_id.loc[closest_hydro_id.site_no==gage.site_no].Max_Thal_Elev_m.item(),2)
 
-        # Closest segment to headwater
-        closest_stream = input_flows.loc[input_flows.HydroID==hydro_id]
-        wkb_closest_stream = dumps(closest_stream.geometry.item())
-        stream_bin_geom = pygeos.io.from_wkb(wkb_closest_stream)
+            # Convert headwater point geometries to WKB representation
+            wkb_gages = dumps(gage.geometry)
 
-        # Linear reference headwater to closest stream segment
-        gage_distance_to_line = pygeos.linear.line_locate_point(stream_bin_geom, gage_bin_geom)
-        referenced_gage = pygeos.linear.line_interpolate_point(stream_bin_geom, gage_distance_to_line)
+            # Create pygeos headwater point geometries from WKB representation
+            gage_bin_geom = pygeos.io.from_wkb(wkb_gages)
 
-        # Convert geometries to wkb representation
-        bin_referenced_gage = pygeos.io.to_wkb(referenced_gage)
+            # Closest segment to headwater
+            closest_stream = input_flows.loc[input_flows.HydroID==hydro_id]
+            wkb_closest_stream = dumps(closest_stream.geometry.item())
+            stream_bin_geom = pygeos.io.from_wkb(wkb_closest_stream)
 
-        # Convert to shapely geometries
-        shply_referenced_gage = loads(bin_referenced_gage)
+            # Linear reference headwater to closest stream segment
+            gage_distance_to_line = pygeos.linear.line_locate_point(stream_bin_geom, gage_bin_geom)
+            referenced_gage = pygeos.linear.line_interpolate_point(stream_bin_geom, gage_distance_to_line)
 
-        # Sample rasters at adjusted gage
-        dem_m_elev = list(rasterio.sample.sample_gen(dem_m,shply_referenced_gage.coords))[0].item().astype(float).round(2)
-        dem_adj_elev = list(rasterio.sample.sample_gen(dem_adj,shply_referenced_gage.coords))[0].item().astype(float).round(2)
+            # Convert geometries to wkb representation
+            bin_referenced_gage = pygeos.io.to_wkb(referenced_gage)
 
-        # Print elevations to log file
-        print(f"post adjusted catchment pixel ID: {dem_m_elev}")
-        print(f"post adjusted elevation: {dem_adj_elev}")
+            # Convert to shapely geometries
+            shply_referenced_gage = loads(bin_referenced_gage)
 
-        # Append dem_m_elev, dem_adj_elev, hydro_id, and gage number to table
-        site_elevations = [gage.site_no, hydro_id, dem_m_elev, dem_adj_elev, min_thal_elev, med_thal_elev, max_thal_elev]
-        gage_data.append(site_elevations)
+            # Sample rasters at adjusted gage
+            dem_m_elev = list(rasterio.sample.sample_gen(dem_m,shply_referenced_gage.coords))[0].item().astype(float).round(2)
+            dem_adj_elev = list(rasterio.sample.sample_gen(dem_adj,shply_referenced_gage.coords))[0].item().astype(float).round(2)
+
+            # Print elevations to log file
+            print(f"post adjusted catchment pixel ID: {dem_m_elev}")
+            print(f"post adjusted elevation: {dem_adj_elev}")
+
+            # Append dem_m_elev, dem_adj_elev, hydro_id, and gage number to table
+            site_elevations = [gage.site_no, hydro_id, dem_m_elev, dem_adj_elev, min_thal_elev, med_thal_elev, max_thal_elev]
+            gage_data.append(site_elevations)
 
 
     elev_table = pd.DataFrame(gage_data, columns=columns)
