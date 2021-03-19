a69c6657da183f3fdc413fcaf4583c4c99160f2d
TrevorGrout-NOAA <trevor.grout@noaa.gov>
2021-03-19 09:21:48 -0500

    Modify wrapper script

--- tools/generate_categorical_fim.py
+++ tools/generate_categorical_fim.py
@@ -2,40 +2,51 @@
 import subprocess
 import argparse
 import time
+from pathlib import Path
 
 if __name__ == '__main__':
+    
     #Parse arguments
     parser = argparse.ArgumentParser(description = 'Run Categorical FIM')
-    parser.add_argument('-w', '--flow_dir', help = 'Workspace where all flow files are stored.', required = True)
-    parser.add_argument('-u', '--nwm_us_search',  help = 'Walk upstream on NWM network this many miles', required = True)
-    parser.add_argument('-d', '--nwm_ds_search', help = 'Walk downstream on NWM network this many miles', required = True)
-    parser.add_argument('-r','--fim_run_dir',help='Name of directory containing outputs of fim_run.sh',required=True)
-    parser.add_argument('-o', '--output_cat_fim_dir',help='Path to directory where categorical FIM outputs will be written.',required=True, default="")
+    parser.add_argument('-f','--fim_run_dir',help='Name of directory containing outputs of fim_run.sh',required=True)
+    parser.add_argument('-w', '--output_workspace', help = 'Workspace where all flow files are stored.', required = True)
     parser.add_argument('-j','--number_of_jobs',help='Number of processes to use. Default is 1.',required=False, default="1",type=int)
-    parser.add_argument('-depthtif','--write_depth_tiff',help='Using this option will write depth TIFFs.',required=False, action='store_true')
-
     args = vars(parser.parse_args())
     
     #Get arguments
-    flow_dir = args['flow_dir']
-    nwm_us_search = args['nwm_us_search']
-    nwm_ds_search = args['nwm_ds_search']
     fim_run_dir = args['fim_run_dir']
-    output_cat_fim_dir = args['output_cat_fim_dir']
+    output_workspace = args['output_workspace']
     number_of_jobs = args['number_of_jobs']
-    write_depth_tiff = args['write_depth_tiff']
     
-    #Generate CatFIM flow files and then map.
+    ####################################################################
+    #Define default arguments. Modify these if necessary. 
+    #Default values are: 
+        #Upstream and downstream mainstem tracing of 10 miles
+        #Flow files stored in subdirectory "flows"
+        #Mapping files stored in subdirectory "mapping"
+        #Depth grids NOT created
+    nwm_us_search = 10
+    nwm_ds_search = 10        
+    output_flows_dir = str(Path(output_workspace) / 'flows')
+    output_mapping_dir = str(Path(output_workspace) / 'mapping')    
+    write_depth_tiff = False
+    ####################################################################
+    
+    ####################################################################
+    #Run CatFIM scripts in sequence
+    ####################################################################
+    #Generate CatFIM flow files.
     print('Creating flow files')
     start = time.time()
-    subprocess.call(['python3','generate_categorical_fim_flows.py', 'w' , flow_dir, 'u', nwm_us_search, 'd', nwm_ds_search])
+    subprocess.call(['python3','generate_categorical_fim_flows.py', 'w' , output_flows_dir, 'u', nwm_us_search, 'd', nwm_ds_search])
     end = time.time()
     elapsed_time = round((end-start)/60,1)
     print(f'Finished creating flow files in {elapsed_time} minutes')
     
-    print('Beginning mapping')
+    #Generate CatFIM mapping.
+    print('Begin mapping')
     start = time.time()
-    subprocess.call(['python3','generate_categorical_fim_mapping.py', 'r' , fim_run_dir, 's', flow_dir, 'o', output_cat_fim_dir, 'j', number_of_jobs, 'depthtiff', write_depth_tiff]) 
+    subprocess.call(['python3','generate_categorical_fim_mapping.py', 'r' , fim_run_dir, 's', output_flows_dir, 'o', output_mapping_dir, 'j', number_of_jobs, 'depthtiff', write_depth_tiff]) 
     end = time.time()
     elapsed_time = round((end-start)/60,1)
     print(f'Finished mapping in {elapsed_time} minutes')
