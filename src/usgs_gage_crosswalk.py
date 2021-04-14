5a8ebcd1e8c054c0a6e82a724142a78a469bffe2
TrevorGrout-NOAA <69653333+TrevorGrout-NOAA@users.noreply.github.com>
2021-04-14 08:47:58 -0500

    Add mainstems attribute for sierra test
    Sierra test considered all USGS gage locations to be mainstems even though many actually occurred with tributaries. This resulted in unrealistic comparisons as incorrect gages were assigned to mainstems segments. This feature branch identifies gages that are on mainstems via attribute field.
    
    - Modifies usgs_gage_crosswalk.py to filter out gages from the usgs_gages.gpkg layer such that for a "MS" run, only consider gages that contain rating curve information (via curve attribute) and are also mainstems gages (via mainstems attribute).
    - Modifies usgs_gage_crosswalk.py to filter out gages from the usgs_gages.gpkg layer such that for a "FR" run, only consider gages that contain rating curve information (via curve attribute) and are not mainstems gages (via mainstems attribute).
    - Modifies how mainstems segments are determined by using the nwm_flows_ms.gpkg as a lookup to determine if the NWM segment specified by WRDS for a gage site is a mainstems gage.
    - Adds a mainstem attribute field to usgs_gages.gpkg that indicates whether a gage is located on a mainstems river.
    - Adds NWM_FLOWS_MS variable to the .env and .env.template files.
    - Adds the extent argument specified by user when running fim_run.sh to usgs_gage_crosswalk.py.

--- src/usgs_gage_crosswalk.py
+++ src/usgs_gage_crosswalk.py
@@ -32,7 +32,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 '''
 
 
-def crosswalk_usgs_gage(usgs_gages_filename,dem_filename,input_flows_filename,input_catchment_filename,wbd_buffer_filename,dem_adj_filename,output_table_filename):
+def crosswalk_usgs_gage(usgs_gages_filename,dem_filename,input_flows_filename,input_catchment_filename,wbd_buffer_filename,dem_adj_filename,output_table_filename,extent):
 
     wbd_buffer = gpd.read_file(wbd_buffer_filename)
     usgs_gages = gpd.read_file(usgs_gages_filename, mask=wbd_buffer)
@@ -41,8 +41,12 @@ def crosswalk_usgs_gage(usgs_gages_filename,dem_filename,input_flows_filename,in
     input_catchment = gpd.read_file(input_catchment_filename)
     dem_adj = rasterio.open(dem_adj_filename,'r')
 
-    #Query out usgs_gages that don't have rating curve data
-    usgs_gages = usgs_gages.query('curve == "yes"')
+    #MS extent use gages that are mainstem
+    if extent == "MS":
+        usgs_gages = usgs_gages.query('curve == "yes" & mainstem == "yes"')
+    #FR extent use gages that are not mainstem
+    if extent == "FR":
+        usgs_gages = usgs_gages.query('curve == "yes" & mainstem == "no"')
 
     if input_flows.HydroID.dtype != 'int': input_flows.HydroID = input_flows.HydroID.astype(int)
 
@@ -113,7 +117,7 @@ if __name__ == '__main__':
     parser.add_argument('-wbd','--wbd-buffer-filename', help='WBD buffer', required=True)
     parser.add_argument('-dem_adj','--dem-adj-filename', help='Thalweg adjusted DEM', required=True)
     parser.add_argument('-outtable','--output-table-filename', help='Table to append data', required=True)
-
+    parser.add_argument('-e', '--extent', help="extent configuration entered by user when running fim_run.sh", required = True)
     args = vars(parser.parse_args())
 
     usgs_gages_filename = args['usgs_gages_filename']
@@ -123,5 +127,6 @@ if __name__ == '__main__':
     wbd_buffer_filename = args['wbd_buffer_filename']
     dem_adj_filename = args['dem_adj_filename']
     output_table_filename = args['output_table_filename']
+    extent = args['extent']
 
-    crosswalk_usgs_gage(usgs_gages_filename,dem_filename,input_flows_filename,input_catchment_filename,wbd_buffer_filename, dem_adj_filename,output_table_filename)
+    crosswalk_usgs_gage(usgs_gages_filename,dem_filename,input_flows_filename,input_catchment_filename,wbd_buffer_filename, dem_adj_filename,output_table_filename, extent)
