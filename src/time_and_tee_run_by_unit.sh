29bf21b5ecb980ca4fca05e773bc192a412cda3d
Brian Avant <brian.avant@noaa.gov>
2021-06-23 17:08:03 +0000

    fixing bug with mprof plots

--- src/time_and_tee_run_by_unit.sh
+++ src/time_and_tee_run_by_unit.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-mprof run --include-children /usr/bin/time -v $srcDir/run_by_unit.sh $1 |& tee $outputRunDataDir/logs/$1.log
-mprof plot -o $outputRunDataDir/logs/$1_memory
+mprof run -o $1.dat --include-children /usr/bin/time -v $srcDir/run_by_unit.sh $1 |& tee $outputRunDataDir/logs/$1.log 
+mprof plot -o $outputRunDataDir/logs/$1_memory $1.dat
 exit ${PIPESTATUS[0]}
-
