e9ff90bde26dfed0f6bb56e862d2f8147a09b19e
fernando-aristizabal <fernandoa@disroot.org>
2021-12-22 17:44:25 +0000

    Manuscript edits made after feedback from co-authors

--- manuscripts/owp_fim4_2021/docker_run_and_make.sh
+++ manuscripts/owp_fim4_2021/docker_run_and_make.sh
@@ -2,4 +2,4 @@
 
 codeDir=$1
 
-docker run --rm -v "$1":/foss_fim fim4_latex:20210131 /foss_fim/manuscripts/owp_fim4_2021/Makefile.sh
+docker run --rm -v "$1":/foss_fim fernandoaristizabal/latex:20211220 /foss_fim/manuscripts/owp_fim4_2021/Makefile.sh
