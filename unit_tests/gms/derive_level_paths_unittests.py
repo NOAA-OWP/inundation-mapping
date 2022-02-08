e4e836ec60465025659e2582c7207fd01448f08b
Rob Hanna <Robert.Hanna@NOAA.gov>
2022-02-08 20:41:37 +0000

    Updates to add multiple json nodes, a critical fix to method loading of the json data (self.params) as it was loading pointers and not a copy, plus updated examples and documentation.

--- unit_tests/gms/derive_level_paths_unittests.py
+++ unit_tests/gms/derive_level_paths_unittests.py
@@ -34,10 +34,19 @@ class test_Derive_level_paths(unittest.TestCase):
 
     # MUST start with the name of "test_"
     def test_Derive_level_paths_success(self):
+
+        '''
+        This NEEDS be upgraded to check the output, as well as the fact that all of the output files exist as expected.
+        Most of the output test and internal tests with this function will test a wide variety of conditions.
+        For subcalls to other py classes will not exist in this file, but the unittest file for the other python file.
+        Only the basic return output value shoudl be tested to ensure it is as expected.
+        For now, we are adding the very basic "happy path" test.
+        '''
        
+        # makes output readability easier and consistant with other unit tests       
         helpers.print_unit_test_function_header()
         
-        params = self.params
+        params = self.params["valid_data"].copy()
 
         # Function Notes:
         # huc_ids no longer used, so it is not submitted
@@ -54,16 +63,7 @@ class test_Derive_level_paths(unittest.TestCase):
                                                        reach_id_attribute = params["reach_id_attribute"],
                                                        verbose = params["verbose"] )
         
-        
-        '''
-        This NEEDS be upgraded to check the output, as well as the fact that all of the output files exist as expected.
-        Most of the output test and internal tests with this function will test a wide variety of conditions.
-        For subcalls to other py classes will not exist in this file, but the unittest file for the other python file.
-        Only the basic return output value shoudl be tested to ensure it is as expected.
-        For now, we are adding the very basic "happy path" test.
-        '''
-        
-        print(actual)
+        #print(actual)
         
         # Later, we should check here that the files outputed to the file system, exist and are valid and those do not have to be new "test_" methods.
         # We can start using unit test "asserts" as we go as well.
@@ -71,22 +71,20 @@ class test_Derive_level_paths(unittest.TestCase):
         print(f"Test Success: {inspect.currentframe().f_code.co_name}")
         print("*************************************************************")        
 
-
+    
     # Invalid Input stream
     # MUST start with the name of "test_"    
     def test_Derive_level_paths_invalid_input_stream_network(self):
 
-        helpers.print_unit_test_function_header()        
+        # makes output readability easier and consistant with other unit tests 
+        helpers.print_unit_test_function_header()
         
         try:
         
-            params = self.params
-            #with open("/foss_fim/unit_tests/gms/derive_level_paths_params.json") as params_file:
-            #    params = json.load(params_file)
-
-            bad_input_stream_network = "some bad path"
-           
-            actual = derive_level_paths.Derive_level_paths(in_stream_network = bad_input_stream_network,
+            params = self.params["valid_data"].copy()
+            params["in_stream_network"] = "some bad path"
+          
+            actual = derive_level_paths.Derive_level_paths(in_stream_network = params["in_stream_network"],
                                                            out_stream_network = params["out_stream_network"],
                                                            branch_id_attribute = params["branch_id_attribute"],
                                                            out_stream_network_dissolved = params["out_stream_network_dissolved"],
@@ -100,14 +98,15 @@ class test_Derive_level_paths(unittest.TestCase):
                                                            
             raise AssertionError("Fail = excepted a thrown exception but did not get it but was received. Unit Test has 'failed'")
             
-        except Exception as ex:
+        except Exception:
             print()
             print(f"Test Success (failed as expected): {inspect.currentframe().f_code.co_name}")
-            print(ex)
             
         finally:
             print("*************************************************************")             
-        
+       
+       
+       
 if __name__ == '__main__':
 
     print("*****************************")
