5bb56e8cbc7a8382f5f35c8605f54d8e73071084
RyanSpies-NOAA <ryan.spies@noaa.gov>
2022-02-09 17:00:52 +0000

    First commit with usgs_gage_src_adjust call from fim_run - Added new env variables to run usgs_gage_src_adjust

--- fim_run.sh
+++ fim_run.sh
@@ -177,6 +177,12 @@ if [ "$src_vrough_toggle" = "True" ]; then
     time python3 /foss_fim/src/vary_mannings_n_composite.py -fim_dir $outputRunDataDir -mann $vmann_input_file -bc $bankfull_attribute -suff $vrough_suffix -j $jobLimit -plots $src_vrough_plot_option "${args[@]}"
 fi
 
+echo -e $startDiv"Performing SRC adjustments using USGS rating curve database"$stopDiv
+if [ "$src_adjust_usgs" = "True" ]; then
+    # Run SRC Optimization routine using USGS rating curve data (WSE and flow @ NWM recur flow thresholds)
+    time python3 foss_fim/tools/usgs_gage_src_adjust.py -fim_dir $outputRunDataDir -usgs_rc $inputDataDir/usgs_gages/usgs_rating_curves.csv -nwm_recur $nwm_recur_file -j $jobLimit
+fi
+
 echo "$viz"
 if [[ "$viz" -eq 1 ]]; then
     # aggregate outputs
