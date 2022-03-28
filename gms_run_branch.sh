f3a4435a66e0e88d8652f2ac2a838a11255f09ae
Rob Hanna <Robert.Hanna@NOAA.gov>
2022-03-28 09:19:03 -0500

    WIP: fix for unit and branch errors with stream orders 1 and 2 blocked

--- gms_run_branch.sh
+++ gms_run_branch.sh
@@ -136,5 +136,5 @@ python3 $srcDir/usgs_gage_aggregate.py -fim $outputRunDataDir -gms $gms_inputs
 
 # -------------------
 ## GET NON ZERO EXIT CODES ##
-cd $outputRunDataDir/logs/branch
+cd $outputRunDataDir/logs/branches
 find ./ -name "*_branch_*.log" -type f | xargs grep "Exit status: [1-9]" >./non_zero_exit_codes.log
