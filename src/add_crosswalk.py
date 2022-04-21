e5c8f7f06ded2a9eb1b146126480ffab6b973c14
RyanSpies-NOAA <ryan.spies@noaa.gov>
2022-04-21 20:08:24 +0000

    Additional mods to address BARC and vmann variables

--- src/add_crosswalk.py
+++ src/add_crosswalk.py
@@ -222,8 +222,16 @@ def add_crosswalk(input_catchments_fileName,input_flows_fileName,input_srcbase_f
     # make hydroTable
     output_hydro_table = output_src.loc[:,['HydroID','feature_id','NextDownID','order_','Number of Cells','SurfaceArea (m2)','BedArea (m2)','TopWidth (m)','LENGTHKM','AREASQKM','WettedPerimeter (m)','HydraulicRadius (m)','WetArea (m2)','Volume (m3)','SLOPE','ManningN','Stage','Discharge (m3s-1)']]
     output_hydro_table.rename(columns={'Stage' : 'stage','Discharge (m3s-1)':'discharge_cms'},inplace=True)
+    ## Set placeholder variables to be replaced in post-processing (as needed). Create here to ensure consistent column vars
     output_hydro_table['barc_on'] = False # set barc_on attribute to Fasle (default) --> will be overwritten if BARC module runs
+    output_hydro_table['raw_discharge_cms'] = output_src['Discharge (m3s-1)']
+    output_hydro_table['raw_Volume (m3)'] = output_src['Volume (m3)']
+    output_hydro_table['raw_WetArea (m2)'] = output_src['WetArea (m2)']
+    output_hydro_table['raw_HydraulicRadius (m)'] = output_src['HydraulicRadius (m)']
     output_hydro_table['vmann_on'] = False # set vmann_on attribute to Fasle (default) --> will be overwritten if variable roughness module runs
+    output_hydro_table['raw_ManningN'] = output_src['ManningN']
+    output_hydro_table['vmann_discharge_cms'] = pd.NA
+    output_hydro_table['vmann_ManningN'] = pd.NA
     output_hydro_table['adjust_src_on'] = False
     output_hydro_table['last_updated'] = pd.NA
     output_hydro_table['submitter'] = pd.NA
