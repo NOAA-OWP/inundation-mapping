718c9c06e6a01420eccf2a41ad92d542f4fff637
fernando-aristizabal <fernandoa@disroot.org>
2022-09-19 13:33:08 +0000

    Resubmitted to WRR

--- manuscripts/owp_fim4_2022/Makefile.sh
+++ manuscripts/owp_fim4_2022/Makefile.sh
@@ -1,20 +1,33 @@
 #!/bin/bash -e
 
+# declare files
+mainFile=owp_fim4.tex
+bibFile=bibliography/owp_fim4_2022.bib
+expandedFile=owp_fim4_expanded.tex
+differenceFile=owp_fim4_expanded_diff.tex
+originalExandedFile=owp_fim4_expanded_original.tex
+
+# record parent path
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $parent_path
 
-fullFile=owp_fim4.tex
-baseName="${fullFile%%.*}"
-extension="${fullFile#*.}"
-
-pdflatex "$fullFile"
-bibtex "$baseName".aux
-pdflatex "$fullFile"
-pdflatex "$fullFile"
+# compile document
+pdflatex "$mainFile"
+bibtex "${mainFile%%.*}".aux
+pdflatex "$mainFile"
+pdflatex "$mainFile"
 
 # expands all tex files into one file
-echo 'Lat-Expand to Create One Tex File ...'
-latexpand --keep-comments --biber bibliography/owp_fim4_2021.bib owp_fim4.tex -o owp_fim4_expanded.tex
+echo 'Latexpand to Create One Tex File ...'
+latexpand --keep-comments --biber "$bibFile" "$mainFile" -o "$expandedFile"
 
-
+# makes difference file
+if test -f "$originalExandedFile"; then
+    echo 'Latexdiff to make a difference file'
+    latexdiff "$originalExandedFile" "$expandedFile" > "$differenceFile"
+    pdflatex "$differenceFile"
+    bibtex "${differenceFile%%.*}".aux
+    pdflatex "$differenceFile"
+    pdflatex "$differenceFile"
+fi
 
