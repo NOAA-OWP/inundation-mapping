25e9602bb53425960d24a5d561dd906e9645825e
Caleb Oliven <caleb.oliven@nwcal-fim-dev1.nwc.nws.noaa.gov>
2022-09-27 17:05:23 +0000

    correct crs fix

--- tools/test_case_by_hydroid.py
+++ tools/test_case_by_hydroid.py
@@ -114,9 +114,10 @@ def assemble_hydro_alpha_for_single_huc(stats,huc8,mag,bench):
         'predPositive_perc', 'predNegative_perc', 'obsPositive_perc', 'obsNegative_perc', 'positiveDiff_perc',
         'masked_count', 'masked_perc', 'masked_area_km2', 'MAG','BENCH'])
         
-
+        #dict_to_df.round(2)
         concat_list = [in_mem_df, dict_to_df]
         in_mem_df = pd.concat(concat_list, sort=False)
+        
 
 
        
@@ -135,7 +136,7 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--csv',
                         help='Path to folder to hold exported csv file.',
                         required=True)
-    
+    ##Rob Notes
     # Assign variables from arguments.
     args = vars(parser.parse_args())
     benchmark_category = args['benchmark_category']
@@ -143,27 +144,33 @@ if __name__ == "__main__":
     csv = args['csv']
     
     # Execution code
-    csv_output = pd.DataFrame(columns=['HydroID', 'huc8', 'true_negatives_count', 'false_negatives_count', 'true_positives_count',
+    
+
+    csv_output = gpd.GeoDataFrame(columns=['HydroID', 'huc8', 'true_negatives_count', 'false_negatives_count', 'true_positives_count',
         'false_positives_count', 'contingency_tot_count', 'cell_area_m2', 'TP_area_km2', 'FP_area_km2', 'TN_area_km2',
         'FN_area_km2', 'contingency_tot_area_km2', 'predPositive_area_km2', 'predNegative_area_km2', 'obsPositive_area_km2',
         'obsNegative_area_km2', 'positiveDiff_area_km2', 'CSI', 'FAR', 'TPR', 'TNR', 'PND', 'PPV', 'NPV', 'ACC', 'Bal_ACC',
         'MCC', 'EQUITABLE_THREAT_SCORE', 'PREVALENCE', 'BIAS', 'F1_SCORE', 'TP_perc', 'FP_perc', 'TN_perc', 'FN_perc',
         'predPositive_perc', 'predNegative_perc', 'obsPositive_perc', 'obsNegative_perc', 'positiveDiff_perc',
-        'masked_count', 'masked_perc', 'masked_area_km2', 'MAG','BENCH','geometry'])
+        'masked_count', 'masked_perc', 'masked_area_km2', 'MAG','BENCH','geometry'], geometry = 'geometry')
     # This funtion, relies on the test_case class defined in run_test_case.py to list all available test cases
+    print('listing_test_cases')
     all_test_cases = test_case.list_all_test_cases(version=version, archive=True, benchmark_categories=[] if benchmark_category == "all" else [benchmark_category])
 
     error_list =[]
     error_count=0
     success_count=0
-
+    counter = 0
     for test_case_class in all_test_cases:
 
         if not os.path.exists(test_case_class.fim_dir):
-            print(f'{test_case_class.fim_dir} does not exist_bilbo_baggins')
+            print(f'{test_case_class.fim_dir} does not exist')
             continue
         else:
             print(test_case_class.test_id)
+            counter = counter + 1
+            if counter >=2:
+                break
             #print(test_case_class.test_id, end='\r')
 
             # Define the catchment geopackage that contains the hydroid and geometry of each catchment. 
@@ -172,34 +179,52 @@ if __name__ == "__main__":
             agreement_dict = test_case_class.get_current_agreements()
             for mag in agreement_dict:
                 for agree_rast in agreement_dict[mag]:
-                    
+                    print('performing_zonal_stats')
                     stats = perform_zonal_stats(huc_gpkg,agree_rast)
                     
-                    in_mem_df = assemble_hydro_alpha_for_single_huc(stats, test_case_class.huc, mag, test_case_class.benchmark_cat)
+                    print('assembling_hydroalpha_for_single_huc')
                     get_geom = gpd.read_file(huc_gpkg)
-                    print(get_geom)
-                    hydro_geom_df = get_geom[["HydroID", "geometry"]]
-                    geom_output = pd.merge(in_mem_df, hydro_geom_df, how = 'inner', on = 'HydroID')
+                    print(get_geom.crs)
                     
+                    in_mem_df = assemble_hydro_alpha_for_single_huc(stats, test_case_class.huc, mag, test_case_class.benchmark_cat)
+                    #get_geom = gpd.read_file(huc_gpkg)
+                    #print(get_geom)
+                    hydro_geom_df = get_geom[["HydroID", "geometry"]]
+                    #geom_output = pd.merge(in_mem_df, hydro_geom_df, how = 'inner', on = 'HydroID')
+                    
+
+                    print('filtering_rows')
+                    #in_mem_df = in_mem_df.convert_dtypes()
+                    #try find and reaplace with NAN then use dropna???might be beter way to do
+                    in_mem_df = in_mem_df[(in_mem_df['CSI']!= "NA") & (in_mem_df['FAR']!= "NA") & (in_mem_df['TPR']!= "NA") 
+                    & (in_mem_df['TNR']!= "NA") & (in_mem_df['PND']!= "NA") & (in_mem_df['PPV']!= "NA")
+                    & (in_mem_df['NPV']!= "NA") & (in_mem_df['ACC']!= "NA") & (in_mem_df['Bal_ACC']!= "NA")
+                    & (in_mem_df['MCC']!= "NA") & (in_mem_df['EQUITABLE_THREAT_SCORE']!= "NA")
+                    & (in_mem_df['PREVALENCE']!= "NA") & (in_mem_df['BIAS']!= "NA") & (in_mem_df['F1_SCORE']!= "NA")
+                    & (in_mem_df['TP_perc']!= "NA") & (in_mem_df['FP_perc']!= "NA") & (in_mem_df['TN_perc']!= "NA")
+                    & (in_mem_df['FN_perc']!= "NA") & (in_mem_df['predPositive_perc']!= "NA")
+                    & (in_mem_df['predNegative_perc']!= "NA") & (in_mem_df['obsPositive_perc']!= "NA")
+                    & (in_mem_df['obsNegative_perc']!= "NA") & (in_mem_df['positiveDiff_perc']!= "NA")]
+                    
+                    geom_output = hydro_geom_df.merge(in_mem_df, on='HydroID')
+                    print('merging_to_output_df')
+                    print(geom_output.crs)
+                    #geom_output = geom_output.convert_dtypes()
                     
                     concat_df_list = [geom_output, csv_output]
-                    print(csv_output.dtypes)
+                    
                     csv_output = pd.concat(concat_df_list, sort=False)
-                
+                    print(csv_output.crs)
+                    print(type(csv_output))
+                    
                
-    csv_output = csv_output.convert_dtypes()
-    csv_output = csv_output[(csv_output['CSI']!= "NA") & (csv_output['FAR']!= "NA") & (csv_output['TPR']!= "NA") 
-                    & (csv_output['TNR']!= "NA") & (csv_output['PND']!= "NA") & (csv_output['PPV']!= "NA")
-                    & (csv_output['NPV']!= "NA") & (csv_output['ACC']!= "NA") & (csv_output['Bal_ACC']!= "NA")
-                    & (csv_output['MCC']!= "NA") & (csv_output['EQUITABLE_THREAT_SCORE']!= "NA")
-                    & (csv_output['PREVALENCE']!= "NA") & (csv_output['BIAS']!= "NA") & (csv_output['F1_SCORE']!= "NA")
-                    & (csv_output['TP_perc']!= "NA") & (csv_output['FP_perc']!= "NA") & (csv_output['TN_perc']!= "NA")
-                    & (csv_output['FN_perc']!= "NA") & (csv_output['predPositive_perc']!= "NA")
-                    & (csv_output['predNegative_perc']!= "NA") & (csv_output['obsPositive_perc']!= "NA")
-                    & (csv_output['obsNegative_perc']!= "NA") & (csv_output['positiveDiff_perc']!= "NA")]
+
+
     
-    print(csv_output.dtypes)
-    csv_output.to_csv(csv, index=False, chunksize=1000)
+    print('writing_to_gpkg')
+    csv_output.to_file(csv, layer='alpha', driver="GPKG") # encoding='utf-8'
+    #csv_output.to_csv(csv, index=False, chunksize=1000)
+    
 
 
 
