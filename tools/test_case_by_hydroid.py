61062ff5320ad4c1d51714d7169c1b0f0353525b
Caleb Oliven <caleb.oliven@nwcal-fim-dev1.nwc.nws.noaa.gov>
2022-09-28 20:39:10 +0000

    functional with requests

--- tools/test_case_by_hydroid.py
+++ tools/test_case_by_hydroid.py
@@ -46,48 +46,48 @@ def assemble_hydro_alpha_for_single_huc(stats,huc8,mag,bench):
         stats_dictionary['HydroID'] = hydroid
                
         
-        true_negatives_count = stats_dictionary['true_negatives_count']
-        false_negatives_count = stats_dictionary['false_negatives_count']
-        true_positives_count = stats_dictionary['true_positives_count']
-        false_positives_count = stats_dictionary['false_positives_count']
-        contingency_tot_count = stats_dictionary['contingency_tot_count']
-        cell_area_m2 = stats_dictionary['cell_area_m2']
-        TP_area_km2 = stats_dictionary['TP_area_km2']
-        FP_area_km2 = stats_dictionary['FP_area_km2']
-        TN_area_km2 = stats_dictionary['TN_area_km2']
-        FN_area_km2 = stats_dictionary['FN_area_km2']
-        contingency_tot_area_km2 = stats_dictionary['contingency_tot_area_km2']
-        predPositive_area_km2 = stats_dictionary['predPositive_area_km2']
-        predNegative_area_km2 = stats_dictionary['predNegative_area_km2']
-        obsPositive_area_km2 = stats_dictionary['obsPositive_area_km2']
-        obsNegative_area_km2 = stats_dictionary['obsNegative_area_km2']
-        positiveDiff_area_km2 = stats_dictionary['positiveDiff_area_km2']
-        CSI = stats_dictionary['CSI']
-        FAR = stats_dictionary['FAR']
-        TPR = stats_dictionary['TPR']
-        TNR = stats_dictionary['TNR']
-        PND = stats_dictionary['PND']
-        PPV = stats_dictionary['PPV']
-        NPV = stats_dictionary['NPV']
-        ACC = stats_dictionary['ACC'] 
-        Bal_ACC = stats_dictionary['Bal_ACC']
-        MCC = stats_dictionary['MCC']
-        EQUITABLE_THREAT_SCORE = stats_dictionary['EQUITABLE_THREAT_SCORE']
-        PREVALENCE = stats_dictionary['PREVALENCE']
-        BIAS = stats_dictionary['BIAS']
-        F1_SCORE = stats_dictionary['F1_SCORE']
-        TP_perc = stats_dictionary['TP_perc']
-        FP_perc = stats_dictionary['FP_perc']
-        TN_perc = stats_dictionary['TN_perc']
-        FN_perc = stats_dictionary['FN_perc']
-        predPositive_perc = stats_dictionary['predPositive_perc'] 
-        predNegative_perc = stats_dictionary['predNegative_perc']
-        obsPositive_perc = stats_dictionary['obsPositive_perc']
-        obsNegative_perc = stats_dictionary['obsNegative_perc']
-        positiveDiff_perc = stats_dictionary['positiveDiff_perc']
-        masked_count = stats_dictionary['masked_count']
-        masked_perc = stats_dictionary['masked_perc']
-        masked_area_km2 = stats_dictionary['masked_area_km2']
+        true_negatives_count = round(float(stats_dictionary['true_negatives_count']),2)
+        false_negatives_count = round(float(stats_dictionary['false_negatives_count']),2)
+        true_positives_count = round(float(stats_dictionary['true_positives_count']),2)
+        false_positives_count = round(float(stats_dictionary['false_positives_count']),2)
+        contingency_tot_count = round(float(stats_dictionary['contingency_tot_count']),2)
+        cell_area_m2 = round(float(stats_dictionary['cell_area_m2']),2)
+        TP_area_km2 = round(float(stats_dictionary['TP_area_km2']),2)
+        FP_area_km2 = round(float(stats_dictionary['FP_area_km2']),2)
+        TN_area_km2 = round(float(stats_dictionary['TN_area_km2']),2)
+        FN_area_km2 = round(float(stats_dictionary['FN_area_km2']),2)
+        contingency_tot_area_km2 = round(float(stats_dictionary['contingency_tot_area_km2']),2)
+        predPositive_area_km2 = round(float(stats_dictionary['predPositive_area_km2']),2)
+        predNegative_area_km2 = round(float(stats_dictionary['predNegative_area_km2']),2)
+        obsPositive_area_km2 = round(float(stats_dictionary['obsPositive_area_km2']),2)
+        obsNegative_area_km2 = round(float(stats_dictionary['obsNegative_area_km2']),2)
+        positiveDiff_area_km2 = round(float(stats_dictionary['positiveDiff_area_km2']),2)
+        CSI = round(float(stats_dictionary['CSI']),2)
+        FAR = round(float(stats_dictionary['FAR']),2)
+        TPR = round(float(stats_dictionary['TPR']),2)
+        TNR = round(float(stats_dictionary['TNR']),2)
+        PND = round(float(stats_dictionary['PND']),2)
+        PPV = round(float(stats_dictionary['PPV']),2)
+        NPV = round(float(stats_dictionary['NPV']),2)
+        ACC = round(float(stats_dictionary['ACC']),2)
+        Bal_ACC = round(float(stats_dictionary['Bal_ACC']),2)
+        MCC = round(float(stats_dictionary['MCC']),2)
+        EQUITABLE_THREAT_SCORE = round(float(stats_dictionary['EQUITABLE_THREAT_SCORE']),2)
+        PREVALENCE = round(float(stats_dictionary['PREVALENCE']),2)
+        BIAS = round(float(stats_dictionary['BIAS']),2)
+        F1_SCORE = round(float(stats_dictionary['F1_SCORE']),2)
+        TP_perc = round(float(stats_dictionary['TP_perc']),2)
+        FP_perc = round(float(stats_dictionary['FP_perc']),2)
+        TN_perc = round(float(stats_dictionary['TN_perc']),2)
+        FN_perc = round(float(stats_dictionary['FN_perc']),2)
+        predPositive_perc = round(float(stats_dictionary['predPositive_perc']),2) 
+        predNegative_perc = round(float(stats_dictionary['predNegative_perc']),2)
+        obsPositive_perc = round(float(stats_dictionary['obsPositive_perc']),2)
+        obsNegative_perc = round(float(stats_dictionary['obsNegative_perc']),2)
+        positiveDiff_perc = round(float(stats_dictionary['positiveDiff_perc']),2)
+        masked_count = round(float(stats_dictionary['masked_count']),2)
+        masked_perc = round(float(stats_dictionary['masked_perc']),2)
+        masked_area_km2 = round(float(stats_dictionary['masked_area_km2']),2)
         HydroID = stats_dictionary['HydroID']
 
 
@@ -168,9 +168,9 @@ if __name__ == "__main__":
             continue
         else:
             print(test_case_class.test_id)
-            counter = counter + 1
-            if counter >=2:
-                break
+            # counter = counter + 1
+            # if counter >=2:
+            #     break
             #print(test_case_class.test_id, end='\r')
 
             # Define the catchment geopackage that contains the hydroid and geometry of each catchment. 
@@ -197,16 +197,21 @@ if __name__ == "__main__":
                     #in_mem_df = in_mem_df.convert_dtypes()
                     #try find and reaplace with NAN then use dropna???might be beter way to do
                     in_mem_df = in_mem_df[(in_mem_df['CSI']!= "NA") & (in_mem_df['FAR']!= "NA") & (in_mem_df['TPR']!= "NA") 
-                    & (in_mem_df['TNR']!= "NA") & (in_mem_df['PND']!= "NA") & (in_mem_df['PPV']!= "NA")
-                    & (in_mem_df['NPV']!= "NA") & (in_mem_df['ACC']!= "NA") & (in_mem_df['Bal_ACC']!= "NA")
-                    & (in_mem_df['MCC']!= "NA") & (in_mem_df['EQUITABLE_THREAT_SCORE']!= "NA")
-                    & (in_mem_df['PREVALENCE']!= "NA") & (in_mem_df['BIAS']!= "NA") & (in_mem_df['F1_SCORE']!= "NA")
-                    & (in_mem_df['TP_perc']!= "NA") & (in_mem_df['FP_perc']!= "NA") & (in_mem_df['TN_perc']!= "NA")
-                    & (in_mem_df['FN_perc']!= "NA") & (in_mem_df['predPositive_perc']!= "NA")
-                    & (in_mem_df['predNegative_perc']!= "NA") & (in_mem_df['obsPositive_perc']!= "NA")
-                    & (in_mem_df['obsNegative_perc']!= "NA") & (in_mem_df['positiveDiff_perc']!= "NA")]
+                    & (in_mem_df['TNR']!= "NA") & (in_mem_df['PND']!= "NA")]
                     
-                    geom_output = hydro_geom_df.merge(in_mem_df, on='HydroID')
+
+                    # & (in_mem_df['PPV']!= "NA")
+                    # & (in_mem_df['NPV']!= "NA") & (in_mem_df['ACC']!= "NA") & (in_mem_df['Bal_ACC']!= "NA")
+                    # & (in_mem_df['MCC']!= "NA") & (in_mem_df['EQUITABLE_THREAT_SCORE']!= "NA")
+                    # & (in_mem_df['PREVALENCE']!= "NA") & (in_mem_df['BIAS']!= "NA") & (in_mem_df['F1_SCORE']!= "NA")
+                    # & (in_mem_df['TP_perc']!= "NA") & (in_mem_df['FP_perc']!= "NA") & (in_mem_df['TN_perc']!= "NA")
+                    # & (in_mem_df['FN_perc']!= "NA") & (in_mem_df['predPositive_perc']!= "NA")
+                    # & (in_mem_df['predNegative_perc']!= "NA") & (in_mem_df['obsPositive_perc']!= "NA")
+                    # & (in_mem_df['obsNegative_perc']!= "NA") & (in_mem_df['positiveDiff_perc']!= "NA")
+
+
+
+                    geom_output = hydro_geom_df.merge(in_mem_df, on='HydroID', how ='inner')
                     print('merging_to_output_df')
                     print(geom_output.crs)
                     #geom_output = geom_output.convert_dtypes()
@@ -214,6 +219,8 @@ if __name__ == "__main__":
                     concat_df_list = [geom_output, csv_output]
                     
                     csv_output = pd.concat(concat_df_list, sort=False)
+                    print(csv_output)
+                    csv_output = csv_output[(csv_output['CSI']!= "NA")]
                     print(csv_output.crs)
                     print(type(csv_output))
                     
@@ -222,8 +229,13 @@ if __name__ == "__main__":
 
     
     print('writing_to_gpkg')
-    csv_output.to_file(csv, layer='alpha', driver="GPKG") # encoding='utf-8'
-    #csv_output.to_csv(csv, index=False, chunksize=1000)
+
+    #tmp_path = "/data/temp/caleb/test_data/ble_test_geom_error.gpkg"
+    csv_output.to_file(csv, driver="GPKG")
+
+    # print('writing_to_csv')
+    # tmp_path2="/data/temp/caleb/test_data/ble_test_geom_error.csv"
+    # csv_output.to_csv(csv, index=False, chunksize=1000, encoding='utf-8')
     
 
 
