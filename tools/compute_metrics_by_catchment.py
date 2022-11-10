d48d90a699ab2d2488582dd9b224d81ec7da7beb
fernando-aristizabal <fernandoa@disroot.org>
2022-11-10 14:50:44 +0000

    almost done computing all secondary metrics

--- tools/compute_metrics_by_catchment.py
+++ tools/compute_metrics_by_catchment.py
@@ -3,17 +3,27 @@
 import os
 from itertools import product
 from time import time
+import warnings
 
 import numpy as np
 import geopandas as gpd
+import pandas as pd
 import rioxarray as rxr
 import xarray as xr
 from geocube.api.core import make_geocube
 from tqdm import tqdm
 from xrspatial.zonal import stats, crosstab
+from dask.dataframe.multi import concat
+from tqdm.dask import TqdmCallback
 #from memory_profiler import profile
 
 from foss_fim.tools.tools_shared_functions import csi, tpr, far, mcc
+
+"""
+- Primary and Secondary metrics can be object inhereting from pd/dask df
+- Functions and variables here can be methods and attributes, respectively, of those objects
+"""
+
 
 # variable declarations
 root_dir = os.path.abspath(os.sep)
@@ -31,35 +41,60 @@ def build_agreement_raster_data_dir(huc, resolution, year):
 #resolutions = [3,5,10,15,20]
 resolutions = [20]
 years = [100,500]
-hucs = ['12020001','12020002','12020003','12020004','12020005','12020006','12020007']
+#hucs = ['12020001','12020002','12020003','12020004','12020005','12020006','12020007']
+hucs = ['12020001']
 chunk_size = 256*4
 
 # metrics dict
 metrics_dict = { 'csi': csi, 'tpr': tpr, 'far': far, 'mcc': mcc } 
 
-# convert to primary metrics
-def compute_primary_metrics(arr):
-    
-    agreement_encoding_digits_to_names = { 0: "TN",
-                                           1: "FN",
-                                           2: "FP",
-                                           3: "TP",
-                                           4: "Water body"
-                                          }
-    
-    unique, counts = np.unique(arr['agreement'].values,
-                               return_counts=True)
 
-    # change unique to string then convert to dict
-    unique = zip( ((agreement_encoding_digits_to_names[u] , c) for u,c in zip(unique,counts)) )
+# changing numbers to str encodings
+agreement_encoding_digits_to_names = { 0: "TN", 1: "FN",
+                                       2: "FP", 3: "TP",
+                                       4: "Waterbody"
+                                     }
 
-    # make dict
-    #primary_metrics = dict(zip(unique,counts))
+### getting this warning when receiving catchments with no samples ###
+# /foss_fim/tools/tools_shared_functions.py:1508: RuntimeWarning: invalid value encountered in long_scalars
+# maybe do some sort of check within metrics to verify prior to computing. Retrun NaN or None otherwise?
+
+# meta for dask df apply
+#meta = ( 'CSI' , 'f8' )
+#meta = [('CSI' , 'f8'),('TPR' , 'f8'),('FAR' , 'f8'),('MCC' , 'f8')]
+
+# metrics
+def make_input_dict(row):
+    """ make dictionary of primary metrics from row object when using pd/dask df apply """
+    return {'TP' : row.TP, 'TN' : row.TN, 'FP' : row.FP, 'FN' : row.FN}
+
+
+def compute_secondary_metrics_on_df(func):
+    """ decorator function to compute secondary metrics when using pd/dask df apply """
     
-    # pop water body
-    primary_metrics.pop("Water body")
+    def wrapper(row):
+        
+        # makes input dict from row object when using pd/dask df apply
+        input_dict = make_input_dict(row)
+        
+        # ignore warnings should be it's own decorator??? 
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore",category='RuntimeWarning')
+            metric = func(**input_dict)
+        
+        return metric
+    
+    return wrapper
 
-    return primary_metrics
+def all_metrics(TP,FP,FN,TN=None):
+    
+    return { 'CSI' : csi(TP,FP,FN,TN),
+             'TPR' : tpr(TP,FP,FN,TN),
+             'FAR' : far(TP,FP,FN,TN),
+             'MCC' : mcc(TP,FP,FN,TN) }
+
+# wrap functions to work with rows
+calc_all_metrics = compute_secondary_metrics_on_df(all_metrics)
 
 
 def compute_metrics_by_catchment( nwm_catchments_fn, 
@@ -68,10 +103,16 @@ def compute_metrics_by_catchment( nwm_catchments_fn,
     # load catchments
     nwm_catchments = gpd.read_file(nwm_catchments_fn)
 
-    # loop over every combination of resolutions and magnitude years
-    prev_h, prev_r = [None] * 2
+    # prepare combinations of resolutions, hucs, and years
     combos = list(product(resolutions,hucs,years))
-    for r,h,y in tqdm(combos,desc='Agreement By Catchment'):
+    num_of_combos = len(combos)
+    
+    # prepare outputs
+    list_of_secondary_metrics_df = [None] * num_of_combos
+    
+    print('Agreement By Catchment')
+    # loop over every combination of resolutions and magnitude years
+    for idx,(r,h,y) in tqdm(enumerate(combos),desc='Rasterizing Catchments & Crosstabbing',total=num_of_combos):
         
         # load agreement raster
         agreement_raster_fn = build_agreement_raster_data_dir(h,r,y)
@@ -85,34 +126,42 @@ def compute_metrics_by_catchment( nwm_catchments_fn,
                                              lock=False
                                             ).sel(band=1,drop=True) 
 
-        # avoid recomputing nwm_catchments_xr for same magnitude
-        if (h != prev_h) & (r != prev_r):
-            
-            # making xarray from catchment vectors
-            print(f"Rasterizing NWM catchments for {h} at {r}m ...")
-            nwm_catchments_xr = make_geocube(nwm_catchments,['ID'],
-                                             like=agreement_raster,
-                                             fill=np.nan) \
-                                                .to_array(dim='band', name='nwm_catchments') \
-                                                 .sel(band='ID') \
-                                                 .drop('band') \
-                                                 .chunk(agreement_raster.chunksizes)
-        
-        # assign previous h and r
-        prev_h, prev_r = h, r
-
+        # making xarray from catchment vectors
+        nwm_catchments_xr = make_geocube(nwm_catchments,['ID'],
+                                         like=agreement_raster,
+                                         fill=np.nan) \
+                                            .to_array(dim='band', name='nwm_catchments') \
+                                             .sel(band='ID') \
+                                             .drop('band') \
+                                             .chunk(agreement_raster.chunksizes)
+    
         # compute cross tabulation table for ct_dask_df
-        ct_dask_df = crosstab(nwm_catchments_xr,agreement_raster,nodata_values=np.nan)
-        #ct_pd_df = ct_dask_df.compute()
+        ct_dask_df = crosstab(nwm_catchments_xr,agreement_raster,nodata_values=np.nan) \
+                                    .rename(columns=agreement_encoding_digits_to_names) \
+                                    .astype(np.int64) \
+                                    .set_index('zone', drop=True, npartitions='auto') # set index on zone
 
-        ## NEXT: CONVERT CT INDICES TO STR OF PRIMARY METRICS AND COMPUTE SECONDARY METRICS PER ZONE WITH APPLY FUNCTIONALITY
-        breakpoint()
+        #### calculate metrics ###
+        # provides framework for computed secondary metrics df
+        meta = pd.DataFrame(columns=('CSI' ,'TPR', 'FAR', 'MCC'), dtype='f8')
+        
+        # applies function to calc secondary metrics across rows. Uses expand to accomodate multiple columns
+        secondary_metrics_df = ct_dask_df.apply(calc_all_metrics,axis=1, meta=meta, result_type='expand') \
+                                         .dropna(how='all')
+        
+        # add categoricals: resolution, year, huc
 
+        # aggregate to list
+        list_of_secondary_metrics_df[idx] = secondary_metrics_df  
+        
+    # concat list of secondary metrics df
+    secondary_metrics_dask_df = concat(list_of_secondary_metrics_df)
+    
+    # compute
+    with TqdmCallback(desc='Computing Metrics'):
+        secondary_metrics_pd_df = secondary_metrics_dask_df.compute()
 
 if __name__ == '__main__':
     
-    # dask client
-    #cluster = LocalCluster()
-    #client = Client(cluster)
-
+    # computes 4 secondary contingency metrics by nwm catchment for year, huc, and resolution
     compute_metrics_by_catchment(nwm_catchments_fn,resolutions,years,hucs, chunk_size)
