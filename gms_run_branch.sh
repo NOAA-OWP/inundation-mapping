5b4dff0b6f10db63fe34ab1b0ea8f0981fa3a743
RyanSpies-NOAA <ryan.spies@noaa.gov>
2022-12-02 11:09:58 -0600

    v4.0.12.0 Channel/overbank subdivision framework (#736)

--- gms_run_branch.sh
+++ gms_run_branch.sh
@@ -204,40 +204,6 @@ fi
 
 ## Track total time of the overall run
 T_total_start
-Tstart
-
-: '
-This makes the local variables from the calb_db_keys files
-into global variables that can be used in other files, including python.
-
-Why not just leave the word export in front of each of the keys in the
-calb_db_keys.env? Becuase that file is used against docker-compose
-when we start up that part of the sytem and it does not like the word
-export.
-'
-export CALIBRATION_DB_HOST=$CALIBRATION_DB_HOST
-export CALIBRATION_DB_NAME=$CALIBRATION_DB_NAME
-export CALIBRATION_DB_USER_NAME=$CALIBRATION_DB_USER_NAME
-export CALIBRATION_DB_PASS=$CALIBRATION_DB_PASS
-
-## CONNECT TO CALIBRATION POSTGRESQL DATABASE (OPTIONAL) ##
-if [ "$src_adjust_spatial" = "True" ]; then
-    if [ ! -f $CALB_DB_KEYS_FILE ]; then
-        echo "ERROR! - the src_adjust_spatial parameter in the params_template.env (or equiv) is set to "True" (see parameter file), but the provided calibration database access keys file does not exist: $CALB_DB_KEYS_FILE"
-        exit 1
-    else
-        source $CALB_DB_KEYS_FILE
-        echo "Populate PostgrSQL database with benchmark FIM extent points and HUC attributes (the calibration database)"
-        echo "Loading HUC Data"
-        time ogr2ogr -overwrite -nln hucs -a_srs ESRI:102039 -f PostgreSQL PG:"host=$CALIBRATION_DB_HOST dbname=$CALIBRATION_DB_NAME user=$CALIBRATION_DB_USER_NAME password=$CALIBRATION_DB_PASS" $inputDataDir/wbd/WBD_National.gpkg WBDHU8
-        echo "Loading Point Data"
-        time ogr2ogr -overwrite -f PostgreSQL PG:"host=$CALIBRATION_DB_HOST dbname=$CALIBRATION_DB_NAME user=$CALIBRATION_DB_USER_NAME password=$CALIBRATION_DB_PASS" $fim_obs_pnt_data usgs_nws_benchmark_points -nln points
-    fi
-fi
-
-Tcount
-Tstart
-date -u
 
 ## RUN GMS BY BRANCH ##
 echo "=========================================================================="
@@ -262,8 +228,54 @@ echo
 echo "Processing usgs gage aggregation"
 python3 $srcDir/usgs_gage_aggregate.py -fim $outputRunDataDir -gms $gms_inputs
 
+## RUN SYNTHETIC RATING CURVE BANKFULL ESTIMATION ROUTINE ##
+if [ "$src_bankfull_toggle" = "True" ]; then
+    echo -e $startDiv"Estimating bankfull stage in SRCs"$stopDiv
+    # Run SRC bankfull estimation routine routine
+    Tstart
+    time python3 /foss_fim/src/identify_src_bankfull.py -fim_dir $outputRunDataDir -flows $bankfull_flows_file -j $jobLimit
+    Tcount
+fi
+
+## RUN SYNTHETIC RATING SUBDIVISION ROUTINE ##
+if [ "$src_subdiv_toggle" = "True" ]; then
+    echo -e $startDiv"Performing SRC channel/overbank subdivision routine"$stopDiv
+    # Run SRC Subdivision & Variable Roughness routine
+    Tstart
+    time python3 /foss_fim/src/subdiv_chan_obank_src.py -fim_dir $outputRunDataDir -mann $vmann_input_file -j $jobLimit
+    Tcount
+fi
+
+## CONNECT TO CALIBRATION POSTGRESQL DATABASE (OPTIONAL) ##
+if [ "$src_adjust_spatial" = "True" ]; then
+    if [ ! -f $CALB_DB_KEYS_FILE ]; then
+        echo "ERROR! - the src_adjust_spatial parameter in the params_template.env (or equiv) is set to "True" (see parameter file), but the provided calibration database access keys file does not exist: $CALB_DB_KEYS_FILE"
+        exit 1
+    else
+        source $CALB_DB_KEYS_FILE
+        : '
+        This makes the local variables from the calb_db_keys files
+        into global variables that can be used in other files, including python.
+
+        Why not just leave the word export in front of each of the keys in the
+        calb_db_keys.env? Becuase that file is used against docker-compose
+        when we start up that part of the sytem and it does not like the word
+        export.
+        '
+        export CALIBRATION_DB_HOST=$CALIBRATION_DB_HOST
+        export CALIBRATION_DB_NAME=$CALIBRATION_DB_NAME
+        export CALIBRATION_DB_USER_NAME=$CALIBRATION_DB_USER_NAME
+        export CALIBRATION_DB_PASS=$CALIBRATION_DB_PASS
+        echo "Populate PostgrSQL database with benchmark FIM extent points and HUC attributes (the calibration database)"
+        echo "Loading HUC Data"
+        time ogr2ogr -overwrite -nln hucs -a_srs ESRI:102039 -f PostgreSQL PG:"host=$CALIBRATION_DB_HOST dbname=$CALIBRATION_DB_NAME user=$CALIBRATION_DB_USER_NAME password=$CALIBRATION_DB_PASS" $inputDataDir/wbd/WBD_National.gpkg WBDHU8
+        echo "Loading Point Data"
+        time ogr2ogr -overwrite -f PostgreSQL PG:"host=$CALIBRATION_DB_HOST dbname=$CALIBRATION_DB_NAME user=$CALIBRATION_DB_USER_NAME password=$CALIBRATION_DB_PASS" $fim_obs_pnt_data usgs_nws_benchmark_points -nln points
+    fi
+fi
+
 ## RUN SYNTHETIC RATING CURVE CALIBRATION W/ USGS GAGE RATING CURVES ##
-if [ "$src_adjust_usgs" = "True" ]; then
+if [ "$src_adjust_usgs" = "True" ] && [ "$src_subdiv_toggle" = "True" ]; then
     Tstart
     echo -e $startDiv"Performing SRC adjustments using USGS rating curve database"$stopDiv
     # Run SRC Optimization routine using USGS rating curve data (WSE and flow @ NWM recur flow thresholds)
@@ -273,7 +285,7 @@ if [ "$src_adjust_usgs" = "True" ]; then
 fi
 
 ## RUN SYNTHETIC RATING CURVE CALIBRATION W/ BENCHMARK POINT DATABASE (POSTGRESQL) ##
-if [ "$src_adjust_spatial" = "True" ]; then
+if [ "$src_adjust_spatial" = "True" ] && [ "$src_subdiv_toggle" = "True" ]; then
     Tstart
     echo -e $startDiv"Performing SRC adjustments using benchmark point database"$stopDiv
     python3 $srcDir/src_adjust_spatial_obs.py -fim_dir $outputRunDataDir -j $jobLimit
