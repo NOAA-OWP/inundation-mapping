d59a0f9b92342d0c88f0d3ba02a9c34818f493cc
MattLuck-NOAA <matt.luck@noaa.gov>
2023-01-18 08:02:23 -0500

    Additional cleanup

--- gms_run_branch.sh
+++ gms_run_branch.sh
@@ -138,7 +138,6 @@ export outputRunDataDir=$outputDataDir/$runName
 export deny_branches_list=$deny_branches_list
 logFile=$outputRunDataDir/logs/branch/summary_gms_branch.log
 export overwrite=$overwrite
-export extent=GMS
 
 ## Check for run data directory and the file. If gms_run_unit failed, the file will not be there ##
 if [ ! -f "$outputRunDataDir/gms_inputs.csv" ]; then 
