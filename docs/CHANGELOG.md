6b933f7e4665600bc3d91d2f944c9b26c58de123
MattLuck-NOAA <matt.luck@noaa.gov>
2023-01-20 12:20:41 -0500

    v4.0.19.2 Consolidate bash environment variables into a single file (#797)

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -1,6 +1,34 @@
 All notable changes to this project will be documented in this file.
 We follow the [Semantic Versioning 2.0.0](http://semver.org/) format.
 
+## v4.0.19.2 - 2023-01-17 - [PR#797](https://github.com/NOAA-OWP/inundation-mapping/pull/797)
+
+Consolidates global bash environment variables into a new `src/bash_variables.env` file. Additionally, Python environment variables have been moved into this file and `src/utils/shared_variables.py` now references this file. Hardcoded projections have been replaced by an environment variable. This also replaces the Manning's N file in `config/params_template.env` with a constant and updates relevant code. Unused environment variables have been removed.
+
+### Additions
+
+- `src/bash_variables.env`: Adds file for global environment variables
+
+### Removals
+
+- `config/`
+    - `mannings_default.json`
+    - `mannings_default_calibrated.json`
+
+### Changes
+
+- `config/params_template.env`: Changes manning_n from filename to default value of 0.06
+- `gms_run_branch.sh`: Adds `bash_variables.env`
+- `gms_run_post_processing.sh`: Adds `bash_variables.env` and changes projection from hardcoded to environment variable
+- `gms_run_unit.sh`: Adds `bash_variables.env`
+- `src/`
+    - `add_crosswalk.py`: Assigns default manning_n value and removes assignments by stream orders
+    - `aggregate_vector_inputs.py`: Removes unused references to environment variables and function
+    - `gms/run_by_unit.sh`: Removes environment variable assignments and uses projection from environment variables
+    - `utils/shared_variables.py`: Removes environment variables and instead references src/bash_variables.env
+
+<br/><br/>
+
 ## v4.0.19.1 - 2023-01-17 - [PR#796](https://github.com/NOAA-OWP/inundation-mapping/pull/796)
 
 ### Changes
--- src/utils/shared_variables.py
+++ src/utils/shared_variables.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python3
-
 import os
 from pyproj import CRS
 
+from dotenv import load_dotenv
+load_dotenv('/foss_fim/src/bash_variables.env')
+
 # -- Projections: EPSG 5070-- #
 PREP_PROJECTION = 'PROJCRS["NAD83 / Conus Albers",BASEGEOGCRS["NAD83",DATUM["North American Datum 1983",ELLIPSOID["GRS 1980",6378137,298.257222101,LENGTHUNIT["metre",1]]],PRIMEM["Greenwich",0,ANGLEUNIT["degree",0.0174532925199433]],ID["EPSG",4269]],CONVERSION["Conus Albers",METHOD["Albers Equal Area",ID["EPSG",9822]],PARAMETER["Latitude of false origin",23,ANGLEUNIT["degree",0.0174532925199433],ID["EPSG",8821]],PARAMETER["Longitude of false origin",-96,ANGLEUNIT["degree",0.0174532925199433],ID["EPSG",8822]],PARAMETER["Latitude of 1st standard parallel",29.5,ANGLEUNIT["degree",0.0174532925199433],ID["EPSG",8823]],PARAMETER["Latitude of 2nd standard parallel",45.5,ANGLEUNIT["degree",0.0174532925199433],ID["EPSG",8824]],PARAMETER["Easting at false origin",0,LENGTHUNIT["metre",1],ID["EPSG",8826]],PARAMETER["Northing at false origin",0,LENGTHUNIT["metre",1],ID["EPSG",8827]]],CS[Cartesian,2],AXIS["easting (X)",east,ORDER[1],LENGTHUNIT["metre",1]],AXIS["northing (Y)",north,ORDER[2],LENGTHUNIT["metre",1]],USAGE[SCOPE["Data analysis and small scale data presentation for contiguous lower 48 states."],AREA["United States (USA) - CONUS onshore - Alabama; Arizona; Arkansas; California; Colorado; Connecticut; Delaware; Florida; Georgia; Idaho; Illinois; Indiana; Iowa; Kansas; Kentucky; Louisiana; Maine; Maryland; Massachusetts; Michigan; Minnesota; Mississippi; Missouri; Montana; Nebraska; Nevada; New Hampshire; New Jersey; New Mexico; New York; North Carolina; North Dakota; Ohio; Oklahoma; Oregon; Pennsylvania; Rhode Island; South Carolina; South Dakota; Tennessee; Texas; Utah; Vermont; Virginia; Washington; West Virginia; Wisconsin; Wyoming."],BBOX[24.41,-124.79,49.38,-66.91]],ID["EPSG",5070]]' 
-DEFAULT_FIM_PROJECTION_CRS = 'EPSG:5070'  
+DEFAULT_FIM_PROJECTION_CRS = os.getenv('DEFAULT_FIM_PROJECTION_CRS')
 PREP_CRS = CRS(PREP_PROJECTION)
 VIZ_PROJECTION ='PROJCS["WGS_1984_Web_Mercator_Auxiliary_Sphere",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Mercator_Auxiliary_Sphere"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",0.0],PARAMETER["Standard_Parallel_1",0.0],PARAMETER["Auxiliary_Sphere_Type",0.0],UNIT["Meter",1.0]]'
 
@@ -13,7 +14,6 @@ VIZ_PROJECTION ='PROJCS["WGS_1984_Web_Mercator_Auxiliary_Sphere",GEOGCS["GCS_WGS
 NHD_URL_PARENT = r'https://prd-tnm.s3.amazonaws.com/StagedProducts/Hydrography/NHDPlusHR/Beta/GDB/'
 NWM_HYDROFABRIC_URL = r'http://www.nohrsc.noaa.gov/pub/staff/keicher/NWM_live/web/data_tools/NWM_channel_hydrofabric.tar.gz'  # Temporary
 WBD_NATIONAL_URL = r'https://prd-tnm.s3.amazonaws.com/StagedProducts/Hydrography/WBD/National/GDB/WBD_National_GDB.zip'
-WBD_HU2_URL_PARENT = r'http://prd-tnm.s3-website-us-west-2.amazonaws.com/?prefix=StagedProducts/Hydrography/WBD/HU2/GDB'
 
 
 # -- Prefixes and Suffixes -- #
@@ -55,28 +55,3 @@ OVERWRITE_ALL = 'OVERWRITE_ALL'
 DOWNSTREAM_THRESHOLD = 10 # distance in km to propogate new roughness values downstream
 ROUGHNESS_MAX_THRESH = 0.8 # max allowable adjusted roughness value (void values larger than this)
 ROUGHNESS_MIN_THRESH = 0.001 # min allowable adjusted roughness value (void values smaller than this)
-
-## Input Paths and Directories
-# Directories
-os.environ['src_dir'] = '/foss_fim/src'
-os.environ['input_dir'] = 'data/inputs'
-
-#os.environ['nhdplus_rasters_dir'] = os.path.join(os.environ.get('input_dir'),'nhdplus_rasters')
-os.environ['nhdplus_vectors_dir'] = os.path.join(os.environ.get('input_dir'),'nhdplus_vectors')
-os.environ['nwm_dir'] = os.path.join(os.environ.get('input_dir'),'nwm_hydrofabric')
-os.environ['wbd_dir'] = os.path.join(os.environ.get('input_dir'),'wbd')
-os.environ['ahps_dir'] = os.path.join(os.environ.get('input_dir'),'ahp_sites')
-os.environ['ahps_filename'] = os.path.join(os.environ.get('ahps_dir'),'nws_lid.gpkg')
-
-os.environ['3dep_dems_10m_5070'] = os.path.join(os.environ.get('input_dir'),'3dep_dems/10m_5070')
-os.environ['3dep_dems_10m_5070_vrt'] = os.path.join(os.environ.get('3dep_dems_10m_5070'),
-                                                    'fim_seamless_3dep_dem_10m_5070.vrt')
-
-os.environ['nwm_dir'] = os.path.join(os.environ.get('input_dir'),'nwm_hydrofabric')
-os.environ['nwm_streams_orig_filename'] = os.path.join(os.environ.get('nwm_dir'),'nwm_flows_original.gpkg')
-os.environ['nwm_streams_all_filename'] = os.path.join(os.environ.get('nwm_dir'),'nwm_flows.gpkg')
-os.environ['nwm_headwaters_filename'] = os.path.join(os.environ.get('nwm_dir'),'nwm_headwaters.gpkg')
-os.environ['nwm_huc4_intersections_filename'] = os.path.join(os.environ.get('nwm_dir'),'nwm_huc4_intersections.gpkg')
-os.environ['nwm_catchments_orig_filename'] = os.path.join(os.environ.get('nwm_dir'),'nwm_catchments_original.gpkg')
-os.environ['nwm_catchments_all_filename'] = os.path.join(os.environ.get('nwm_dir'),'nwm_catchments.gpkg')
-os.environ['nhd_huc8_intersections_filename'] = os.path.join(os.environ.get('nwm_dir'),'nhd_huc8_intersections.gpkg')
