ab49dcb9603694a217ab7aca82df4ea499a9df24
“MattLuck-NOAA” <matt.luck@noaa.gov>
2023-03-13 11:20:54 -0400

    Add parameter for levee_id_attribute

--- src/associate_levelpaths_with_levees.py
+++ src/associate_levelpaths_with_levees.py
@@ -5,11 +5,10 @@ import argparse
 import numpy as np
 import pandas as pd
 import geopandas as gpd
-from shapely import wkb
 
-def associate_levelpaths_with_levees(levees_filename, leveed_areas_filename, levelpaths_filename, levee_buffer, out_filename):
+def associate_levelpaths_with_levees(levees_filename, levee_id_attribute, leveed_areas_filename, levelpaths_filename, branch_id_attribute, levee_buffer, out_filename):
     """
-    Finds the level path associated with each levee
+    Finds the level path associated with each levee. Ignores level paths that cross a levee exactly once.
     """
 
     if os.path.exists(levees_filename):
@@ -18,9 +17,9 @@ def associate_levelpaths_with_levees(levees_filename, leveed_areas_filename, lev
         leveed_areas = gpd.read_file(leveed_areas_filename)
         levelpaths = gpd.read_file(levelpaths_filename)
 
-        levees['SYSTEM_ID'] = levees['SYSTEM_ID'].astype(int)
-        leveed_areas['SYSTEM_ID'] = leveed_areas['SYSTEM_ID'].astype(int)
-        levelpaths['levpa_id'] = levelpaths['levpa_id'].astype(int)
+        levees[levee_id_attribute] = levees[levee_id_attribute].astype(int)
+        leveed_areas[levee_id_attribute] = leveed_areas[levee_id_attribute].astype(int)
+        levelpaths[branch_id_attribute] = levelpaths[branch_id_attribute].astype(int)
 
         # Buffer each side of levee line
         levees_buffered_left = levees.copy()
@@ -33,8 +32,8 @@ def associate_levelpaths_with_levees(levees_filename, leveed_areas_filename, lev
         leveed_right = gpd.overlay(levees_buffered_right, leveed_areas, how='intersection')
 
         # Associate levees and leveed areas
-        matches_left = np.where(leveed_left['SYSTEM_ID_1']==leveed_left['SYSTEM_ID_2'])[0]
-        matches_right = np.where(leveed_right['SYSTEM_ID_1']==leveed_right['SYSTEM_ID_2'])[0]
+        matches_left = np.where(leveed_left[f'{levee_id_attribute}_1']==leveed_left[f'{levee_id_attribute}_2'])[0]
+        matches_right = np.where(leveed_right[f'{levee_id_attribute}_1']==leveed_right[f'{levee_id_attribute}_2'])[0]
 
         leveed_left = leveed_left.loc[matches_left]
         leveed_right = leveed_right.loc[matches_right]
@@ -43,11 +42,11 @@ def associate_levelpaths_with_levees(levees_filename, leveed_areas_filename, lev
         leveed_left['leveed_area'] = leveed_left.area
         leveed_right['leveed_area'] = leveed_right.area
 
-        leveed_left = leveed_left[['SYSTEM_ID_1', 'leveed_area', 'geometry']]
-        leveed_right = leveed_right[['SYSTEM_ID_1', 'leveed_area', 'geometry']]
+        leveed_left = leveed_left[[f'{levee_id_attribute}_1', 'leveed_area', 'geometry']]
+        leveed_right = leveed_right[[f'{levee_id_attribute}_1', 'leveed_area', 'geometry']]
 
         # Merge left and right levee protected areas
-        leveed = leveed_left.merge(leveed_right, on='SYSTEM_ID_1', how='outer', suffixes=['_left', '_right'])
+        leveed = leveed_left.merge(leveed_right, on=f'{levee_id_attribute}_1', how='outer', suffixes=['_left', '_right'])
 
         # Set unmatched areas to zero
         leveed.loc[np.isnan(leveed['leveed_area_left']), 'leveed_area_left'] = 0
@@ -57,46 +56,48 @@ def associate_levelpaths_with_levees(levees_filename, leveed_areas_filename, lev
         leveed['levee_side'] = np.where(leveed['leveed_area_left'] < leveed['leveed_area_right'], 'left', 'right')
 
         # Split into sides
-        left_ids = leveed.loc[leveed['levee_side']=='left', 'SYSTEM_ID_1']
-        right_ids = leveed.loc[leveed['levee_side']=='right', 'SYSTEM_ID_1']
+        left_ids = leveed.loc[leveed['levee_side']=='left', f'{levee_id_attribute}_1']
+        right_ids = leveed.loc[leveed['levee_side']=='right', f'{levee_id_attribute}_1']
 
         # Associate level paths with levee buffers
         levee_levelpaths_left = gpd.sjoin(levees_buffered_left, levelpaths)
         levee_levelpaths_right = gpd.sjoin(levees_buffered_right, levelpaths)
 
-        levee_levelpaths_left = levee_levelpaths_left[['SYSTEM_ID', 'levpa_id']]
-        levee_levelpaths_right = levee_levelpaths_right[['SYSTEM_ID', 'levpa_id']]
+        levee_levelpaths_left = levee_levelpaths_left[[levee_id_attribute, branch_id_attribute]]
+        levee_levelpaths_right = levee_levelpaths_right[[levee_id_attribute, branch_id_attribute]]
 
         # Select streams on the correct side of levee
-        levee_levelpaths_left = levee_levelpaths_left[levee_levelpaths_left['SYSTEM_ID'].isin(left_ids)]
-        levee_levelpaths_right = levee_levelpaths_right[levee_levelpaths_right['SYSTEM_ID'].isin(right_ids)]
+        levee_levelpaths_left = levee_levelpaths_left[levee_levelpaths_left[levee_id_attribute].isin(left_ids)]
+        levee_levelpaths_right = levee_levelpaths_right[levee_levelpaths_right[levee_id_attribute].isin(right_ids)]
 
-        out_df =  pd.concat([levee_levelpaths_right[['SYSTEM_ID', 'levpa_id']], levee_levelpaths_left[['SYSTEM_ID', 'levpa_id']]]).drop_duplicates()
+        out_df =  pd.concat([levee_levelpaths_right[[levee_id_attribute, branch_id_attribute]], levee_levelpaths_left[[levee_id_attribute, branch_id_attribute]]]).drop_duplicates()
 
-        out_df['SYSTEM_ID'] = out_df['SYSTEM_ID'].astype(int)
-        out_df['levpa_id'] = out_df['levpa_id'].astype(int)
+        out_df[levee_id_attribute] = out_df[levee_id_attribute].astype(int)
+        out_df[branch_id_attribute] = out_df[branch_id_attribute].astype(int)
 
         out_df = out_df.reset_index(drop=True)
 
         # Remove levelpaths that cross the levee exactly once
         for j, row in out_df.iterrows():
             # Intersect levees and levelpaths
-            row_intersections = gpd.overlay(levees[levees['SYSTEM_ID'] == row['SYSTEM_ID']], levelpaths[levelpaths['levpa_id'] == row['levpa_id']], how='intersection', keep_geom_type=False)
+            row_intersections = gpd.overlay(levees[levees[levee_id_attribute] == row[levee_id_attribute]], levelpaths[levelpaths[branch_id_attribute] == row[branch_id_attribute]], how='intersection', keep_geom_type=False)
             row_intersection_points = row_intersections[row_intersections.geom_type.isin(['Point', 'MultiPoint'])]
 
             if len(row_intersection_points) == 1:
                 out_df = out_df.drop(j)
                 
-        out_df.to_csv(out_filename, columns=['SYSTEM_ID', 'levpa_id'], index=False)
+        out_df.to_csv(out_filename, columns=[levee_id_attribute, branch_id_attribute], index=False)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Associate level paths with levees')
     parser.add_argument('-nld','--levees-filename', help='NLD levees filename', required=True, type=str)
+    parser.add_argument('-l','--levee-id-attribute', help='Levee ID attribute name', required=True, type=str)
     parser.add_argument('-out','--out-filename', help='out CSV filename', required=True, type=str)
     parser.add_argument('-s', '--levelpaths-filename', help='Level path layer filename', required=True, type=str)
+    parser.add_argument('-b','--branch-id-attribute', help='Level path ID attribute name', required=True, type=str)
     parser.add_argument('-lpa', '--leveed-areas-filename', help='NLD levee-protected areas filename', required=True, type=str)
-    parser.add_argument('-w', '--levee-buffer', help='Buffer width (in meters)', required=False, type=float, default=1000.)
+    parser.add_argument('-w', '--levee-buffer', help='Buffer width (in meters)', required=True, type=float)
 
     args = vars(parser.parse_args())
 
--- src/mask_dem.py
+++ src/mask_dem.py
@@ -10,13 +10,11 @@ import argparse
 from utils.shared_functions import mem_profile
 
 @mem_profile
-def mask_dem(dem_filename, nld_filename, out_dem_filename, branch_id_attribute, branch_id, order_attribute, branch_zero_id, levee_streams):
+def mask_dem(dem_filename, nld_filename, levee_id_attribute, out_dem_filename, branch_id_attribute, branch_id, branch_zero_id, levee_streams):
     """
     Masks levee-protected areas from DEM in branch 0 or if the level path is associated with a levee
     """
-
-
-
+    
     # Rasterize if branch zero
     if (branch_id == branch_zero_id):
         with rio.open(dem_filename) as dem, fiona.open(nld_filename) as leveed:
@@ -33,16 +31,16 @@ def mask_dem(dem_filename, nld_filename, out_dem_filename, branch_id_attribute, 
     elif os.path.exists(levee_streams):
         levee_streams = pd.read_csv(levee_streams)
 
-        levee_streams = levee_streams[levee_streams['levpa_id'] == branch_id]
+        levee_streams = levee_streams[levee_streams[branch_id_attribute] == branch_id]
 
-        levelpath_levees = list(levee_streams['SYSTEM_ID'])
+        levelpath_levees = list(levee_streams[levee_id_attribute])
         
         if len(levelpath_levees) > 0:
             with rio.open(dem_filename) as dem:#, fiona.open(nld_filename) as leveed:
                 leveed = gpd.read_file(nld_filename)
                 dem_profile = dem.profile.copy()
 
-                geoms = [feature['geometry'] for i, feature in leveed.iterrows() if feature['SYSTEM_ID'] in levelpath_levees]
+                geoms = [feature['geometry'] for i, feature in leveed.iterrows() if feature[levee_id_attribute] in levelpath_levees]
 
                 # Mask out levee-protected areas from DEM
                 out_dem_masked, _ = mask(dem, geoms, invert=True)
@@ -54,12 +52,12 @@ def mask_dem(dem_filename, nld_filename, out_dem_filename, branch_id_attribute, 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Mask levee-protected areas from DEM')
-    parser.add_argument('-dem','--dem-filename', help='DEM filename', required=True,type=str)
-    parser.add_argument('-nld','--nld-filename', help='NLD levee-protected areas filename', required=True,type=str)
-    parser.add_argument('-out','--out-dem-filename', help='out DEM filename', required=True,type=str)
-    parser.add_argument('-b', '--branch-id-attribute', help='Branch ID attribute name', required=False, default='levpa_id')
+    parser.add_argument('-dem','--dem-filename', help='DEM filename', required=True, type=str)
+    parser.add_argument('-nld','--nld-filename', help='NLD levee-protected areas filename', required=True, type=str)
+    parser.add_argument('-l','--levee-id-attribute', help='Levee ID attribute name', required=True,type=str)
+    parser.add_argument('-out','--out-dem-filename', help='out DEM filename', required=True, type=str)
+    parser.add_argument('-b', '--branch-id-attribute', help='Branch ID attribute name', required=True, type=str)
     parser.add_argument('-i', '--branch-id', help='Branch ID', type=int, required='True')
-    parser.add_argument('-a', '--order-attribute', help='Stream order attribute name', required=False, default='order_')
     parser.add_argument('-b0', '--branch-zero-id', help='Branch zero ID', type=int, required=False, default=0)
     parser.add_argument('-csv', '--levee-streams', help='Stream layer filename', required=True)
 
