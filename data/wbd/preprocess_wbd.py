062aa91a57922474c609685b673ba980ffaa9547
“MattLuck-NOAA” <matt.luck@noaa.gov>
2023-03-22 13:39:54 -0400

    Add data/wbd/preprocess_wbd.py

---
+++ data/wbd/preprocess_wbd.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import os
+from pathlib import Path
+import argparse
+import geopandas as gpd
+
+
+def clip_wbd_to_dem_domain(dem:str, wbd_in:str, wbd_out:str, huc_level:int):
+    """
+    Clips Watershed Boundary Dataset (WBD) to DEM domain
+
+    Parameters
+    ----------
+    dem: str
+        Path to DEM domain file
+    wbd_in: str
+        Path to WBD file input
+    wbd_out: str
+        Path to WBD file output
+    huc_level: int
+        HUC level
+    """
+
+    # Erase area outside 3DEP domain
+    if Path(wbd_in).is_file() and Path(dem).is_file():
+        layer = f'WBDHU{huc_level}'
+        wbd = gpd.read_file(wbd_in, layer=layer)
+        dem_domain = gpd.read_file(dem)
+        wbd = gpd.clip(wbd, dem_domain)
+
+        wbd.to_file(wbd_out, layer=layer)
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Clip WBD to DEM domain')
+    parser.add_argument('-d', '--dem', help='Path to DEM', type=str, required=True)
+    parser.add_argument('-w', '--wbd-in', help='Input WBD filename', type=str, required=True)
+    parser.add_argument('-x', '--wbd-out', help='Output WBD filename', type=str, required=True)
+    parser.add_argument('-l', '--huc-level', help='HUC level', type=int, required=True)
+
+    args = vars(parser.parse_args())
+
+    clip_wbd_to_dem_domain(**args)
+
+    # Example:
+    # clip_wbd_to_dem_domain.py -d /data/inputs/3dep_dems/10m_5070/HUC6_dem_domain.gpkg -w /data/inputs/wbd/WBD_National_EPSG_5070.gpkg -x /data/temp/WBD_clip.gpkg -l 8'
