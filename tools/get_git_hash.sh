64452862c288fd36bad46a0bc605c71f68ac1980
“MattLuck-NOAA” <matt.luck@noaa.gov>
2024-01-11 17:44:52 -0500

    working

--- tools/get_git_hash.sh
+++ tools/get_git_hash.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-
-echo
 echo 'Saving '`pwd`'/config/params.env'
 
 cp config/params_template.env config/params.env
 
 echo >> config/params.env
-echo '#### GitHub commit hash ####' >> config/params.env
+echo -e '\n#### GitHub commit hash ####' >> config/params.env
 echo 'export commit_hash='`(git rev-parse --short HEAD)` >> config/params.env
