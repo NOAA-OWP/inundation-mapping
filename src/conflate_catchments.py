9da454a1b7ead7d9d7cb1fefa0faaef20a9417e6
“MattLuck-NOAA” <matt.luck@noaa.gov>
2024-02-19 11:11:01 -0500

    Generate upstream contributing area for each NWM segment

--- src/conflate_catchments.py
+++ src/conflate_catchments.py
@@ -18,6 +18,27 @@ def main(
     out_filename: str,
     sink_value=None,
 ):
+    """
+    Conflate catchments with network
+
+    Parameters
+    ----------
+    catchments_filename : str
+        Catchments filename
+    streams_filename : str
+        Streams filename
+    id_attribute : str
+        Name of ID attribute
+    from_attribute : str
+        Name of 'from' attribute
+    to_attribute : str
+        Name of 'to' attribute
+    out_filename : str
+        Output filename
+    sink_value : int
+        Sink value
+    """
+
     # Check if input files exist
     assert os.path.exists(catchments_filename), f"Catchments file {catchments_filename} not found"
     assert os.path.exists(streams_filename), f"Streams file {streams_filename} not found"
@@ -51,27 +72,18 @@ def main(
             Target network with upstream IDs
         """
 
-        print('id', id)
-
         catchment_geoms = []
-
-        if id == 5791184:
-            pass
 
         # Get IDs of upstream segments
         upstream_ids = streams.loc[streams[to_attribute] == id, id_attribute]
 
         if len(upstream_ids) > 0:
             for i, upstream_id in enumerate(upstream_ids):
-                print('\tid, upstream_id', id, upstream_id)
-
                 # Get feature geometry
                 catchment_geom = catchments.loc[catchments[id_attribute] == upstream_id, 'geometry']
 
                 if upstream_id in headwater_ids:
-                    if (
-                        len(catchment_geom) > 0
-                    ):  # To avoid empty geometries (e.g., NWM stream ID 9569559 in 12090301)
+                    if len(catchment_geom) > 0:  # To avoid empty geometries (streams without catchments)
                         # Add headwater catchment to out_catchments
                         out_catchments = pd.concat(
                             [
@@ -93,63 +105,40 @@ def main(
 
                 else:
                     # Recursively find upstream segments
-                    out_catchments, catchment_geoms = _get_upstream_area(
+                    out_catchments, upstream_catchment_geoms = _get_upstream_area(
                         upstream_id, streams, catchments, headwater_ids, out_catchments
                     )
 
-                    if len(catchment_geom) > 0:
-                        # Dissolve catchment_geom and catchment_geoms
-                        catchment_geoms.append(catchment_geom)
+                    if len(upstream_catchment_geoms) > 0:
+                        catchment_geoms.extend(upstream_catchment_geoms)
 
-                    if len(catchment_geoms) > 0:
-                        catchment_geometry = shapely.unary_union(catchment_geoms)
+            # Add downstream catchment to dissolved catchments
+            # Get feature geometry
+            catchment_geom = catchments.loc[catchments[id_attribute] == id, 'geometry']
 
-                        # Add dissolved catchment to out_catchments
-                        out_catchments = pd.concat(
-                            [
-                                out_catchments,
-                                gpd.GeoDataFrame(
-                                    [
-                                        {
-                                            id_attribute: upstream_id,
-                                            'downstream_id': id,
-                                            'geometry': catchment_geometry,
-                                        }
-                                    ]
-                                ),
-                            ]
-                        )
+            if len(catchment_geom) > 0:
+                catchment_geoms.append(catchment_geom)
 
-            if i > 0:
-                # Dissolve upstream catchments (with the same downstream_id)
-                catchments_dissolved = (
-                    out_catchments.loc[out_catchments['downstream_id'] == id]
-                    .dissolve(by='downstream_id')
-                    .geometry.values[0]
-                )
+            if len(catchment_geoms) > 0:
+                catchment_geometry = shapely.unary_union(catchment_geoms)
 
-                # Add downstream catchment to dissolved catchments
-                # Get feature geometry
-                catchment_geom = catchments.loc[catchments[id_attribute] == id, 'geometry']
+                downstream_id = streams.loc[streams[id_attribute] == id, to_attribute].values[0]
 
-                if len(catchment_geom) > 0:
-                    catchments_dissolved = shapely.unary_union(
-                        [catchments_dissolved, catchment_geom.values[0]]
-                    )
-
-                # # Add dissolved catchment to out_catchments
-                # catchments[catchments[id_attribute] == id, 'geometry'] = catchments_dissolved
+                # Add dissolved catchment to out_catchments
                 out_catchments = pd.concat(
                     [
                         out_catchments,
                         gpd.GeoDataFrame(
-                            [{id_attribute: id, 'downstream_id': id, 'geometry': catchments_dissolved}]
+                            [
+                                {
+                                    id_attribute: id,
+                                    'downstream_id': downstream_id,
+                                    'geometry': catchment_geometry,
+                                }
+                            ]
                         ),
                     ]
                 )
-
-                # Update out_catchments
-                # out_catchments.loc[out_catchments[id_attribute] == id, 'geometry'] = catchments_dissolved
 
         return out_catchments, catchment_geoms
 
@@ -171,19 +160,6 @@ def main(
                 upstream_id, streams, catchments, headwater_ids, out_catchments
             )
 
-    # # Update confluence catchments
-    # confluence_ids = out_catchments.loc[out_catchments[id_attribute]==out_catchments['downstream_id'], id_attribute].unique()
-
-    # for id in confluence_ids:
-    #     # Dissolve confluence catchments
-    #     catchments_dissolved = out_catchments.loc[out_catchments[id_attribute] == id].dissolve(by=id_attribute).geometry.values[0]
-
-    #     # Update out_catchments
-    #     out_catchments.loc[(out_catchments[id_attribute] == id) & (out_catchments['downstream_id'] != id), 'geometry'] = catchments_dissolved
-
-    #     # Remove original catchment
-    #     out_catchments = out_catchments.loc[~((out_catchments[id_attribute] == id) & (out_catchments['downstream_id'] == id))]
-
     # Save output
     out_catchments.to_file(out_filename, crs=catchments.crs)
 
@@ -192,9 +168,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Conflate NWM feature_ids to NHDPlus network')
     parser.add_argument('-catch', '--catchments-filename', help='Catchments filename')
     parser.add_argument('-stream', '--streams-filename', help='Streams filename')
-    parser.add_argument('-id', '--id-attribute', help='ID attribute')
-    parser.add_argument('-to', '--to-attribute', help='To attribute')
-    parser.add_argument('-from', '--from-attribute', help='From attribute')
+    parser.add_argument('-id', '--id-attribute', help='Name of ID attribute')
+    parser.add_argument('-to', '--to-attribute', help='Name of to attribute')
+    parser.add_argument('-from', '--from-attribute', help='Name of from attribute')
     parser.add_argument('-sink', '--sink-value', help='Sink value', default=None, required=False)
     parser.add_argument('-out', '--out-filename', help='Output filename')
 
