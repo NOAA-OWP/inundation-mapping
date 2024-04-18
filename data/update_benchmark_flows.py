8b327ae67dc1f4533e623e37e873d8fc7b3be4ae
“MattLuck-NOAA” <matt.luck@noaa.gov>
2024-04-18 15:07:41 -0400

    Retain levelpath segments outside of domain

--- data/update_benchmark_flows.py
+++ data/update_benchmark_flows.py
@@ -11,7 +11,7 @@ import pandas as pd
 gpd.options.io_engine = "pyogrio"
 
 
-def update_benchmark_flows(fim_dir: str, output_dir_base: str):
+def update_benchmark_flows(fim_dir: str, output_dir_base: str, verbose: bool = False):
     """
     Update benchmark flows of the levelpath in the domain for stream segments missing from the flow file
 
@@ -24,7 +24,7 @@ def update_benchmark_flows(fim_dir: str, output_dir_base: str):
     """
 
     def iterate_over_sites(
-        levelpaths: gpd.GeoDataFrame, domain: gpd.GeoDataFrame, flows: pd.DataFrame
+        levelpaths: gpd.GeoDataFrame, levelpaths_domain: gpd.GeoDataFrame, flows: pd.DataFrame
     ) -> pd.DataFrame:
         """
         Update benchmark flows of stream segments missing from the flow file
@@ -33,8 +33,8 @@ def update_benchmark_flows(fim_dir: str, output_dir_base: str):
         ----------
         levelpaths : gpd.GeoDataFrame
             Level paths of the HUC
-        domain : gpd.GeoDataFrame
-            Domain of the site
+        levelpaths_domain : gpd.GeoDataFrame
+            Levelpaths in the domain
         flows : pd.DataFrame
             Benchmark flows
 
@@ -44,31 +44,53 @@ def update_benchmark_flows(fim_dir: str, output_dir_base: str):
             Flows with updated benchmark flows
         """
 
-        # Intersect levelpaths with the domain to get feature_ids
-        levelpaths = gpd.sjoin(levelpaths, domain, how="inner", predicate="intersects")
-
-        if levelpaths.empty:
+        if levelpaths_domain.empty:
             return flows
 
-        # Find the levelpath with the highest order (to exclude tributaries)
-        levelpaths = levelpaths[levelpaths['order_'] == levelpaths['order_'].max()]
+        # Find the levelpaths with the highest order (to exclude tributaries)
+        levelpaths_domain = levelpaths_domain[
+            levelpaths_domain['order_'] == levelpaths_domain['order_'].max()
+        ]
 
         # Find the levelpath that has flows in the flow file
-        levelpath = levelpaths.loc[levelpaths['ID'].isin(flows['feature_id']), 'levpa_id']
+        if levelpaths_domain['levpa_id'].nunique() > 1:
+            # If there are multiple levelpaths with the highest order, take the longest one (intersect levelpaths_domain with the domain to get the length of the levelpath)
+            levelpaths_domain_intersect = gpd.overlay(levelpaths, domain, how='intersection')
+            levelpaths_domain_intersect['length'] = levelpaths_domain_intersect['geometry'].length
 
-        if levelpath.empty:
+            # Get the total length of all the segments in the levelpath
+            levelpaths_domain_intersect_length = (
+                levelpaths_domain_intersect.groupby('levpa_id').agg({'length': 'sum'}).reset_index()
+            )
+
+            # Get the longest levelpath
+            levelpath = levelpaths_domain_intersect_length.loc[
+                levelpaths_domain_intersect_length['length'].idxmax(), 'levpa_id'
+            ]
+
+        else:
+            levelpath = levelpaths_domain['levpa_id'].iloc[0]
+
+        # Get IDs of all features in the levelpath
+        IDs_to_keep = levelpaths[levelpaths['levpa_id'] == levelpath]['ID']
+
+        # Get IDs of all features in the levelpath in the domain
+        IDs = levelpaths_domain.loc[levelpaths_domain['levpa_id'] == levelpath, 'ID']
+
+        # Keep the flows that are in the levelpath (remove tributaries)
+        flows = flows.iloc[flows.loc[flows['feature_id'].isin(IDs_to_keep)].index]
+
+        if flows.empty:
             return flows
 
-        # Remove flows not in the levelpath
-        flows = flows.iloc[flows.loc[flows['feature_id'].isin(levelpaths.ID), 'feature_id'].index]
-
-        levelpath = levelpath.values[0]
-
-        levelpaths = levelpaths[levelpaths['levpa_id'] == levelpath]
-        IDs = levelpaths.loc[levelpaths['levpa_id'] == levelpath, 'ID']
-
         # Find IDs not in the flow file
-        IDs_to_add = levelpaths.loc[~IDs.isin(flows['feature_id']), 'ID']
+        add_IDs = ~IDs.isin(flows['feature_id'])
+
+        # Exit if no IDs to add
+        if not any(add_IDs):
+            return flows
+
+        IDs_to_add = levelpaths_domain.loc[add_IDs.index, 'ID']
 
         # Add the missing IDs with flows to the flow file
         if not IDs_to_add.empty:
@@ -82,7 +104,8 @@ def update_benchmark_flows(fim_dir: str, output_dir_base: str):
         return flows_out
 
     for org in ['nws', 'usgs']:
-        print('Processing', org)
+        if verbose:
+            print('Processing', org)
 
         count_total = 0
         count_updated = 0
@@ -92,9 +115,15 @@ def update_benchmark_flows(fim_dir: str, output_dir_base: str):
         huc8s = next(os.walk(base_dir))[1]
 
         for huc8 in huc8s:
+            if verbose:
+                print(f'\t{huc8}')
+
             lids = next(os.walk(f'{base_dir}/{huc8}'))[1]
 
             for lid in lids:
+                if verbose:
+                    print(f'\t\t{lid}')
+
                 # Read the input files
                 levelpath_file = f'{fim_dir}/{huc8}/nwm_subset_streams_levelPaths.gpkg'
                 if not os.path.exists(levelpath_file):
@@ -109,6 +138,9 @@ def update_benchmark_flows(fim_dir: str, output_dir_base: str):
                     continue
 
                 domain = gpd.read_file(domain_file)
+
+                # Intersect levelpaths with the domain to get feature_ids
+                levelpaths_domain = gpd.sjoin(levelpaths, domain, how="inner", predicate="intersects")
 
                 magnitudes = next(os.walk(validation_path))[1]
 
@@ -137,7 +169,7 @@ def update_benchmark_flows(fim_dir: str, output_dir_base: str):
 
                     flows = pd.read_csv(backup_flow_file)
 
-                    flows_new = iterate_over_sites(levelpaths, domain, flows)
+                    flows_new = iterate_over_sites(levelpaths, levelpaths_domain, flows)
 
                     if not flows_new.equals(flows):
                         count_updated += 1
@@ -161,6 +193,8 @@ if __name__ == "__main__":
     )
     parser.add_argument('-f', '--fim_dir', help='Location of FIM files', type=str, required=True)
     parser.add_argument('-o', '--output_dir-base', help='Output directory', type=str, default=None)
+    parser.add_argument('-v', '--verbose', help='Verbose output', action='store_true')
+
     args = vars(parser.parse_args())
 
     update_benchmark_flows(**args)
