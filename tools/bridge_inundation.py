12cf1e8e3e22d00e23d5d324ed80b47d654d82ac
Zahra Ghahremani <1632979513113305@mil>
2024-06-04 11:16:24 -0600

    applied linting

--- tools/bridge_inundation.py
+++ tools/bridge_inundation.py
@@ -1,13 +1,13 @@
-import pandas as pd
-import geopandas as gpd
-import os
-import errno
 import argparse
+import errno
+import os
 from timeit import default_timer as timer
+
+import geopandas as gpd
+import pandas as pd
 
 
 def bridge_risk_status(hydrofabric_dir: str, flow_file_dir: str, output_dir: str) -> gpd.GeoDataFrame:
-
     """
     This function detect which bridge points are affected by a specified flow file. The function requires a flow file (expected to follow
     the schema used by 'inundation_mosaic_wrapper') with data organized by 'feature_id' and 'discharge' in cms. The output includes a geopackage
@@ -22,12 +22,11 @@ def bridge_risk_status(hydrofabric_dir: str, flow_file_dir: str, output_dir: str
         output (str):             Path to output geopackage.
     """
 
-
     dir_path = hydrofabric_dir
     # Check that hydrofabric_dir exists
     if not os.path.exists(dir_path):
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), dir_path)
-    
+
     # Get the list of all hucs in the directory
     entries = os.listdir(dir_path)
     hucs = []
@@ -37,20 +36,20 @@ def bridge_risk_status(hydrofabric_dir: str, flow_file_dir: str, output_dir: str
         # check if the netry is a directory
         if os.path.isdir(full_path):
             hucs.append(entry)
-            
+
     # Check that flow file exists
     if not os.path.exists(flow_file_dir):
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), flow_file_dir)
-    
+
     # Read the flow_file
-    dtype_dict = {'feature_id' : str}
-    flow_file = pd.read_csv(flow_file_dir, dtype = dtype_dict)
+    dtype_dict = {'feature_id': str}
+    flow_file = pd.read_csv(flow_file_dir, dtype=dtype_dict)
 
     # Initialize an empty list to hold GeoDataFrames
     gdfs = []
     # Iterate through hucs
-    for huc in hucs: 
-        print(f'Processing HUC: {huc}') 
+    for huc in hucs:
+        print(f'Processing HUC: {huc}')
         # Construct the file path
         gpkg_path = os.path.join(dir_path, huc, 'osm_bridge_centroids.gpkg')
         # Check if the file exists
@@ -59,19 +58,20 @@ def bridge_risk_status(hydrofabric_dir: str, flow_file_dir: str, output_dir: str
             continue
         # Open the bridge point GeoPackage for each huc
         bri_po = gpd.read_file(gpkg_path)
-        
-         # Save the origignal crs in a new column
+
+        # Save the origignal crs in a new column
         bri_po['original_crs'] = bri_po.crs.to_string()
-    
+
         # Reproject to EPSG:4326
         bri_po = bri_po.to_crs('epsg:4326')
         gdfs.append(bri_po)
-        
+
     # Concatenate all GeoDataFrame into a single GeoDataFrame
     bridge_points = gpd.GeoDataFrame(pd.concat(gdfs, ignore_index=True))
 
     # Find the common feature_id between flow_file and bridge_points
     merged_bri = bridge_points.merge(flow_file, on='feature_id', how='inner')
+
     # Assign risk status for each point
     def risk_class(row):
         if row['discharge'] > row['max_discharge']:
@@ -80,6 +80,7 @@ def bridge_risk_status(hydrofabric_dir: str, flow_file_dir: str, output_dir: str
             return 'at_risk'
         else:
             return 'not_at_risk'
+
     # Apply risk_class function to each row
     merged_bri['risk_status'] = merged_bri.apply(risk_class, axis=1)
     merged_bri.drop('discharge', axis=1, inplace=True)
@@ -93,7 +94,7 @@ def bridge_risk_status(hydrofabric_dir: str, flow_file_dir: str, output_dir: str
     bridge_out.reset_index(drop=True, inplace=True)
     bridge_out.drop('risk', axis=1, inplace=True)
     bridge_out.to_file(output_dir, driver='GPKG', layer='bridge_risk_status')
-    
+
     return bridge_out
 
 
@@ -116,14 +117,7 @@ if __name__ == "__main__":
         required=True,
         type=str,
     )
-    parser.add_argument(
-        "-o",
-        "--gpkg-filename",
-        help="Path to geopackage output.",
-        required=True,
-        type=str,
-    )
-    
+    parser.add_argument("-o", "--gpkg-filename", help="Path to geopackage output.", required=True, type=str)
 
     start = timer()
 
