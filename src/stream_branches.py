0c10e1bc88f3e38ee6e3af9437dc8e815d535f70
MattLuck-NOAA <matt.luck@noaa.gov>
2024-10-04 14:39:33 -0600

    More edits

--- src/stream_branches.py
+++ src/stream_branches.py
@@ -1093,8 +1093,13 @@ class StreamNetwork(gpd.GeoDataFrame):
 
             # For each outlet
             for outlet in s_not_in_wbd.itertuples():
-                outlet_id = self_in_wbd.loc[self_in_wbd['to'] == outlet.ID, 'ID'].values[0]
-                outlets_extended = add_outlet_segments(outlets_extended, self_copy, outlet_id, outlet)
+                # Select segments in levelpath
+                temp_df = self_copy[self_copy[branch_id_attribute] == outlet.bids_temp]
+
+                # Check if the levelpath outlet is external
+                if not len(temp_df.merge(self_copy, left_on='to', right_on='ID')) == len(temp_df):
+                    outlet_id = self_in_wbd.loc[self_in_wbd['to'] == outlet.ID, 'ID'].values[0]
+                    outlets_extended = add_outlet_segments(outlets_extended, self_copy, outlet_id, outlet)
 
             # merges each multi-line string to a singular linestring
             for lpid, row in tqdm(
