f20f423f4f7d18f23eb4332bad7e54851c3c6c07
Rob Hanna <Robert.Hanna@NOAA.gov>
2024-10-30 20:06:07 +0000

    cleanup raster load

--- tools/catfim/generate_categorical_fim_mapping.py
+++ tools/catfim/generate_categorical_fim_mapping.py
@@ -926,14 +926,19 @@ def reformat_inundation_maps(
 
         # trying a similar process from 'identify_catchment_boundary.py
         with rasterio.open(tif_to_process) as src:
-            affine = src.transform
             band = src.read(1)
             band[np.where(band <= 0)] = 0
             band[np.where(band > 0)] = 1
             inundated = band > 0
-            results = (
+            # results = (
+            #     {'properties': {'extent': v}, 'geometry': s}
+            #     for i, (s, v) in enumerate(shapes(band, mask=inundated, transform=affine))
+            # )  # don't need enumerate (slows it down a tad)
+            # Oct 29, 2024: Found this trick in HydroVIS PR 941
+            # https://github.com/NOAA-OWP/hydrovis/pull/941
+            results = list(
                 {'properties': {'extent': v}, 'geometry': s}
-                for i, (s, v) in enumerate(shapes(band, mask=inundated, transform=affine))
+                for ___, (s, v) in rasterio.features.shapes(band, mask=inundated, transform=src.transform)
             )
 
         # Convert list of shapes to polygon
