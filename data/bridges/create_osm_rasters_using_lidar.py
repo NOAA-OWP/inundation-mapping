289be2545763080e247bbca227caaa9624ca81b7
AliForghani-NOAA <ali.forghani@noaa.gov>
2024-12-27 06:44:00 +0000

    added two new scripts to create lidar info for bridges

---
+++ data/bridges/create_osm_rasters_using_lidar.py
@@ -0,0 +1,407 @@
+import argparse
+import datetime as dt
+import laspy
+import numpy as np
+import pdal
+import json
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import pandas as pd
+import os
+import glob
+from pathlib import Path
+from shapely.geometry import Point,MultiPoint    
+from scipy.spatial import KDTree
+import xarray as xr
+from multiprocessing import Pool
+
+
+def download_lidar_points(args):
+    osmid,poly_geo, lidar_url,output_dir=args
+    poly_wkt=poly_geo.wkt
+    las_file_path=os.path.join(output_dir,'point_files','%s.las'%str(osmid))
+    my_pipe={
+    "pipeline": [
+        {
+            "polygon": str(poly_wkt),
+            "filename": lidar_url,
+            "type": "readers.ept",
+            "tag": "readdata"
+        },
+        {
+        "type": "filters.returns",
+        "groups": "last,only"  #last mean the last if when there are multiple returns..it does not include cases When there is only one return. so we need both "last" and "only" 
+        },
+
+        # {
+        #     "in_srs":'EPSG:3857',
+        #     "out_srs": 'EPSG:%d'%tif_crs,
+        #     "type": "filters.reprojection",
+        #     "tag": "reprojected",
+        # },
+
+        {
+        # "filename": os.path.join(st.session_state['this_bridge_output'] , 'all_last_return_points.las'),
+        "filename": las_file_path ,
+        "tag": "writerslas",
+        "type": "writers.las"
+        }
+            ]
+        }
+
+    # Create a PDAL pipeline object
+    pipeline = pdal.Pipeline(json.dumps(my_pipe))
+
+    # Execute the pipeline
+    pipeline.execute()
+
+
+
+def las_to_gpkg(las_path):
+    las=laspy.read(las_path)
+
+    #make x,y coordinates
+    x_y = np.vstack((np.array(las.x), np.array(las.y))).transpose()
+
+    #convert the coordinates to a list of shapely Points
+    las_points = list(MultiPoint(x_y).geoms)
+
+    #put the points in a GeoDataFrame for a more standard syntax through Geopandas
+    points_gdf= gpd.GeoDataFrame(geometry=las_points, crs="epsg:3857")
+
+
+    #add other required data into gdf...here only elevation
+    z_values=np.array(las.z)
+    points_gdf['z']=z_values
+
+    return_values=np.array(las.return_number)
+    points_gdf['return']=return_values
+
+    class_values=np.array(las.classification)
+    points_gdf['classification']=class_values
+
+    number_of_returns = np.array(las.number_of_returns)
+    points_gdf['number_of_returns'] = number_of_returns
+    
+    #note that pdal uses below scenarios of 'return number' and 'number_of_return' as described in documentauon below to identify, first, and last returns ...
+    # There is no other way to do this classifcation task. Note that x,y of different returns of a specifc pulse can be in different locations. so you should not 
+    # expect to have multiple points at a exact x, y with different return numbers. 
+    #  Also, 'point_source_id' are usually not reliable.  So, the only way to assign return numbers is by comparing return number and 'number of returns' as pdal is doing:
+    #https://pdal.io/en/latest/stages/filters.returns.html 
+
+    return points_gdf
+
+
+def handle_noises(points_gdf):
+    points_gdf.loc[:,'x'] = points_gdf.geometry.x
+    points_gdf.loc[:,'y'] = points_gdf.geometry.y
+
+    #save the original z
+    points_gdf.loc[:, 'origi_z'] = points_gdf['z'].values
+
+
+    noise_bool=  ~points_gdf['classification'].isin([17,13]) 
+    points_gdf.loc[:, 'noise'] = np.where(noise_bool, 'y', 'n')
+
+    non_noises=points_gdf[points_gdf['noise']=='n']
+    noises=points_gdf[(points_gdf['noise']=='y')]
+    if len(non_noises)/len(points_gdf)<0.05: #if there is few class 13 and 17
+        return None
+
+    # # Create KDTree using x and y coordinates of non-noise points across all classes
+    tree = KDTree(non_noises[['x', 'y']])
+
+    # Find the 2 nearest neighbors for each outlier in class 1 based on x, y coordinates
+    distances, indices = tree.query(noises[['x', 'y']].values, k=2)
+
+    # Get the z values of the nearest 2 neighbors
+    nearest_z_values = non_noises.iloc[indices.flatten()]['z'].values.reshape(indices.shape)
+    noises.loc[:,'z'] = np.mean(nearest_z_values, axis=1)
+
+
+    modified_points_gdf = pd.concat([noises, non_noises])
+    return modified_points_gdf
+
+
+def make_local_tifs(modified_las_path,raster_resolution, tif_path):
+    my_pipe={
+        "pipeline": [
+            {
+            "type": "readers.las",
+            "filename": modified_las_path, 
+            "spatialreference": "EPSG:3857"  # specify the correct coordinate reference system
+            },
+            
+            {
+            "type": "writers.gdal",
+            "filename": tif_path,
+            "dimension": "Z",
+            "output_type": "idw", # or try  "mean",
+            "resolution": raster_resolution,
+            "nodata": -999,
+            "data_type": "float32",
+            }
+                ]
+    }
+
+    # Create a PDAL pipeline object
+    pipeline = pdal.Pipeline(json.dumps(my_pipe))
+
+    # Execute the pipeline
+    pipeline.execute()
+
+def gpkg_to_las(points_gdf):
+
+    x = points_gdf.geometry.x
+    y = points_gdf.geometry.y
+    z = points_gdf['z'].values 
+
+    header = laspy.LasHeader()
+    las_obj = laspy.LasData(header)
+    las_obj.x = x
+    las_obj.y = y
+    las_obj.z = z
+    return las_obj
+
+def make_lidar_footprints():
+    str_hobu_footprints = (
+        r"https://raw.githubusercontent.com/hobu/usgs-lidar/master/boundaries/boundaries.topojson"
+    )
+    entwine_footprints_gdf = gpd.read_file(str_hobu_footprints)
+    entwine_footprints_gdf.set_crs("epsg:4326", inplace=True)  #it is geographic (lat-long degrees) commonly used for GPS for accurate locations
+    #it is important to reproject to 3857 because poly_wkt in process_lidar must be in that crs to properly apply "readers.ept" step.
+
+    entwine_footprints_gdf.to_crs("epsg:3857", inplace=True) 
+    return entwine_footprints_gdf
+
+
+def create_dem_diff(original_dem_path,local_tif_paths,diff_dem_path):
+    # Step 1: Open the regional TIFF as an xarray DataArray
+    original_da = xr.open_dataarray(original_dem_path, engine="rasterio")
+    original_nodata=original_da.rio.nodata
+    enhanced_da = original_da.copy()
+
+    for i, local_tif_path in enumerate(local_tif_paths): 
+        print(local_tif_path)
+    
+        # Step 2: Open the local TIFF as an xarray DataArray and reproject to match the regional grid, if needed
+        local_da = xr.open_dataarray(local_tif_path, engine="rasterio")
+
+        if local_da.rio.crs != original_da.rio.crs:
+            local_da = local_da.rio.reproject_match(original_da) 
+
+        # Step 3: Replace values in the regional DataArray with the local DataArray values at overlapping locations
+        enhanced_da = enhanced_da.where(local_da.isnull(), other=local_da)
+
+    # # Step 4: Set nodata value to be consistent
+    enhanced_da=enhanced_da.fillna(original_nodata)
+    enhanced_da.rio.write_nodata(original_nodata, inplace=True)
+
+    # Sif needed, Explicitly assign the CRS from the regional data to the final combined data
+    # updated_regional_da.rio.write_crs(regional_da.rio.crs, inplace=True)
+
+    # enhanced_da.rio.to_raster(enhanced_dem_path)
+
+    diff = enhanced_da - original_da
+
+    # Save the result to a new TIFF file
+    diff.rio.to_raster(diff_dem_path)
+
+def NOT_USED_add_diff_into_rem(rem_path,diff_dem_path,updated_rem_path):
+    # Open REM and DIFF using xarray with rasterio engine
+    rem = xr.open_dataarray(rem_path, engine="rasterio")
+    diff = xr.open_dataarray(diff_dem_path, engine="rasterio")
+
+    # Reproject DIFF to match REM
+    diff_reprojected = diff.rio.reproject_match(rem)
+
+    # Add DIFF values to REM where DIFF has valid data
+    updated_rem = rem.copy()
+    updated_rem = updated_rem.where(diff_reprojected.isnull(), rem + diff_reprojected)
+
+    # Save the result to a new file
+    updated_rem.rio.to_raster(updated_rem_path)
+
+def process_bridges_lidar_data(OSM_bridge_file,buffer_width,raster_resolution,output_dir):
+    start_time = dt.datetime.now(dt.timezone.utc)
+
+    # Create subfolders 'lidar_points' and 'classifications' inside the main folder
+    point_dir = os.path.join(output_dir, 'point_files')
+    tif_files_dir = os.path.join(output_dir, 'tif_files')
+
+    os.makedirs(point_dir, exist_ok=True)
+    os.makedirs(tif_files_dir, exist_ok=True)
+
+    tif_crs=3857 # consider using for Alaska ?
+
+    #produce footprints of lidar dataset over conus
+    entwine_footprints_gdf=make_lidar_footprints()
+
+    OSM_bridge_lines_gdf=gpd.read_file(OSM_bridge_file)
+    OSM_polygons_gdf=OSM_bridge_lines_gdf.copy()
+    OSM_polygons_gdf['geometry'] = OSM_polygons_gdf['geometry'].buffer(buffer_width)  
+    OSM_polygons_gdf.to_crs(entwine_footprints_gdf.crs, inplace=True)
+    OSM_polygons_gdf.rename(columns={'name':'bridge_name'}, inplace=True)
+        
+    #intersect with lidar urls
+    OSM_polygons_gdf = gpd.overlay(OSM_polygons_gdf, entwine_footprints_gdf, how='intersection')
+
+    OSM_polygons_gdf.to_file(os.path.join(output_dir,'buffered_bridges.gpkg'))
+
+    #filter if there are multiple urls for a bridge, keep the url with highest count
+    OSM_polygons_gdf = OSM_polygons_gdf.loc[OSM_polygons_gdf.groupby('osmid')['count'].idxmax()]
+    OSM_polygons_gdf = OSM_polygons_gdf.reset_index(drop=True)
+
+    pool_args=[]
+    for i, row in OSM_polygons_gdf.iterrows():
+        osmid,poly_geo, lidar_url = row.osmid, row.geometry, row.url
+        pool_args.append((osmid,poly_geo, lidar_url,output_dir))
+        # download_lidar_points(osmid,poly_geo, lidar_url,output_dir)
+
+    print('There are %d files to get downloaded'%len(pool_args))
+    with Pool(15) as pool:
+        pool.map(download_lidar_points, pool_args)
+
+    downloaded_points_files = glob.glob(os.path.join(output_dir,'point_files', '*.las'))
+
+    for points_path in downloaded_points_files:
+        osmid= os.path.basename(points_path).split('.las')[0]
+
+        #make a gpkg file from points
+        points_gdf=las_to_gpkg(points_path)
+        if not points_gdf.empty:
+            
+            modified_points_gdf=handle_noises(points_gdf)
+            if modified_points_gdf is None:
+                continue
+    
+            #make a las file for subsequent pdal pipeline
+            modified_las_path=os.path.join(output_dir,'point_files','%s_modified.las'%osmid)
+            las_obj=gpkg_to_las(modified_points_gdf)
+            las_obj.write(modified_las_path)
+            
+            #make tif files
+            tif_output=os.path.join(output_dir,'tif_files','%s.tif'%osmid)
+            make_local_tifs(modified_las_path,raster_resolution, tif_output)
+
+            #remove the temporary las file
+            os.remove(modified_las_path)
+
+    # #identify osmids with lidar-tif or not
+    # lidar_tif_folder = os.path.join(output_dir,'tif_files')
+    # tif_ids = set(os.path.splitext(os.path.basename(f))[0] for f in os.listdir(lidar_tif_folder) if f.endswith('.tif'))
+    # OSM_bridge_lines_gdf['has_lidar_tif'] = OSM_bridge_lines_gdf['osmid'].apply(lambda x: 'Y' if str(x) in tif_ids else 'N')
+
+    # ## TODO update the osm bridge lines fie (with a new name?) 
+    # base, ext = os.path.splitext(OSM_bridge_file) 
+    # OSM_bridge_lines_gdf.to_file(os.path.join(output_dir,f"{base}_modified{ext}"))
+
+    #make a dif file by integrating lidar enerated tifs and 3DEP DEM
+    # original_dem_path=r"../Data/HUC8_12090301_dem.tif" 
+    # original_dem_path=r"C:\Users\ali.forghani\Desktop\lidar\Data\HUC8_02050206_dem.tif"
+    # diff_dem_path=os.path.join(output_dir,'diff_dem_02050206.tif')
+    # lidar_tif_paths=list(glob.glob(os.path.join(lidar_tif_folder, '*.tif')))
+    # create_dem_diff(original_dem_path,lidar_tif_paths,diff_dem_path)
+
+
+
+
+
+
+if __name__ == "__main__":  
+
+    '''
+        Sample usage:
+       python create_osm_raster_using_lidar.py
+       -i osm_all_bridges.gpkg
+       -b 1.5
+       -r 3
+       -o /results/02050206
+    '''
+
+    parser = argparse.ArgumentParser(description='Download lidar points for buffered OSM bridges and make tif files')
+
+    parser.add_argument(
+        '-i', '--OSM_bridge_file', help='REQUIRED: A gpkg that contains the bridges lines', required=True
+    ) 
+
+    parser.add_argument(
+        '-b',
+        '--buffer_width',
+        help='OPTIONAL: Buffer to apply to OSM bridge lines to convert them to polygon. Default value is 1.5m (on each side)',
+        required=False,
+        default=1.5,
+        type=float,
+    )
+
+    parser.add_argument(
+        '-r',
+        '--raster_resolution',
+        help='OPTIONAL: Resolution of bridge raster files generated from lidar. Default value is 3m',
+        required=False,
+        default=3.0,
+        type=float,
+    )
+
+    parser.add_argument(
+        '-o',
+        '--output_dir',
+        help='REQUIRED: folder path where individual HUC8 results will be saved to.'
+        ' File names are hardcoded to format hucxxxxxxxx_bridges_lidar.',
+        required=True,
+    )
+
+
+    args = vars(parser.parse_args())
+
+    # try:
+    process_bridges_lidar_data(**args)
+
+    # except Exception:
+    #     print('something wrong')
+    #     #logging.info(traceback.format_exc())
+    #     end_time = dt.datetime.now(dt.timezone.utc)
+    #     #logging.info(f"   End time: {end_time.strftime('%m/%d/%Y %H:%M:%S')}")
+
+
+
+
+
+    
+
+
+    # #updated 3DEP DEM
+    # original_dem_path=r"../Data/HUC8_12090301_dem.tif" 
+    # enhanced_dem_path=os.path.join(output_dir,'enhanced','HUC8_12090301_dem_enhanced.tif')
+    # diff_dem_path=r"../Results\final\enhanced/diff_dem.tif"
+    # local_tif_paths=list(glob.glob(os.path.join(r'../Results/final/lidar_generated_tif', '*.tif')))
+    # create_dem_diff(original_dem_path,local_tif_paths,diff_dem_path)
+
+    # #record for which osmids we have a lidar-generated tif
+    # local_tif_ids = [int(os.path.splitext(os.path.basename(path))[0]) for path in local_tif_paths]
+    # intersection['has_lidar_tif'] = np.where(intersection['osmid'].isin(local_tif_ids),'Y','N')
+    # intersection.to_file('../Results/final/Intersected_with_lidar.gpkg')
+
+
+
+    # #add diff tif file into HAND GRID
+    # rem_path = r"../Data\rem_zeroed_masked_0.tif"
+    # updated_rem_path = r"../Results\final\enhanced\rem_updated.tif"
+    # add_diff_into_rem(rem_path,diff_dem_path,updated_rem_path)
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
---
+++ data/bridges/make_bridge_dem_dif_rasters.py
@@ -0,0 +1,236 @@
+
+
+import rasterio
+from rasterio.merge import merge
+import xarray as xr
+import rioxarray  # Extension of xarray for geospatial data
+import glob
+import os
+import time
+import argparse
+import geopandas as gpd
+
+
+def merge_bridge_tifs(lidar_tif_dir,HUC6_lidar_tif_osmids,merged_file_path):
+    
+    # Read and merge the lidar generated TIFF files
+    # lidar_tif_files=list(glob.glob(os.path.join(lidar_tif_dir, '*.tif')))
+
+    # HUC6_lidar_tif_osmids=OSM_bridge_lines_gdf[(OSM_bridge_lines_gdf['HUC6']==HUC6)&(OSM_bridge_lines_gdf['has_lidar_tif']=='Y') ]['osmid'].values.tolist()
+
+    
+
+    #keep the tif files within HUC6
+    # HUC6_gdf=gpd.read_file(os.path.join(huc6_dir,'HUC6_%s.gpkg'%HUC6))
+    # HUC6_gdf.to_crs(OSM_bridge_lines_gdf.crs, inplace=True)
+    # OSM_bridges_within_HUC6=OSM_bridge_lines_gdf.sjoin(HUC6_gdf,how="inner", predicate="within")['osmid'].values.astype(str)
+
+    # HUC6_lidar_tif_files =  [ tif_file for tif_file in lidar_tif_files if os.path.basename(tif_file).split('.')[0] in OSM_bridge_lines_gdf['HUC6'].values]
+
+    # if not HUC6_lidar_tif_osmids: 
+    #     return False
+    
+    # else:
+    src_files_to_mosaic = []
+
+    for osmid in HUC6_lidar_tif_osmids:
+        fp=os.path.join(lidar_tif_dir,'%s.tif'%str(osmid))
+        # print(fp)
+
+        src = rasterio.open(fp)
+        src_files_to_mosaic.append(src)
+
+    mosaic, out_trans = merge(src_files_to_mosaic)
+    # Close all the source files
+    for src in src_files_to_mosaic:
+        src.close()
+    # Create an empty raster that covers the entire domain
+    # Define the metadata
+    out_meta = src.meta.copy()
+    # out_meta.update({
+    #     "driver": "GTiff",
+    #     "height": mosaic.shape[1],
+    #     "width": mosaic.shape[2],
+    #     "transform": out_trans,
+    #     "crs": 'EPSG:3857'
+    # })
+
+    out_meta.update({
+            'driver': 'GTiff',
+            'dtype': 'float32',
+            'compress': 'LZW',
+            'tiled': True,
+            'blockxsize': 256,
+            'blockysize': 256,
+            'BIGTIFF': 'YES',
+            # 'nodata': original_nodata,
+            'width': mosaic.shape[2],
+            'height': mosaic.shape[1],
+            'transform': out_trans
+        })
+
+    with rasterio.open(merged_file_path, 'w', **out_meta) as dest:
+        dest.write(mosaic)
+
+
+    # return True
+
+def identify_bridges_with_lidar(OSM_bridge_lines_gdf,lidar_tif_dir):
+    #identify osmids with lidar-tif or not
+    
+    tif_ids = set(os.path.splitext(os.path.basename(f))[0] for f in os.listdir(lidar_tif_dir) if f.endswith('.tif'))
+    OSM_bridge_lines_gdf['has_lidar_tif'] = OSM_bridge_lines_gdf['osmid'].apply(lambda x: 'Y' if str(x) in tif_ids else 'N')
+    return OSM_bridge_lines_gdf
+
+
+def make_dif_ratsers(OSM_bridge_file,dem_dir,lidar_tif_dir,output_dir):
+    start_time = time.time()
+
+    #add HUC6 info update the osm bridge line file with existence of lidar 
+    OSM_bridge_lines_gdf=gpd.read_file(OSM_bridge_file)
+    OSM_bridge_lines_gdf['HUC6']=OSM_bridge_lines_gdf['HUC'].str[:6]
+    OSM_bridge_lines_gdf=identify_bridges_with_lidar(OSM_bridge_lines_gdf,lidar_tif_dir)
+    # OSM_bridge_lines_gdf[OSM_bridge_lines_gdf['has_lidar_tif']=='Y'].to_file('/outputs/Ali_bridges_lidar/final/Step4_make_diff_elev/test.gpkg')
+
+
+    dem_files=list(glob.glob(os.path.join(dem_dir, '*.tif')))
+    # dem_files=['/data/inputs/dems/3dep_dems/10m_5070/20240916/HUC6_020502_dem.tif']
+    # dem_files=[]
+    for dem_file in dem_files:
+        base_name, extension = os.path.splitext(os.path.basename(dem_file))
+        HUC6=base_name.split('_')[1]
+
+        #get alis t of osmids in this HUC6 with lidar-generated tif files
+        HUC6_lidar_tif_osmids=OSM_bridge_lines_gdf[(OSM_bridge_lines_gdf['HUC6']==HUC6)&(OSM_bridge_lines_gdf['has_lidar_tif']=='Y') ]['osmid'].values.tolist()
+
+        if HUC6_lidar_tif_osmids and len(HUC6_lidar_tif_osmids)>5:  #the second condition is just for debugging...remove it
+            print('making diff for HUC6_%s with %d tif'%(HUC6,len(HUC6_lidar_tif_osmids)))
+            output_file_name=f"{base_name}_diff{extension}"
+            merged_file_path=os.path.join(output_dir,'merged.tif')
+            print('merging')
+            merge_bridge_tifs(lidar_tif_dir,HUC6_lidar_tif_osmids,merged_file_path)
+
+            local_da = xr.open_dataarray(merged_file_path, engine="rasterio")
+            #remove the merged file which is not needed anymore
+            os.remove(os.path.join(output_dir,'merged.tif'))
+
+            # Retrieve profile from the original DEM file
+            with rasterio.open(dem_file) as src:
+                dem_profile = src.profile
+
+            # Open the regional TIFF as an xarray DataArray
+            original_da = xr.open_dataarray(dem_file, engine="rasterio")
+            original_nodata=original_da.rio.nodata
+            enhanced_da = original_da.copy()
+
+            # Open the local TIFF as an xarray DataArray and reproject to match the regional grid, if needed
+            print('reprojeting the merged')
+            if local_da.rio.crs != original_da.rio.crs:
+                local_da = local_da.rio.reproject_match(original_da) 
+            # Replace values in the regional DataArray with the local DataArray values at overlapping locations
+            enhanced_da = enhanced_da.where(local_da.isnull(), other=local_da)
+            # # Set nodata value to be consistent
+            enhanced_da=enhanced_da.fillna(original_nodata)
+            enhanced_da.rio.write_nodata(original_nodata, inplace=True)
+
+            diff = enhanced_da - original_da
+
+            # Save the result to a new TIFF file
+            # diff.rio.to_raster(os.path.join(output_dir,output_file_name),compress="LZW") first version
+            # Update the profile with the desired settings
+            # Ensure CRS is properly set for the output
+            if diff.rio.crs is None:
+                diff.rio.write_crs(original_da.rio.crs, inplace=True)
+
+            # Update the profile with the desired settings
+            output_profile = dem_profile.copy()
+            output_profile.update({
+                'driver': 'GTiff',
+                'dtype': 'float32',
+                'compress': 'LZW',
+                'tiled': True,
+                'blockxsize': 256,
+                'blockysize': 256,
+                'BIGTIFF': 'YES',
+                'nodata': original_nodata,
+                'width': original_da.rio.width,  # Use original DEM's width
+                'height': original_da.rio.height,  # Use original DEM's height
+                'transform': original_da.rio.transform()
+            })
+
+            # Ensure diff.values has the correct shape
+            data = diff.values
+            if data.ndim == 4:
+                data = data.squeeze()  # Remove extra dimensions if present
+            if data.ndim == 2:  # Single-band case (height, width)
+                data = data[None, :, :]  # Add band dimension
+
+            # Write the final output raster
+            output_file_path = os.path.join(output_dir, output_file_name)
+            with rasterio.open(output_file_path, 'w', **output_profile) as dst:
+                dst.write(data)
+
+            end_time = time.time()
+            print(f"Method2 time: {end_time - start_time:.4f} seconds")
+
+        else: #if there are no lidar raster for this HUC6, just return a zero diff file
+            print('There is no lidar-generated raster file for osm bridges in HUC%s'%HUC6)
+
+
+    # #save the osm bridge line file
+    base, ext = os.path.splitext(os.path.basename(OSM_bridge_file)) 
+    OSM_bridge_lines_gdf.to_file(os.path.join(output_dir,f"{base}_modified{ext}"))
+
+
+
+if __name__ == "__main__":  
+
+    '''
+        Sample usage:
+       python create_osm_raster_using_lidar.py
+       -i osm_all_bridges.gpkg
+       -b 1.5
+       -r 3
+       -o /results/02050206
+    '''
+
+    parser = argparse.ArgumentParser(description='Make bridge dem difference rasters')
+
+    parser.add_argument(
+        '-i', '--OSM_bridge_file', help='REQUIRED: A gpkg that contains the bridges lines', required=True
+    ) 
+
+    parser.add_argument(
+        '-d',
+        '--dem_dir',
+        help='REQUIRED: folder path where 3DEP dems are loated.',
+        required=True,
+    )
+
+    parser.add_argument(
+        '-l',
+        '--lidar_tif_dir',
+        help='REQUIRED: folder path where lidar-gerenared bridge elevtions are located.',
+        required=True,
+    )
+
+    # parser.add_argument(
+    #     '-huc',
+    #     '--huc6_dir',
+    #     help='REQUIRED: folder path where huc6 gpkg files are located.',
+    #     required=True,
+    # )
+
+
+    parser.add_argument(
+        '-o',
+        '--output_dir',
+        help='REQUIRED: folder path for output diff rasters.',
+        required=True,
+    )
+
+
+    args = vars(parser.parse_args())
+
+    make_dif_ratsers(**args)
+
