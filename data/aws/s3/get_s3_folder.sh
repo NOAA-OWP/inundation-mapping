4d317ebb0a9b6f3faafb5ebb4d993d83cfd19b5a
Rob Hanna <robert.hanna@noaa.gov>
2025-02-13 20:22:40 +0000

    filtered version complete

--- data/aws/s3/get_s3_folder.sh
+++ data/aws/s3/get_s3_folder.sh
@@ -3,10 +3,10 @@
 # Note: This is pretty rough with alot of hardcoding
 # and is used for rtx FIM_30 at this time
 
-# Remember to run the lastest 3 AWS key variables in the same window
+# Remember to run EXPORT lastest 3 AWS key variables in the same window
 # before running this.
 
-# As well as the export TRG_ROOT='/our efs drive name/>>>>>>(some path)/20241115'
+# As well as the export TRG_ROOT='/efs drive name/>>>>>>(some path)/20241115'
 # at command line prior to running the script
 
 :
@@ -48,8 +48,6 @@ Calc_Duration( ) {
     local end_time=`date +%s`
 
     local total_sec=$(( $end_time - $start_time ))
-    # local dur_hrs=$((total_sec / 3600 ))
-    # local dur_min=$((total_sec %3600 / 60))
     local dur_min=$((total_sec / 60))
     local dur_remainder_sec=$((total_sec %60))
     local dur_sec_percent=$((100*$dur_remainder_sec/60))
@@ -58,6 +56,9 @@ Calc_Duration( ) {
 
 }
 
+log_file_date=$(date +"%Y_%m_%d")
+log_file_path="${TRG_ROOT}/s3_download_log_${log_file_date}.txt"
+# echo "$log_file_path"
 
 dt_string=$(date +"%Y-%m-%d %H:%M:%S")
 log_msg="======================== \n"
@@ -69,9 +70,13 @@ log_msg="${log_msg}model_name: ${model_name} \n"
 
 t_start=`date +%s`
 
-sleep 20
-# aws s3 sync s3://fimc-data/ripple/30_pcnt_domain/collections/${model_name} ${trg}/ ; date
+# sleep 20
+cli_args=" --exclude 'library/*' --exclude 'qc/*' --exclude 'submodels/*' --exclude 'source_models/*' "
+cli_args="$cli_args --exclude '*-shm' --exclude '*-wal' "
+
+aws s3 sync s3://fimc-data/ripple/30_pcnt_domain/collections/${model_name} ${trg}/ --exclude '*' --include 'library_extent/*' --include 'qc/*' --include '*.xlsx' --include 'ripple.gpkg' --include 'start_reaches.csv'
 echo "AWS download done"
+date
 
 dur=$(Calc_Duration $t_start)
 echo "Duration (in percent minutes): $dur min"
@@ -83,26 +88,9 @@ disk_usage="$(du -sh  $trg | cut -f1)"
 echo "Total Size: $disk_usage"
 log_msg="${log_msg}Total size: $disk_usage \n"
 
-#folder_size_path_removed=$((disk_usage / 10000000))
-#folder_size_path_removed=${disk_usage#"$trg"}
-# echo "Total size: $folder_size_path_removed"
-
-#disk_usage_in_k=$(du -b ${trg})
-#echo "$disk_usage_in_k"
-#dec_num=$(printf "'%.2f\n' $disk_usage_in_k / 100000000" | bc)
-size=`du -b --max-depth=0  $trg | cut -f1`
-echo $size
-#printf '%.3f\n' $(echo "35/3600" | bc -l)
-# printf '%.2f\n' $(echo $size /100000000 | bc -l)
-#framename=$(printf 'frame_%03d' $framenr)
-#framename=$(printf 'frame_%03d' $framenr)printf '%.2f\n' $(echo $size /100000000 | bc -l
-f_size=$(printf '%.2f\n' $(echo $size /100000000 | bc -l))
-echo $f_size
-#dec_num=$(echo "scale=2; $disk_usage / 10000000" | bc)
-#echo "my dec is $dec_num"
-
-#log_msg="${log_msg}Total size: $disk_usage \n"
-# echo "$log_msg"
+size_in_kb=`du -b --max-depth=0  $trg | cut -f1`
+f_size=$(printf '%.4f\n' $(echo $size_in_kb /100000000 | bc -l))
+echo "Folder Size in GiB: $f_size"
 
 # find ${trg}/library_extent/ -mindepth 1 -maxdepth 1 -type d | wc -l | xargs echo "Total folders: "
 folder_count=$(find ${trg}/library_extent/ -mindepth 1 -maxdepth 1 -type d | wc -l)
@@ -110,15 +98,14 @@ echo "Total folders: $folder_count"
 log_msg="${log_msg}Total folders: $folder_count"
 
 echo
-echo "$log_msg" >> ${TRG_ROOT}/s3_download_log.txt
+echo "$log_msg" >> ${log_file_path}
 
 # Now build up the stats line for the log
-#foo=${string#"$prefix"}
-#"${Test/Today/$Date}"
-#folder_size_path_removed=${disk_usage#""}
-stats="$model_name, $f_size, $folder_count, $dur"
+# This one is a simple csv, no headers.
+download_date=$(date +"%m/%d/%Y")
+stats="$model_name,$f_size,$dur,$folder_count,$download_date"
 echo $stats
-echo "$stats" >> ${TRG_ROOT}/s3_download_log.txt
+csv_file_path="${TRG_ROOT}/s3_download_stats_${log_file_date}.csv"
+echo "$stats" >> ${csv_file_path}
 echo "Model download complete"
 echo
-
