2a4f51634b35906f4a8bec72f3f9a2e2e79e029c
Rob Hanna <robert.hanna@noaa.gov>
2025-02-19 19:05:14 +0000

    update notes

--- data/aws/s3/get_s3_folder.sh
+++ data/aws/s3/get_s3_folder.sh
@@ -1,13 +1,28 @@
 #!/bin/bash -e
 
+
 # Note: This is pretty rough with alot of hardcoding
 # and is used for rtx FIM_30 at this time
+
 
 # Remember to run EXPORT lastest 3 AWS key variables in the same window
 # before running this.
 
 # As well as the export TRG_ROOT='/efs drive name/>>>>>>(some path)/20241115'
 # at command line prior to running the script
+
+
+# NOTE: This solution is not complete. This script loads just one S3 folder at a time.
+# I will build a wrapper script that can feed in a whole list and jsut iterate through these
+# The key is trapping errors which has proven to be many and getting stats on it as well.
+
+# This also could be change to python scripts... TBD
+# We also could jsut pulll down everyting in the remote S3 bucket in one large pull
+# but it is prone to timing out and erroring with such a large pull
+
+# Besides.. this filters out just folders and files we need, dropping the total download
+# size by appx 80%.
+
 
 :
 usage_msg()
