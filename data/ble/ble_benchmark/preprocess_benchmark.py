1ab9a6de004dddfe67f88443869c1b6c4f4cabcb
MattLuck-NOAA <matt.luck@noaa.gov>
2025-02-21 11:53:26 -0700

    revert preprocess_benchmark.py (with rasterio)

--- data/ble/ble_benchmark/preprocess_benchmark.py
+++ data/ble/ble_benchmark/preprocess_benchmark.py
@@ -3,7 +3,9 @@
 import argparse
 
 import numpy as np
-from osgeo import gdal
+import rasterio
+import rasterio.mask
+from rasterio.warp import Resampling, calculate_default_transform, reproject
 
 
 def preprocess_benchmark_static(benchmark_raster, reference_raster, out_raster_path=None):
@@ -31,83 +33,60 @@ def preprocess_benchmark_static(benchmark_raster, reference_raster, out_raster_p
             (required for writing to output dataset).
 
     '''
-    # # Set arbitrary no data value that is not possible value of the benchmark dataset. It is reassigned later.
-    # nodata_value = -2147483648
-
     # Open and read raster and benchmark rasters
-    reference = gdal.Open(reference_raster)
-    benchmark = gdal.Open(benchmark_raster)
+    reference = rasterio.open(reference_raster)
+    benchmark = rasterio.open(benchmark_raster)
+    benchmark_arr = benchmark.read(1)
 
-    ulx, reference_xRes, xskew, uly, yskew, reference_yRes = reference.GetGeoTransform()
+    # Set arbitrary no data value that is not possible value of the benchmark dataset. It is reassigned later.
+    nodata_value = -2147483648
 
-    # ulx, xres, xskew, uly, yskew, yres  = benchmark.GetGeoTransform()
-    # sizeX = benchmark.RasterXSize * xres
-    # sizeY = benchmark.RasterYSize * yres
-    # lrx = ulx + sizeX
-    # lry = uly + sizeY
-
-    gdal.Warp(
-        out_raster_path,
-        benchmark,
-        outputType=gdal.GDT_Float32,
-        dstSRS=reference.GetProjection(),
-        xRes=reference_xRes,
-        yRes=reference_yRes,
-        resampleAlg=gdal.GRA_Bilinear,
+    # Determine the new transform and dimensions of reprojected/resampled raster
+    new_transform, new_width, new_height = calculate_default_transform(
+        benchmark.crs,
+        reference.crs,
+        benchmark.width,
+        benchmark.height,
+        *benchmark.bounds,
+        resolution=reference.res,
     )
 
-    #  outputBounds=[ulx, lry, lrx, uly],
-    #  outputBoundsSRS=benchmark.GetProjection(),
-    #  resampleAlg=gdal.GRA_Bilinear,
-    #  multithread=True)
+    # Define an empty array that is same dimensions as output by the "calculate_default_transform" command
+    benchmark_projected = np.empty((new_height, new_width), dtype=np.int32)
 
-    # # Determine the new transform and dimensions of reprojected/resampled raster
-    # new_transform, new_width, new_height = calculate_default_transform(
-    #     benchmark.crs,
-    #     reference.crs,
-    #     benchmark.width,
-    #     benchmark.height,
-    #     *benchmark.bounds,
-    #     resolution=reference.res,
-    # )
+    # Reproject and resample the benchmark dataset. Bilinear resampling due to continuous depth data
+    reproject(
+        benchmark_arr,
+        destination=benchmark_projected,
+        src_transform=benchmark.transform,
+        src_crs=benchmark.crs,
+        src_nodata=benchmark.nodata,
+        dst_transform=new_transform,
+        dst_crs=reference.crs,
+        dst_nodata=nodata_value,
+        dst_resolution=reference.res,
+        resampling=Resampling.bilinear,
+    )
 
-    # # Define an empty array that is same dimensions as output by the "calculate_default_transform" command
-    # benchmark_projected = np.empty((new_height, new_width), dtype=np.int32)
+    # Convert entire depth grid to boolean (1 = Flood, 0 = No Flood)
+    boolean_benchmark = np.where(benchmark_projected != nodata_value, 1, 0)
 
-    # # Reproject and resample the benchmark dataset. Bilinear resampling due to continuous depth data
-    # reproject(
-    #     benchmark_arr,
-    #     destination=benchmark_projected,
-    #     src_transform=benchmark.transform,
-    #     src_crs=benchmark.crs,
-    #     src_nodata=benchmark.nodata,
-    #     dst_transform=new_transform,
-    #     dst_crs=reference.crs,
-    #     dst_nodata=nodata_value,
-    #     dst_resolution=reference.res,
-    #     resampling=Resampling.bilinear,
-    # )
+    # Update profile (data type, NODATA, transform, width/height)
+    profile = reference.profile
+    profile.update(transform=new_transform)
+    profile.update(dtype=rasterio.int8)
+    # Update NODATA to some integer so we can keep int8 datatype. There are no NODATA in the raster dataset
+    profile.update(nodata=2)
+    profile.update(width=new_width)
+    profile.update(height=new_height)
 
-    # profile = reference.profile
-
-    # # Convert entire depth grid to boolean (1 = Flood, 0 = No Flood)
-    # boolean_benchmark = np.where(benchmark_projected != nodata_value, 1, 0)
-
-    # # Update profile (data type, NODATA, transform, width/height)
-    # profile.update(transform=new_transform)
-    # profile.update(dtype=rasterio.int8)
-    # # Update NODATA to some integer so we can keep int8 datatype. There are no NODATA in the raster dataset
-    # profile.update(nodata=2)
-    # profile.update(width=new_width)
-    # profile.update(height=new_height)
-
-    # # Write out preprocessed benchmark array to raster if path is supplied
-    # if out_raster_path is not None:
-    #     with rasterio.Env():
-    #         # Write out reassigned values to raster dataset
-    #         with rasterio.open(out_raster_path, 'w', **profile) as dst:
-    #             dst.write(boolean_benchmark.astype('int8'), 1)
-    # return boolean_benchmark.astype('int8'), profile
+    # Write out preprocessed benchmark array to raster if path is supplied
+    if out_raster_path is not None:
+        with rasterio.Env():
+            # Write out reassigned values to raster dataset
+            with rasterio.open(out_raster_path, 'w', **profile) as dst:
+                dst.write(boolean_benchmark.astype('int8'), 1)
+    return boolean_benchmark.astype('int8'), profile
 
 
 if __name__ == '__main__':
