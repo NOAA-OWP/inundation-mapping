971bfe8afa382d621eb621c93dbba48347b73875
EmilyDeardorff-NOAA <Emily.Deardorff@NOAA.gov>
2025-02-24 23:35:21 +0000

    Overhaul catfim_sites_compare to compare 2+ versions into a cleaner csv.

--- tools/catfim/catfim_sites_compare.py
+++ tools/catfim/catfim_sites_compare.py
@@ -5,215 +5,249 @@ import os
 import sys
 import traceback
 from datetime import datetime, timezone
-
 import pandas as pd
+import re
 
-import utils.fim_logger as fl
+pd.options.mode.chained_assignment = None  # default='warn'
 
+# import utils.fim_logger as fl
+# FLOG = fl.FIM_logger()  # the non mp version
 
-# global RLOG
-FLOG = fl.FIM_logger()  # the non mp version
+''' 
+This tool compares two or more versions of the CatFIM output site CSV.
 
+It will create separate output CSV for flow- and stage-based version comparisons with the following columns:
+- site_id
+- <version>_site_processed
+- <version>_catfim_mapped
+- <version>_status
 
-### sorry.. this is a bit ugly and you need to comment, or adjust column names
-# based on what you are loading. (for now)
+It will auto overwrite output files already existing.
 
-"""
-This tool can compare two version of the "sites" csv"s (not the poly libray files at this time).
-It is intended to compare a previous "sites" csv to a new one to see what changes have appeared
-with sites.
+Potential Upgrades TODO: 
+- Could add a flag saying to save differences only.
+- Could add a flag to save a log file.
+- Could add a flag to save a summary of the differences.
+- Could implement sorting by version order in the output CSV.
+- Could include HUC in output CSV.
 
-Rules:
-- see if one of the two files has extra sites
-- And  yes.. for now, it assumes both of those sets of columns exist in both files
-- compares the following columns at this time:  Note: Not geometry at this time
-   - ahps_id
-   - nws_data_name
-   - HUC8
-   - mapped
-   - status
+'''
 
-- It will display both sets of columns for each set, then a column with a TRUE/FALSE, if the
-  each column matches.
-- You optionallly can add a flag saying differences only.
-- It will auto overwrite output files already existing.
+# Function that compiles CatFIM sites based on an input path list
+def compile_catfim_sites(sorted_path_list):
+    '''
+    Inputs: 
+    - sorted_path_list: a list of string with paths to CatFIM runs (which should already be sorted in to flow-based or stage-based)
 
-"""
+    Outputs: 
+    - combined_sites_df
+    '''
 
+    print(f'Results to compile: {sorted_path_list}')
+    
+    for path in sorted_path_list:
+    
+        # Create mapping filepath and check that mapping folder exists
+        mapping_path = os.path.join(path, 'mapping')
+        if not os.path.exists(mapping_path):
+            print(f'WARNING: Filepath does not exist for mapping folder: {mapping_path}')
+            continue
+    
+        # Get the CSV filename and check that it exists
+        csv_path = None
+        for filename in os.listdir(mapping_path):
+            if filename.endswith('sites.csv'):
+                csv_path = os.path.join(mapping_path, filename)
+    
+        if csv_path == None:
+            print(f'WARNING: No CSV path found for input path {path}')
+            continue
+    
+        # Read in site CSV
+        sites_df = pd.read_csv(csv_path)
+    
+        # Reconcile site ID column name
+        if 'ahps_lid' in sites_df.columns:
+            sites_df['site_id'] = sites_df['ahps_lid']
+        elif 'nws_lid' in sites_df.columns:
+            sites_df['site_id'] = sites_df['nws_lid']
+        else:
+            print(f'WARNING: Did not find ahps_lid or nws_lid column in {csv_path}')
+            continue
+        
+        # Make a new df with only the needed columns
+        sites_df['site_processed'] = 'yes'
+        # trimmed_sites_df = sites_df[['site_id', 'HUC8', 'name', 'states', 'mapped', 'status']] # maybe add this additional data later, or have this already in the appended data?
+        trimmed_sites_df = sites_df[['site_id', 'site_processed', 'mapped', 'status']] # maybe add this additional data later
+    
+        # Extract version_id from the path
+        match = version_id = re.search(r'(hand|fim)_(\d+_\d+_\d+_\d+)', path)
+        if match:
+            version_id = match.group(2)
+        else:
+            print(f'WARNING: Unable to extract version ID from {path}')
+            continue
+    
+        # Rename status 'mapped' and 'status' columns to have the version_id 
+        trimmed_sites_df.rename(columns={'mapped': f'{version_id}_catfim_mapped', 'status': f'{version_id}_status', 
+                                        'site_processed': f'{version_id}_site_processed'}, inplace=True)
+    
+        # If combined_sites_df exists already, do an outer join to add this one
+        # If it doesn't already exist, set this trimmed_sites_df to be combined_sites_df
+        try:
+            combined_sites_df
+            # If 'combined_sites_df' exists, perform an outer join with 'trimmed_sites_df'
+            combined_sites_df = pd.merge(combined_sites_df, trimmed_sites_df, how='outer', on='site_id')
+        except NameError:
+            # If 'combined_sites_df' doesn't exist, assign 'trimmed_sites_df' to it
+            combined_sites_df = trimmed_sites_df
+        # End path loop
 
-def compare_sites(prev_file, new_file, output_file):
+    # Loop through columns and fill in details for NA columns
+    for col in combined_sites_df.columns:
+        if 'site_processed' in col:
+            # Fill with 'no' where the value is not 'yes'
+            combined_sites_df[col] = combined_sites_df[col].apply(lambda x: 'yes' if x == 'yes' else 'no')
+            
+        elif 'catfim_mapped' in col:
+            # Fill with 'no' where the value is not 'yes'
+            combined_sites_df[col] = combined_sites_df[col].apply(lambda x: 'yes' if x == 'yes' else 'no')
+            
+        elif 'status' in col:
+            # Get the version ID from the 'status' column
+            version_id = col.replace('_status', '')
+            
+            # Construct the corresponding 'site_processed' column name
+            site_processed_col = f'{version_id}_site_processed'
+            
+            # Check where the 'status' is NaN and 'site_processed' is 'no'
+            combined_sites_df[col] = combined_sites_df.apply(
+                lambda row: f'Site not processed in {version_id}. See release notes.' 
+                if pd.isna(row[col]) and row[site_processed_col] == 'no' 
+                else row[col],
+                axis=1
+            )
+    # End column loop
+            
+    return combined_sites_df
 
-    # Validation
-    if not os.path.exists(prev_file):
-        print(f"The -p (prev file) does not exist. {prev_file} ")
-        sys.exit(1)
+# Main function for comparing CatFIM sites
+def main(path_list, output_save_filepath):
+    '''
+    Inputs
+    - path_list:
+    - output_save_filepath
 
-    if not os.path.exists(new_file):
-        print(f"The -n (new file) does not exist. {new_file} ")
-        sys.exit(1)
+    Outputs
+    - saves a CSV to the output_save_filepath
 
-    output_folder = os.path.dirname(output_file)
-    if not os.path.exists(output_folder):
-        print("While the output file does not need to exist, the output folder does.")
-        sys.exit(1)
+    '''
 
-    # Setup variables
+    # Verify that output save path exists
+    if not os.path.exists(output_save_filepath):
+        sys.exit(f'ERROR: Output save path does not exist: {output_save_filepath}.')
+    
+    # Start stopwatch
     overall_start_time = datetime.now(timezone.utc)
     dt_string = overall_start_time.strftime("%m/%d/%Y %H:%M:%S")
 
-    # setup logging system
-    log_file_name = f"compare_log_file_{overall_start_time.strftime('%Y_%m_%d__%H_%M_%S')}"
-    log_path = os.path.join(output_folder, log_file_name)
-    FLOG.setup(log_path)
+    # NOTE: Removed logging file for now, since this is such a simple script. 
+    # Can implement later if needed by replacing print with FLOG.lprint
+    # # Set up logging system 
+    # log_file_name = f"compare_log_file_{overall_start_time.strftime('%Y_%m_%d__%H_%M_%S')}"
+    # log_path = os.path.join(output_save_filepath, log_file_name)
+    # FLOG.setup(log_path)
 
-    FLOG.lprint("================================")
-    FLOG.lprint(f"Start sites compare - (UTC): {dt_string}")
-    FLOG.lprint("")
-    # FLOG.lprint("Input arguments:")
-    # FLOG.lprint(locals())
-    # FLOG.lprint("")
+    print('================================')
+    print(f'Start CatFIM site comparison - (UTC): {dt_string}')
+    print('')
 
-    # Load both files
-    print("loading previous file")
+    # Separate path list into flow- and stage-based lists
+    # Initialize empty lists for stage and flow
+    stage_path_list = []
+    flow_path_list = []
+    
+    # Separate space-delimited list into list
+    path_list = path_list.split()
 
-    # Column names to compare   (for 4.4.0.0 and 4.5.11.1, the columns were named ahps_lid for stage)
-    # flow is / was always just ahps_lid
-    # col_names = ["nws_lid", "nws_data_name", "HUC8", "mapped", "status"]
+    # Loop through the path_list and categorize the paths
+    for path in path_list:
+        if 'stage' in path:
+            stage_path_list.append(path)
+        elif 'flow' in path:
+            flow_path_list.append(path)
+        else:
+            print(f'WARNING: Unable to process path that does not contain "stage" or "flow": {path}')
+    
+    # Run site compilation for stage-based CatFIM
+    if len(stage_path_list) != 0:
+        print('--------- Compiling stage-based CatFIM sites ---------')
+        stage_based_combined_sites_df = compile_catfim_sites(stage_path_list)
+        
+        # Save stage-based outputs
+        out_save_path = os.path.join(output_save_filepath, 'stage_based_site_organizer.csv')
+        stage_based_combined_sites_df.to_csv(out_save_path, index=False)
+        print(f'Stage-based outputs saved to {out_save_path}')
+        print()
 
-    # Column names to compare   (for 4.5.11.1, the columns were named ahps_lid for stage)
-    col_names = ["ahps_lid", "nws_data_name", "HUC8", "mapped", "status"]
-
-    # If not, load the columns, the rename to match
-    prev_df = pd.read_csv(prev_file, usecols=col_names)
-    if "nws_lid" in prev_df.columns:
-        prev_df.rename(columns={'nws_lid': 'ahps_lid'}, inplace=True)
-    # change huc8 to string and order
-    prev_df.HUC8.astype(str)
-    prev_df.HUC8 = prev_df.HUC8.astype('str').str.zfill(8)
-    prev_df = prev_df.sort_values(by=['ahps_lid'])
-    prev_df = prev_df.add_prefix("prev_data_")  # rename all columns to add the suffix of "prev_"
-    # prev_df.to_csv(output_file)
-
-    print("loading new file")
-    # This has a bug. in 4.5.2.11 stage the column was named 'nws_lid'
-    # but for future versions, it will be named "ahps_lid"
-
-    # 4.5.2.11 stage
-    # new_col_names = ["nws_lid", "nws_data_name", "HUC8", "mapped", "status"]
-
-    # All flow ones are ahps_lid  and 4.5.11.1 stage is ahps_lid
-    new_col_names = ["ahps_lid", "nws_data_name", "HUC8", "mapped", "status"]
-    new_df = pd.read_csv(new_file, usecols=new_col_names)
-    # to get it in sync with the prev column names
-    if "nws_lid" in new_df.columns:
-        new_df.rename(columns={'nws_lid': 'ahps_lid'}, inplace=True)
-
-    new_df.HUC8.astype(str)
-    new_df.HUC8 = new_df.HUC8.astype('str').str.zfill(8)
-    new_df = new_df.sort_values(by=['ahps_lid'])
-    new_df = new_df.add_prefix("new_data_")  # rename all columns to add the suffix of "new_"
-
-    # print("Prev")
-    # print(prev_df) # prev_df.loc[0]
-    # print()
-
-    # print("new")
-    # print(new_df)
-    # print()
-
-    col_names = ["ahps_lid", "nws_data_name", "HUC8", "mapped", "status"]
-    compare_data(prev_df, new_df, output_file, col_names)
+    # Run site compilation for flow-based CatFIM
+    if len(flow_path_list) != 0:
+        print('--------- Compiling flow-based CatFIM sites ---------')
+        flow_based_combined_sites_df = compile_catfim_sites(flow_path_list)
+        
+        # Save flow-based outputs
+        out_save_path = os.path.join(output_save_filepath, 'flow_based_site_organizer.csv')
+        flow_based_combined_sites_df.to_csv(out_save_path, index=False)
+        print(f'Flow-based outputs saved to {out_save_path}')
+        print()
 
     # Wrap up
     overall_end_time = datetime.now(timezone.utc)
-    FLOG.lprint("================================")
+    print()
+    print('================================')
     dt_string = overall_end_time.strftime("%m/%d/%Y %H:%M:%S")
-    FLOG.lprint(f"End sites compare - (UTC): {dt_string}")
+    print(f'End sites compare - (UTC): {dt_string}')
 
-    # calculate duration
+    # Calculate duration
     time_duration = overall_end_time - overall_start_time
-    FLOG.lprint(f"Duration: {str(time_duration).split('.')[0]}")
+    print(f"Duration: {str(time_duration).split('.')[0]}")
 
-    return  # helps show the end of the def
-
-
-def compare_data(prev_df, new_df, output_file, col_names):
-
-    # find the matching recs based on aphs_id, and gets all of the prev_dfs recs
-    results_df = prev_df.merge(
-        new_df, left_on='prev_data_ahps_lid', right_on='new_data_ahps_lid', how='outer'
-    )
-
-    # strip out line breaks from the two status columns
-    results_df["prev_data_status"] = results_df["prev_data_status"].replace("\n", "")
-    results_df["new_data_status"] = results_df["new_data_status"].replace("\n", "")
-
-    for col in col_names:
-        # add compare columns and sets defaults
-        results_df[f"does_{col}_match"] = results_df[f"prev_data_{col}"] == results_df[f"new_data_{col}"]
-
-    # for 4.4.0.0, almost all stage status columns have the phrase "usgs_elev_table missing, "
-    # remove that from the front of prev_if applicable (the space on the end is critical)
-
-    results_df["prev_data_status"] = results_df["prev_data_status"].str.replace(
-        "usgs_elev_table missing, ", ""
-    )
-    # results_df["is_match_status_adj_status"] = results_df["prev_data_adj_status"] == results_df["new_status"]
-
-    # 4.5.2.11. status was the word "OK" and 4.4.0.0, now in 4.5.11.1 it is the word "Good"
-    # results_df["does_status_match"] = results_df[f"does_{col}_match"] == False & results_df["prev_data_status"] == "OK" & results_df["new_data_status"] == "Good"
-
-    # for col in col_names:
-    #     results_df[f"has_{col}"] = False
-
-    results_df.to_csv(output_file)
-
-    return  # helps show the end of the def
+    return
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    """
-    This tool can compare two version of the catfim sites csv's (not the poly libray files at this time).
-    It is intended to compare a previous sites csv to a new one to see what changes have appeared
-    with sites.
-    More details at the top of this file
+    '''
+    This tool compares multiple versions of the CatFIM output site CSV. 
 
     It will auto overwrite output files already existing.
-    """
-
-    """
-    Sample
+    
+    Sample usage: 
     python /foss_fim/tools/catfim_sites_compare.py
-    -p /data/catfim/fim_4_4_0_0_stage_based/mapping/stage_based_catfim_sites.csv
-    -n /data/catfim/fim_4_5_2_11_stage_based/mapping/stage_based_catfim_sites.csv
-    -o /data/catfim/fim_4_5_2_11_stage_based/4_4_0_0__4_5_2_11_sites_comparison.csv
-    """
+    -p  '/data/catfim/hand_4_5_11_1_stage_based/ /data/catfim/fim_4_5_2_11_stage_based/ /data/catfim/fim_4_4_0_0_stage_based/ /data/catfim/hand_4_5_11_1_flow_based/ /data/catfim/fim_4_5_2_11_flow_based/ /data/catfim/fim_4_5_2_0_flow_based/'
+    -o '/home/emily.deardorff/notebooks/'
+    '''
 
     # Parse arguments
-    parser = argparse.ArgumentParser(description="Run CatFIM sites comparison")
+    parser = argparse.ArgumentParser(description='Run CatFIM sites comparison.')
 
-    parser.add_argument(
-        "-p",
-        "--prev-file",
-        help="Path to the prev (or any one) of the CatFIM ahps sites csv's",
+    parser.add_argument( # NOTE: Should this be a textfile input?
+        '-p', 
+        '--path-list',
+        help='REQUIRED: Space-delimited list of CatFIM output paths from which to compile sites.',
         required=True,
     )
 
     parser.add_argument(
-        "-n", "--new-file", help="Path to the new (or any one) of the 'sites' csv", required=True
-    )
-
-    parser.add_argument(
-        "-o", "--output-file", help="Path to where the results file will be saved", required=True
+        '-o', '--output-save-filepath', 
+        help='REQUIRED: Path to where the results files will be saved.', 
+        required=True
     )
 
     args = vars(parser.parse_args())
 
     try:
-
-        # call main program
-        compare_sites(**args)
+        main(**args)
 
     except Exception:
-        FLOG.critical(traceback.format_exc())
+        print(traceback.format_exc())
