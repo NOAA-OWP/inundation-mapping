35e9ba9ed9648830ba011961e510c8e67322cf05
MattLuck-NOAA <matt.luck@noaa.gov>
2025-04-09 06:27:28 -0600

    Extend subtraction grid to branch polygon buffer

--- src/adjust_floodplains.py
+++ src/adjust_floodplains.py
@@ -34,36 +34,39 @@ def adjust_floodplains(
         dem = dem_src.read(1)
         dem_nodata = dem_src.nodata
 
-    branch_polys = gpd.read_file(branch_polygons)
-    branch_poly = branch_polys[branch_polys['levpa_id'] == branch_id]
+    # branch_polys = gpd.read_file(branch_polygons)
+    # branch_poly = branch_polys[branch_polys['levpa_id'] == branch_id]
 
-    fema_flood_zones = gpd.read_file(fema_flood_zones_file)
+    # fema_flood_zones = gpd.read_file(fema_flood_zones_file)
 
-    # Clip the FEMA flood zones to the branch polygon
-    fema_flood_zones_clipped = gpd.clip(fema_flood_zones, branch_poly)
+    # # Clip the FEMA flood zones to the branch polygon
+    # fema_flood_zones_clipped = gpd.clip(fema_flood_zones, branch_poly)
 
-    # Mask the distance raster with fema_flood_zones_clipped
-    distance_mask = np.zeros_like(distance)
-    for geom in fema_flood_zones_clipped.geometry:
-        mask = rio.features.geometry_mask(
-            [geom], out_shape=distance.shape, transform=src.transform, invert=True
-        )
-        distance_mask[mask] = 1
-    distance = np.where(distance_mask == 1, distance, np.nan)
+    # # Mask the distance raster with fema_flood_zones_clipped
+    # distance_mask = np.zeros_like(distance)
+    # for geom in fema_flood_zones_clipped.geometry:
+    #     mask = rio.features.geometry_mask(
+    #         [geom], out_shape=distance.shape, transform=src.transform, invert=True
+    #     )
+    #     distance_mask[mask] = 1
+    # distance = np.where(distance_mask == 1, distance, np.nan)
 
-    # Save distance raster
-    with rio.open(distance_file, 'w', **profile) as dst:
-        dst.write(distance.astype(rio.float32), 1)
+    # # Save distance raster
+    # with rio.open(distance_file, 'w', **profile) as dst:
+    #     dst.write(distance.astype(rio.float32), 1)
 
-    # Calculate the mean and standard deviation of the distance
-    zs = zonal_stats(fema_flood_zones_clipped, distance_file, stats=['mean', 'std'])
+    # # Calculate the mean and standard deviation of the distance
+    # zs = zonal_stats(fema_flood_zones_clipped, distance_file, stats=['mean', 'std'])
 
-    distance_mean = zs[0]['mean']
-    distance_std = zs[0]['std']
-    distance_threshold = distance_mean + distance_std
+    # distance_mean = zs[0]['mean']
+    # distance_std = zs[0]['std']
+    # distance_threshold = distance_mean + distance_std
 
-    with rio.open(distance_file) as src:
-        distance = src.read(1)
+    # with rio.open(distance_file) as src:
+    #     distance = src.read(1)
+
+    distance_threshold = 7000.0
+    z_factor = z_factor * distance_threshold / 1000.0
 
     # Limit the distance to the mean + 1 std
     distance = np.where(distance <= distance_threshold, distance, np.nan)
--- src/src_adjust_spatial_obs.py
+++ src/src_adjust_spatial_obs.py
@@ -183,7 +183,14 @@ def find_hucs_with_points(points_file_dir, fim_out_huc_list):
     <fim_out_huc_list> that contain calibration point data.
     '''
 
-    files_in_points_file_dir = os.listdir(points_file_dir)
+    try:
+        files_in_points_file_dir = os.listdir(points_file_dir)
+    except FileNotFoundError:
+        print(
+            f"ERROR: Could not find the input calibration points directory: {points_file_dir}. "
+            "Please check the path and try again."
+        )
+        return []
 
     # Use list comprehension to slice .parquet off filename, and also prune non-parquet files in directory
     hucs_in_points_file_dir = [i[:-8] for i in files_in_points_file_dir if i.endswith('.parquet')]
