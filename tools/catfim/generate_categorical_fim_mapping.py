c8a64ec8aa499f0888ea8f752c4b0f5ad88e3f1d
EmilyDeardorff-NOAA <emily.deardorff@noaa.gov>
2025-04-10 21:28:53 +0000

    Linting.

--- tools/catfim/generate_categorical_fim_mapping.py
+++ tools/catfim/generate_categorical_fim_mapping.py
@@ -584,7 +584,9 @@ def run_inundation(
         # TODO: Update to only run if lake detected?
         with rasterio.open(output_extent_tif, 'r+') as output_extent_src:
             output_extent_array = output_extent_src.read(1)
-            output_extent_array_masked, mask_status = mask_out_lakes(output_extent_array, huc, output_extent_src, fim_run_dir)
+            output_extent_array_masked, mask_status = mask_out_lakes(
+                output_extent_array, huc, output_extent_src, fim_run_dir
+            )
             output_extent_src.write(output_extent_array_masked, 1)
 
         MP_LOG.trace(f"Lake masking complete for {huc} : {ahps_site} : {magnitude}")
