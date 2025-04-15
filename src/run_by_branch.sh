10e233064cebd5760a3a6bf0d4cd0934b32490d7
MattLuck-NOAA <matt.luck@noaa.gov>
2025-04-15 10:11:09 -0600

    Fix input parameters

--- src/run_by_branch.sh
+++ src/run_by_branch.sh
@@ -95,7 +95,7 @@ $srcDir/adjust_floodplains.py -d $current_branch_id \
     # -p $tempHucDataDir/branch_polygons.gpkg \
     # -b $current_branch_id \
     -o $tempCurrentBranchDataDir/dem_burned_$current_branch_id.tif \
-    -z 50 \
+    -z 50
     # -f /data/inputs/fema/nfhl/nfhl_flood_hazard_zones_${hucNumber}_100yr.gpkg
 
 ## PIT REMOVE BURNED DEM - BRANCH 0 (include all NWM streams) ##
