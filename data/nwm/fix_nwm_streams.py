366c076e887905da92f25fcb531e1ce33cd500a2
MattLuck-NOAA <matt.luck@noaa.gov>
2025-05-13 11:34:50 -0600

    Make input_fema_flood_hazard_zones an env variable

--- data/nwm/fix_nwm_streams.py
+++ data/nwm/fix_nwm_streams.py
@@ -3,14 +3,18 @@ import os
 import geopandas as gpd
 
 
-ids = [20906989, 20906985, 20906869]
-# dirname = '/data/inputs/nwm_hydrofabric/'
-# filename_in = os.path.join(dirname, '/nwm_flows.gpkg')
-# filename_out = os.path.join(dirname, 'nwm_flows_20250328.gpkg')
+### This is a simple script is used to fix a few of the NWM streams that have coordinates
+### in the wrong order. It is intended as a one-time fix for the input NWM streams data.
 
-dirname = '/data/inputs/pre_clip_huc8/20250218/11070203'  # previously 20241002
-filename_in = os.path.join(dirname, 'nwm_subset_streams.gpkg')
-filename_out = os.path.join(dirname, 'nwm_subset_streams_20250328.gpkg')
+ids = [20906989, 20906985, 20906869]
+dirname = '/data/inputs/nwm_hydrofabric/'
+filename_in = os.path.join(dirname, '/nwm_flows.gpkg')
+filename_out = os.path.join(dirname, 'nwm_flows_20250328.gpkg')
+
+# dirname = '/data/inputs/pre_clip_huc8/20250218/11070203'  # previously 20241002
+# filename_in = os.path.join(dirname, 'nwm_subset_streams.gpkg')
+# filename_out = os.path.join(dirname, 'nwm_subset_streams_20250328.gpkg')
+
 data = gpd.read_file(filename_in)
 for id in ids:
     data.loc[data['ID'] == id, 'geometry'] = data[data['ID'] == id].geometry.reverse()
--- src/run_by_branch.sh
+++ src/run_by_branch.sh
@@ -99,7 +99,7 @@ $srcDir/adjust_floodplains.py \
     -z $floodplain_z_factor \
     -p $tempHucDataDir/branch_polygons.gpkg \
     -b $current_branch_id \
-    -f /data/inputs/fema/nfhl/nfhl_$hucNumber.gpkg
+    -f $input_fema_flood_hazard_zones/nfhl_$hucNumber.gpkg
 
 ## PIT REMOVE BURNED DEM - BRANCH 0 (include all NWM streams) ##
 echo -e $startDiv"Pit remove Burned DEM $hucNumber $current_branch_id"
