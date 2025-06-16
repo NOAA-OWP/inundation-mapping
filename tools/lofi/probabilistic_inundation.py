24189dcb9d0b55131c7401932f8790a41808066d
Ryan Grout <ryan.grout@noaa.gov>
2025-06-16 12:15:20 -0600

    Make --overwrite a boolean.

--- tools/lofi/probabilistic_inundation.py
+++ tools/lofi/probabilistic_inundation.py
@@ -854,7 +854,7 @@ if __name__ == '__main__':
 
     parser.add_argument(
         "--overwrite",
-        default=False,
+        action='store_true',
         help="OPTIONAL: Whether to overwrite existing output",
         required=False,
     )
