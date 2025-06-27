e34c9df7e30091db9af7250e0588ee0d65dcc1ca
Gregory Petrochenkov <greg.petrochenkov@noaa.gov>
2025-06-27 13:17:53 -0400

    Include check for hydrotable_df None type

--- tools/inundate_gms.py
+++ tools/inundate_gms.py
@@ -218,7 +218,7 @@ def __inundate_gms_generator(
     hydro_table_df: Union[str, pd.DataFrame]
         Hydrotable DataFrame.
     verbose: Optional[bool], default = False
-        Whether to qsilence output or not
+        Whether to silence output or not
     windowed: Optional[bool], default = False
         Whether to use window memory optimization
 
@@ -274,7 +274,9 @@ def __inundate_gms_generator(
             else:
                 hydro_table_huc = None
 
-            if os.path.isfile(hydro_table_huc):  # TODO: Replace with s3_or_local_is_file
+            if hydro_table_huc is not None and os.path.isfile(
+                hydro_table_huc
+            ):  # TODO: Replace with s3_or_local_is_file
 
                 hydro_table_all.set_index(["HUC", "feature_id", "HydroID"], inplace=True)
                 hydro_table_branch = hydro_table_all.loc[hydro_table_all["branch_id"] == int(branch_id)]
