c53635225b0151c46c29a6c47f2c6832369202af
MattLuck-NOAA <matt.luck@noaa.gov>
2025-08-15 16:18:20 -0600

    fix for fim_pipeline

--- src/run_by_branch.sh
+++ src/run_by_branch.sh
@@ -16,6 +16,8 @@ huc2Identifier=${hucNumber:0:2}
 ## SET CRS
 if [ $huc2Identifier -eq 19 ]; then
     huc_CRS=$ALASKA_CRS
+elif [ $hucNumber -eq 22010000 ]; then
+    huc_CRS=$GUAM_CRS
 else
     huc_CRS=$DEFAULT_FIM_PROJECTION_CRS
 fi
