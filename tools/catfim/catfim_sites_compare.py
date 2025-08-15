3a34e59a45c73e4366bf58feae8ec334a2c62a2c
EmilyDeardorff-NOAA <Emily.Deardorff@NOAA.gov>
2025-08-15 22:21:47 +0000

    Linting.

--- tools/catfim/catfim_sites_compare.py
+++ tools/catfim/catfim_sites_compare.py
@@ -463,7 +463,6 @@ def remove_polygon_shards(input_gdf, id_col, mag_col, minimum_area_threshold):
     if len(cleaned_gdf) == 0:
         return None
 
-
     cleaned_gdf.reset_index(inplace=True, drop=True)
 
     # Condense back into a multipolygon
