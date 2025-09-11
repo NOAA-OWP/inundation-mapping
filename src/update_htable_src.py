e2e7bf9c562cf5e7b30109bd6426884a7273c2c1
ZahraGhahremani <Zahra.ghahremani@noaa.gov>
2025-09-11 23:35:23 +0000

    Update

--- src/update_htable_src.py
+++ src/update_htable_src.py
@@ -17,13 +17,13 @@ def process_branch(sub_branch_path, branch):
 
     src_full_preserve_columns = [
         'Stage',
-        'Number of Cells',
-        'SurfaceArea (m2)',
-        'BedArea (m2)',
-        'Volume (m3)',
+        # 'Number of Cells', # Read from src_base
+        # 'SurfaceArea (m2)',
+        # 'BedArea (m2)',
+        # 'Volume (m3)',
         'SLOPE_RISE_RUN',
-        'LENGTHKM',
-        'AREASQKM',
+        # 'LENGTHKM',
+        # 'AREASQKM',
         'ManningN',
         'HydroID',
         'NextDownID',
