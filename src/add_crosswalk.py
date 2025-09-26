64f4dd8843710cc52496c1783c902837915e8990
ZahraGhahremani <Zahra.ghahremani@noaa.gov>
2025-09-26 17:55:20 +0000

    fixed a bug

--- src/add_crosswalk.py
+++ src/add_crosswalk.py
@@ -284,11 +284,11 @@ def add_crosswalk(
     # hfab_mask = (input_src_base['SLOPE_HFAB'] >= SLOPE_MIN) & (input_src_base['SLOPE_HFAB'] <= SLOPE_MAX)
 
     # Apply masks to filter out invalid slope values
-    sword_slope = input_src_base['SLOPE_IRIS_SWORD'].where(sword_mask).round(17)
+    sword_slope = input_src_base['SLOPE_IRIS_SWORD'].where(sword_mask).astype(float).round(17)
     # hfab_slope = input_src_base['SLOPE_HFAB'].where(hfab_mask)
 
     # Assign SLOPE values with priority: IRIS_SWORD then RISE_RUN
-    input_src_base['SLOPE'] = sword_slope.combine_first(input_src_base['SLOPE_RISE_RUN']).round(17)
+    input_src_base['SLOPE'] = sword_slope.combine_first(input_src_base['SLOPE_RISE_RUN']).astype(float).round(17)
 
     input_src_base = input_src_base.rename(columns=lambda x: x.strip(" "))
     input_src_base = input_src_base.apply(pd.to_numeric, **{'errors': 'coerce'})
