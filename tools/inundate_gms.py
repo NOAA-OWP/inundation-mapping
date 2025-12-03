348ad4cd84673084dc5ff86478cd7c54e164802f
Gregory Petrochenkov <greg.petrochenkov@noaa.gov>
2025-12-03 13:55:10 -0500

    Hardcode supress output from tqdm for testing

--- tools/inundate_gms.py
+++ tools/inundate_gms.py
@@ -135,7 +135,7 @@ def Inundate_gms(
         as_completed(executor_generator),
         total=len(executor_generator),
         desc=f"Inundating branches with {num_workers} workers",
-        disable=(not verbose),
+        disable=True,
     ):
         hucCode, branch_id = executor_generator[future]
 
