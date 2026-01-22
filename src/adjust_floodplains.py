8895aaeea7f366f35295931c4c67f11fc3edcce5
ZahraGhahremani <Zahra.ghahremani@noaa.gov>
2026-01-22 17:20:12 +0000

    remove only the optional/default behavior

--- src/adjust_floodplains.py
+++ src/adjust_floodplains.py
@@ -64,7 +64,7 @@ def adjust_floodplains(
         The ID of the branch to adjust.
     fema_flood_zones_file : str
         The file containing the FEMA flood zones.
-    fema_flood_zones_layer : str.
+    fema_flood_zones_layer : str, the layer name of the FEMA flood zones.
 
     Returns
     -------
