68621a0a45fc0c1d016607fae053351624b8af48
EmilyDeardorff-NOAA <Emily.Deardorff@NOAA.gov>
2026-06-22 19:59:26 +0000

    Update NWS LID gpkg function.

--- tools/generate_nws_lid.py
+++ tools/generate_nws_lid.py
@@ -2,13 +2,15 @@
 
 import argparse
 import os
-from collections import defaultdict
 from pathlib import Path
 
 import geopandas as gpd
 import pandas as pd
 from dotenv import load_dotenv
+from datetime import datetime, timezone
+
 from tools_shared_functions import aggregate_wbd_hucs, get_metadata
+from src.utils.shared_functions import FIM_Helpers as fh
 
 from utils.shared_variables import PREP_PROJECTION
 
@@ -16,16 +18,38 @@ from utils.shared_variables import PREP_PROJECTION
 gpd.options.io_engine = "pyogrio"
 
 
-# TODO: Jun 2025: Change this to have a path to the config via an arg.
-# See get_usgs_rating_curves for an example
-load_dotenv()
-# import variables from .env file
-API_BASE_URL = os.getenv("API_BASE_URL")
-EVALUATED_SITES_CSV = os.getenv("EVALUATED_SITES_CSV")
-WBD_LAYER = os.getenv("WBD_LAYER")
+def __validate_inputs(env_file, workspace, keep_blank_nwm):
+    '''
+    Get info from env file and validate input filepaths.
+    '''
+
+    load_dotenv(env_file)
+    API_BASE_URL = os.getenv("API_BASE_URL")
+    WBD_LAYER = os.getenv("WBD_LAYER")
+    EVALUATED_SITES_CSV = os.getenv("EVALUATED_SITES_CSV")
+
+    metadata_url = f'{API_BASE_URL}/metadata/'
+
+    filepath_list = [WBD_LAYER, EVALUATED_SITES_CSV]
+    for path in filepath_list:
+        if not os.path.exists(path):
+            raise Exception(f'Unable to find file at {path}, check input env file ({env_file})')
+
+    # Set up output file and confirm that it exists
+    Path(workspace).mkdir(parents=True, exist_ok=True)
+
+    if not os.path.exists(workspace):
+        raise Exception(f'Workspace filepath does not exist. Check workspace input filepath: {workspace}')
+
+    out_filepath = os.path.join(workspace, 'nws_lid.gpkg')
+
+    # Make sure this val is a true boolean
+    keep_blank_nwm = bool(keep_blank_nwm)
+
+    return WBD_LAYER, EVALUATED_SITES_CSV, metadata_url, out_filepath, keep_blank_nwm
 
 
-def generate_nws_lid(workspace):
+def generate_nws_lid(workspace, env_file, keep_blank_nwm):
     '''
     Generate the nws_lid layer containing all nws_lid points
 
@@ -33,23 +57,39 @@ def generate_nws_lid(workspace):
     ----------
     workspace : STR
         Directory where outputs will be saved.
+    env_file : STR
+        Direvtory where env file is saved.
+    keep_blank_nwm : BOOL
+        If True, all rows will be saved (regardless of whether they have NWM Feature ID values).
 
     Returns
     -------
     None.
 
     '''
+    WBD_LAYER, EVALUATED_SITES_CSV, metadata_url, out_filepath, keep_blank_nwm = __validate_inputs(env_file, workspace, keep_blank_nwm)
 
-    # Get all nws_lid points
-    print('Retrieving metadata ..')
+    # Record start time
+    overall_start_dt = datetime.now(timezone.utc)
+    display_dt_string = datetime.now(timezone.utc).strftime("%m/%d/%Y %H:%M:%S")
 
-    metadata_url = f'{API_BASE_URL}/metadata/'
-    # Trace downstream from all rfc_forecast_point.
+    print("=========================================================================")
+    print(f"Generating NWS LID GeoPackage - {display_dt_string} (UTC)")
+    print("")
+
+    if os.path.isfile(out_filepath):
+        print(f"Warning: File already exists and will be overwritten: {out_filepath}")
+        print("")
+
+    # ---
+    # Get the metdata (with downstream trace) for all RFC forecast points
+    print("Getting metadata for all RFC forecast points...")
+
     select_by = 'nws_lid'
     selector = ['all']
     must_include = 'nws_data.rfc_forecast_point'
     downstream_trace_distance = 'all'
-    fcst_list, fcst_dataframe = get_metadata(
+    fcst_list, __, err_msg = get_metadata(
         metadata_url=metadata_url,
         select_by=select_by,
         selector=selector,
@@ -57,18 +97,27 @@ def generate_nws_lid(workspace):
         upstream_trace_distance=None,
         downstream_trace_distance=downstream_trace_distance,
     )
+
+    if err_msg != "":
+        print(err_msg)
 
     # Get list of all evaluated sites not in fcst_list
     fcst_list_sites = [record.get('identifiers').get('nws_lid').lower() for record in fcst_list]
     evaluated_sites = pd.read_csv(EVALUATED_SITES_CSV)['Total_List'].str.lower().to_list()
     evaluated_sites = list(set(evaluated_sites) - set(fcst_list_sites))
 
-    # Trace downstream from all evaluated sites not in fcst_list
+    print(f"Downloaded metadata for {len(fcst_list)} forecast sites")
+    print("")
+
+    # ---
+    # Get metadata (with downstream trace) for all evaluated sites not in forecast sites list
+    print(f"Getting metadata for {len(evaluated_sites)} additional non-forecast sites...")
+
     select_by = 'nws_lid'
     selector = evaluated_sites
     must_include = None
     downstream_trace_distance = 'all'
-    eval_list, eval_dataframe = get_metadata(
+    eval_list, __, err_msg = get_metadata(
         metadata_url=metadata_url,
         select_by=select_by,
         selector=selector,
@@ -77,62 +126,87 @@ def generate_nws_lid(workspace):
         downstream_trace_distance=downstream_trace_distance,
     )
 
+    if err_msg != "":
+        print(err_msg)
+
     # Append all lists
     all_lists = fcst_list + eval_list
 
-    # Compile NWM segments from all_lists
+    print(f"Downloaded metadata for {len(eval_list)} additional eval sites")
+    print(f"Metadata retrieved for {len(all_lists)} total sites")
+    print("")
 
-    # Get dictionary of downstream segment (key) and target segments (values)
-    # Get dictionary of target segment (key) and site code (value)
-    downstream = defaultdict(list)
-    target = defaultdict(list)
-    # For each lid metadata dictionary in list
-    for lid in all_lists:
-        site = lid.get('identifiers').get('nws_lid')
-        # Get the nwm feature id associated with the location
-        location_nwm_seg = lid.get('identifiers').get('nwm_feature_id')
-        # get all downstream segments
-        downstream_nwm_segs = lid.get('downstream_nwm_features')
-        # If valid location_nwm_segs construct two dictionaries.
-        if location_nwm_seg:
-            # Dictionary with target segment and site
-            target[int(location_nwm_seg)].append(site)
-            # Dictionary of key (2nd to last element) and value (target segment)
-            # 2nd to last element used because last element is always 0 (ocean) and the 2nd to last allows
-            # for us to get the river 'tree' (Mississippi, Colorado, etc)
-            value = location_nwm_seg
-            if not downstream_nwm_segs:
-                # Special case, no downstream nwm segments are returned (PR/VI/HI).
-                key = location_nwm_seg
-            elif len(downstream_nwm_segs) == 1:
-                # Special case, the nws_lid is within 1 segment of the ocean (0)
-                key = location_nwm_seg
-            elif len(downstream_nwm_segs) > 1:
-                # Otherwise, 2nd to last element used to identify proper river system.
-                key = downstream_nwm_segs[-2]
-            # Dictionary with key of 2nd to last downstream segment and value of site nwm segment
-            downstream[int(key)].append(int(value))
-
-    # Get Spatial data and populate headwater/duplicate attributes
-    print('Attributing nws_lid layer..')
-
-    # Geodataframe from all_lists, reproject, and reset index.
+    # ---
+    # Get spatial data and populate headwater/duplicate attributes
+    print("Aggregating WBD HUCs...")
     _, nws_lid_gdf = aggregate_wbd_hucs(all_lists, WBD_LAYER, retain_attributes=False)
+
+    # Create GDF Geodataframe from all_lists and reproject GDF
     nws_lid_gdf.columns = [name.replace('identifiers_', '') for name in nws_lid_gdf.columns]
     nws_lid_gdf.to_crs(PREP_PROJECTION, inplace=True)
+
+    # Remove rows without NWM Feature IDs if the -k flag was not used
+    if not keep_blank_nwm:
+        print("Removing rows with blank NWM feature ID")
+        nws_lid_gdf.dropna(subset=['nwm_feature_id'], inplace=True)
+    else:
+        print("Will not remove rows with blank NWM feature ID")
+
+    print("Finished creating NWS LID geodataframe")
+    print("")
+
+    # ---
+    # Reset index and save output files
     nws_lid_gdf.reset_index(drop=True)
-    Path(workspace).mkdir(parents=True, exist_ok=True)
-    nws_lid_gdf.dropna(subset=['nwm_feature_id'], inplace=True)
-    nws_lid_gdf.to_file(Path(workspace) / 'nws_lid.gpkg', driver='GPKG', engine='fiona')
+    nws_lid_gdf.to_file(out_filepath, driver='GPKG', engine='fiona')
+    print(f"Saved GPKG to {out_filepath}")
+
+    # Save a CSV for QC purposes
+    out_csv_filepath = out_filepath.replace(".gpkg", ".csv")
+    nws_lid_gdf.to_csv(out_csv_filepath, index=False)
+    print(f"Saved CSV to {out_csv_filepath}")
+
+    display_dt_string = datetime.now(timezone.utc).strftime("%m/%d/%Y %H:%M:%S")
+    dur_msg = fh.print_date_time_duration(overall_start_dt, datetime.now(timezone.utc), False)
+
+    print("")
+    print(f"Program complete - {display_dt_string} (UTC)")
+    print(f"{dur_msg}")
+    print("=========================================================================")
+
+    return
 
 
 if __name__ == '__main__':
+    '''
+    python /foss_fim/tools/generate_nws_lid.py -w '/dev_fim_share/foss_fim/temp/emily/' -k
+    '''
     # Parse arguments
     parser = argparse.ArgumentParser(
         description='Create spatial data of nws_lid points attributed with mainstems and colocated.'
     )
     parser.add_argument('-w', '--workspace', help='Workspace where all data will be stored.', required=True)
+
+    parser.add_argument(
+        '-e',
+        '--env-file',
+        help='OPTIONAL: Docker mount path to the environment file.'
+        'default = /data/config/fim_enviro_values.env',
+        required=False,
+        default='/data/config/fim_enviro_values.env',
+    )
+
+    parser.add_argument(
+        '-k',
+        '--keep-blank-nwm',
+        help="OPTIONAL: If this flag is set, the sites with blank nwm_feature_id columns will not be removed."
+        "default = False",
+        required=False,
+        default=False,
+        action='store_true',
+    )
+
     args = vars(parser.parse_args())
 
-    # Run get_env_paths and static_flow_lids
+    # Run generate_nws_lid
     generate_nws_lid(**args)
