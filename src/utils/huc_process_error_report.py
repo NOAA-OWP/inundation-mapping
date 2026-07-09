f8f7b2785dcb261b21b7fc39d72dd82450022177
Rob Hanna <robert.hanna@noaa.gov>
2026-07-09 18:57:26 +0000

    fix bug to not create huc error report if no records

--- src/utils/huc_process_error_report.py
+++ src/utils/huc_process_error_report.py
@@ -50,12 +50,13 @@ def scan_error_log(huc_number, source_log_file, output_csv_path):
     lines_found = log_kw_search(source_log_file, huc_number)
 
     # Note: might be an empty file and that is ok.
-    if lines_found == 0:
+    if len(lines_found == 0):
         print("GREAT JOB... no errors found.")
-    output_df = pd.DataFrame(lines_found).astype(str)
-    # print(output_df)
-    output_df.to_csv(output_csv_path, index=False)
-    print(f"Error log report saved to {output_csv_path}")
+    else:
+        output_df = pd.DataFrame(lines_found).astype(str)
+        # print(output_df)
+        output_df.to_csv(output_csv_path, index=False)
+        print(f"Error log report saved to {output_csv_path}")
 
 
 def log_kw_search(logfile, huc_number):
