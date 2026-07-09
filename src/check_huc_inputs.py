bd5957359b2f072498fde6f654c7ca316888d203
Rob Hanna <robert.hanna@noaa.gov>
2026-07-09 18:53:47 +0000

    linting fix

--- src/check_huc_inputs.py
+++ src/check_huc_inputs.py
@@ -56,8 +56,10 @@ def __check_for_membership(hucs, accepted_hucs_set, full_huc_list):
         if (type(huc) is str) and (not huc.isnumeric()):
             huc = str(huc)
             if "." in huc:
-                raise ValueError("The huc(s) or huc list you provided appears invalid and contains a dot."
-                                f" Is this a bad path to a file? arg supplied is {huc}")
+                raise ValueError(
+                    "The huc(s) or huc list you provided appears invalid and contains a dot."
+                    f" Is this a bad path to a file? arg supplied is {huc}"
+                )
             msg = f"Huc value of {huc} does not appear to be a number. "
             msg += "It could be an incorrect value but also could be that the huc list "
             msg += "(if you used one) is incorrect or is not unix encoded."
