0b19edeb30f0585c3a08d86a0ce5b5a21b8a9a52
Ryan Grout <ryan.grout@noaa.gov>
2026-09-08 19:04:58 -0600

    Reuse file handle.

--- tools/inundate_gms.py
+++ tools/inundate_gms.py
@@ -120,56 +120,54 @@ def Inundate_gms(
     branch_ids = [None] * number_of_branches
 
     executor_generator = {executor.submit(inundate, **inp): ids for inp, ids in inundate_input_generator}
-    idx = 0
-    for future in tqdm(
-        as_completed(executor_generator),
-        total=len(executor_generator),
-        desc=f"Inundating branches with {num_workers} workers",
-        disable=(not verbose),
-    ):
-        hucCode, branch_id = executor_generator[future]
+    with open(log_file, "a") as _file:
+        for idx, future in enumerate(tqdm(
+            as_completed(executor_generator),
+            total=len(executor_generator),
+            desc=f"Inundating branches with {num_workers} workers",
+            disable=(not verbose),
+        )):
+            hucCode, branch_id = executor_generator[future]
 
-        try:
-            future.result()
+            try:
+                future.result()
 
-        except NoForecastFound as exc:
-            if log_file is not None:
-                print(f"{hucCode},{branch_id},{exc.__class__.__name__}, {exc}", file=open(log_file, "a"))
-            elif verbose:
-                print(f"{hucCode},{branch_id},{exc.__class__.__name__}, {exc}")
+            except NoForecastFound as exc:
+                if log_file is not None:
+                    print(f"{hucCode},{branch_id},{exc.__class__.__name__}, {exc}", file=_file)
+                elif verbose:
+                    print(f"{hucCode},{branch_id},{exc.__class__.__name__}, {exc}")
 
-        except hydroTableHasOnlyLakes as exc:
-            if log_file is not None:
-                print(f"{hucCode},{branch_id},{exc.__class__.__name__}, {exc}", file=open(log_file, "a"))
-            elif verbose:
-                print(f"{hucCode},{branch_id},{exc.__class__.__name__}, {exc}")
+            except hydroTableHasOnlyLakes as exc:
+                if log_file is not None:
+                    print(f"{hucCode},{branch_id},{exc.__class__.__name__}, {exc}", file=_file)
+                elif verbose:
+                    print(f"{hucCode},{branch_id},{exc.__class__.__name__}, {exc}")
 
-        except Exception as exc:
-            traceback.print_exc(file=sys.stdout)
-            if log_file is not None:
-                print(f"{hucCode},{branch_id},{exc.__class__.__name__}, {exc}", file=open(log_file, "a"))
+            except Exception as exc:
+                traceback.print_exc(file=sys.stdout)
+                if log_file is not None:
+                    print(f"{hucCode},{branch_id},{exc.__class__.__name__}, {exc}", file=_file)
+                else:
+                    print(f"{hucCode},{branch_id},{exc.__class__.__name__}, {exc}")
             else:
-                print(f"{hucCode},{branch_id},{exc.__class__.__name__}, {exc}")
-        else:
-            hucCodes[idx] = hucCode
-            branch_ids[idx] = branch_id
+                hucCodes[idx] = hucCode
+                branch_ids[idx] = branch_id
 
-            try:
-                inundation_raster_fileNames[idx] = future.result()[0][0]
-            except TypeError:
-                pass
+                try:
+                    inundation_raster_fileNames[idx] = future.result()[0][0]
+                except TypeError:
+                    pass
 
-            try:
-                depths_raster_fileNames[idx] = future.result()[1][0]
-            except TypeError:
-                pass
+                try:
+                    depths_raster_fileNames[idx] = future.result()[1][0]
+                except TypeError:
+                    pass
 
-            try:
-                inundation_polygon_fileNames[idx] = future.result()[2][0]
-            except TypeError:
-                pass
-
-            idx += 1
+                try:
+                    inundation_polygon_fileNames[idx] = future.result()[2][0]
+                except TypeError:
+                    pass
 
     # power down pool
     executor.shutdown(wait=True)
