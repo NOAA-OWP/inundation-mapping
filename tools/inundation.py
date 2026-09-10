1e47a7e9bc9acd79fe96b127724b011223dc23d1
Gregory Petrochenkov <greg.petrochenkov@noaa.gov>
2026-09-10 17:42:44 -0400

    Update _fast_inundate

--- tools/inundation.py
+++ tools/inundation.py
@@ -309,22 +309,6 @@ def __inundate_in_huc(
 
 
 def _fast_inundate(rem, catchment, stage_dict, nodata_c, min_value):
-    """
-    Parameters
-        ----------
-        rem : np.ndarray
-            Relative elevation model values which will be replaced by inundation depth values
-        catchments : np.ndarray
-            Rasterized catchments represented by HydroIDs to be replaced with inundation values
-        catchment_stages_dict : tuple
-            Ordered array mapping with HydroID as a key and flood stage as a value
-        x : int
-            Shape of longitude coordinates
-        y : int
-            Shape of latitude coordinates
-        nodata_c : int
-            Nodata value to use for catchment values
-    """
     valid = catchment != nodata_c
 
     vc = catchment[valid]
@@ -332,25 +316,30 @@ def _fast_inundate(rem, catchment, stage_dict, nodata_c, min_value):
 
     keys, values = stage_dict
     idx = np.searchsorted(keys, vc)
-    mask = idx != len(keys)
+    idx = np.minimum(idx, len(keys) - 1, out=idx)
+    mask = keys[idx] == vc
 
-    depths = values[idx[mask]]
-    np.subtract(depths, vr, out=depths, where=mask)
+    if np.any(mask):
+        depths = values[idx]
+        np.subtract(depths, vr, out=depths, where=mask)
 
-    # mask = mask & (vr >= 0) & (depths >= min_value)
-    tmp = vr >= 0
-    np.logical_and(mask, tmp, out=mask)
-    np.greater_equal(depths, min_value, out=tmp)
-    np.logical_and(mask, tmp, out=mask)
-    del tmp
-    np.copyto(vr, depths, where=mask)
+        tmp = vr >= 0
+        np.logical_and(mask, tmp, out=mask)
+        np.greater_equal(depths, min_value, out=tmp)
+        np.logical_and(mask, tmp, out=mask)
+        del tmp
+        # mask = mask & (vr >= 0) & (depths >= min_value)
+        np.copyto(vr, depths, where=mask)
 
-    mask = np.logical_not(mask, out=mask)
-    vr[mask] = 0
-    vc[mask] *= -1
+        mask = np.logical_not(mask, out=mask)
+        vr[mask] = 0
+        vc[mask] *= -1
+    else:
+        vr.fill(0)
+        vc *= -1
+
     del mask
 
-    # Copy back to rem and catchment arrays
     rem[valid] = vr
     catchment[valid] = vc
 
